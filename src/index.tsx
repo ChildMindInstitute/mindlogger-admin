e505e05344a9019ec2498cb40568d2f44b163209
Igor <igoness@gmail.com>
2022-10-25 16:42:19 +0300

    "src" were removed from imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'src/index.css';
-import App from 'src/App';
-import reportWebVitals from 'src/reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
