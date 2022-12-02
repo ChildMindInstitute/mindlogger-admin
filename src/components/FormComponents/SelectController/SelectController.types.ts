a70dbffa57e4f13a1f27c1faeebdedf58055b432
Danilenka, Kiryl <kdanilenka@scnsoft.com>
2022-12-02 17:41:24 +0300

    feature/CMIML-223-add-user

--- src/components/FormComponents/SelectController/SelectController.types.ts
+++ src/components/FormComponents/SelectController/SelectController.types.ts
@@ -1,6 +1,5 @@
 import { ChangeEvent } from 'react';
 import { TextFieldProps } from '@mui/material/TextField';
-
 import { FieldValues, UseControllerProps } from 'react-hook-form';
 
 export type Option = {
