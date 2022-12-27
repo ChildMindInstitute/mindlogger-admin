f21e0402cd24ba6de5330e93c9287f7dbcb3c725
Danilenka, Kiryl <kdanilenka@scnsoft.com>
2022-12-27 13:34:01 +0300

    feature/CMIML-329-create-activity

--- src/features/Schedule/CreateActivityPopup/Availability/Availability.tsx
+++ src/features/Schedule/CreateActivityPopup/Availability/Availability.tsx
@@ -1,14 +1,44 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { SelectController } from 'components/FormComponents';
 import { CheckboxController } from 'components/FormComponents';
 import { StyledBodyMedium } from 'styles/styledComponents/Typography';
 
+import { ToggleButtonGroup } from 'components/ToggleButtonGroup';
+
+import { TimePicker } from 'components/TimePicker';
 import { options } from './Availability.const';
 import { AvailabilityProps } from './Availability.props.types';
 
 export const Availability = ({ control }: AvailabilityProps) => {
   const { t } = useTranslation('app');
+
+  const [activeButton, setActiveButton] = useState('');
+  const [date, setDate] = useState<Date | undefined | null>(new Date());
+
+  const togglebuttons = [
+    {
+      value: 'once',
+      label: 'Once',
+    },
+    {
+      value: 'Daily',
+      label: 'Daily',
+    },
+    {
+      value: 'Weekly',
+      label: 'Weekly',
+    },
+    {
+      value: 'Weekdays',
+      label: 'Weekdays',
+    },
+    {
+      value: 'Monthly',
+      label: 'Monthly',
+    },
+  ];
 
   return (
     <>
@@ -19,6 +49,18 @@ export const Availability = ({ control }: AvailabilityProps) => {
         label=""
         control={control}
       />
+      <CheckboxController
+        name="oneTimeCompletion"
+        control={control}
+        label={<StyledBodyMedium>{t('oneTimeCompletion')}</StyledBodyMedium>}
+      />
+      <ToggleButtonGroup
+        toggleButtons={togglebuttons}
+        activeButton={activeButton}
+        setActiveButton={setActiveButton}
+      />
+      <TimePicker value={date} setValue={setDate} label="From" />
+      <TimePicker value={date} setValue={setDate} label="To" />
       <CheckboxController
         name="oneTimeCompletion"
         control={control}
