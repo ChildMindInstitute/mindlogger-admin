401840b8f94c27f791b4510617572dc443f948f9
nmialik <nmialik@scnsoft.com>
2023-01-26 12:17:56 +0100

    M2-476: fix PR

--- src/components/FormComponents/TagsAutocompleteController/TagsAutocompleteController.types.ts
+++ src/components/FormComponents/TagsAutocompleteController/TagsAutocompleteController.types.ts
@@ -4,9 +4,8 @@ import { FieldValues, UseControllerProps } from 'react-hook-form';
 export type FormInputProps = {
   name: string;
   options: string[] | undefined;
+  onRemove?: (value: string) => void;
 } & TextFieldProps;
 
 export type TagsAutocompleteControllerProps<T extends FieldValues> = FormInputProps &
-  UseControllerProps<T> & {
-    onRemove?: (value: string) => void;
-  };
+  UseControllerProps<T>;
