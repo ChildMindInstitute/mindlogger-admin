7c5505a8cffd44af4d9e8525a924e0e476af68e3
Hanna <35298264+wildkiomi@users.noreply.github.com>
2023-02-01 15:27:57 +0300

    M2-468 Availability in popups (#124)

--- src/features/Applet/Schedule/CreateActivityPopup/CreateActivityPopup.types.ts
+++ src/features/Applet/Schedule/CreateActivityPopup/CreateActivityPopup.types.ts
@@ -1,4 +1,8 @@
+import { Dispatch, SetStateAction } from 'react';
+
 export type CreateActivityPopupProps = {
   open: boolean;
   onClose: () => void;
+  activityName: string;
+  setCreateActivityPopupVisible: Dispatch<SetStateAction<boolean>>;
 };
