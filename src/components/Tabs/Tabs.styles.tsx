a822a2f1826a54a6d9ad6ba99e63ad00ae325167
wildkiomi <whisann.lie14@gmail.com>
2023-02-01 19:04:50 +0300

    Fix

--- src/components/Tabs/Tabs.styles.tsx
+++ src/components/Tabs/Tabs.styles.tsx
@@ -16,7 +16,7 @@ export const StyledTabs = styled(Tabs, shouldForwardProp)`
   ${({ uiType }) =>
     uiType === UiType.primary &&
     `
-    width: 900rem;
+    width: 90rem;
     margin: 0 auto;
   `}
 
