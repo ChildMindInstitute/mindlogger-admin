044727d26c05dfffee46162cf733d314d58b3423
wildkiomi <whisann.lie14@gmail.com>
2023-02-09 13:11:27 +0300

    Fix

--- src/features/Builder/BuilderAppletSettings/BuilderAppletSettings.const.tsx
+++ src/features/Builder/BuilderAppletSettings/BuilderAppletSettings.const.tsx
@@ -4,12 +4,19 @@ import {
   TransferOwnershipSetting,
   ShareAppletSetting,
   DownloadSchemaSetting,
+  DeleteAppletSetting,
+  ExportDataSetting,
 } from 'features/AppletSettings';
 
 export const settings = [
   {
     label: 'usersAndData',
     items: [
+      {
+        icon: <Svg id="export" />,
+        label: 'exportData',
+        component: <ExportDataSetting />,
+      },
       {
         icon: <Svg id="data-retention" />,
         label: 'dataRetention',
@@ -26,9 +33,19 @@ export const settings = [
         component: <DownloadSchemaSetting />,
       },
       {
+        icon: <Svg id="version-history" />,
+        label: 'versionHistory',
+        component: <>versionHistory</>,
+      },
+      {
         icon: <Svg id="transfer-ownership" />,
         label: 'transferOwnership',
         component: <TransferOwnershipSetting />,
+      },
+      {
+        icon: <Svg id="trash" />,
+        label: 'deleteApplet',
+        component: <DeleteAppletSetting />,
       },
     ],
   },
