f0f0b14ab31bbf4fd72d71268ac744487b92ceec
nmialik <115553085+nmialik@users.noreply.github.com>
2023-02-23 11:41:01 +0100

    Fix: Library - Mocked data (#157)

--- src/features/Library/AppletDetails/AppletDetails.tsx
+++ src/features/Library/AppletDetails/AppletDetails.tsx
@@ -1,17 +1,22 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
+import { PublishedApplet } from 'redux/modules';
 import { useBreadcrumbs } from 'hooks';
 import { StyledBody } from 'styles/styledComponents';
 import { Header } from 'features/Library/Header';
 import { RightButtonType } from 'features/Library/Header/Header.types';
 import { Applet, AppletUiType } from 'features/Library/Applet';
 import { page } from 'resources';
+import { mockedApplets } from 'features/Library/AppletsCatalog/mocked';
 
 import { ContentContainer } from './AppletDetails.styles';
-import { mockedApplet as applet } from './mocked';
 
 export const AppletDetails = () => {
   const navigate = useNavigate();
+  const { id } = useParams();
+
+  // TODO: replace with real data when the endpoint is ready
+  const applet = mockedApplets.data.find(({ appletId }) => appletId === id) as PublishedApplet;
 
   useBreadcrumbs();
 
---
+++ src/features/Library/AppletsCatalog/mocked.ts
@@ -0,0 +1,154 @@
+export const mockedApplets = {
+  data: [
+    {
+      accountId: '60d304bbb8d2547327547d2f',
+      appletId: '6177f838ca0b5b2015f31320',
+      categoryId: null,
+      description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      id: '6177f9e3ca0b5b2015f3132d',
+      image: '',
+      keywords: ['multiple owners', 'view all contributions test'],
+      name: 'applet library test',
+      subCategoryId: null,
+    },
+    {
+      accountId: '616fe37386fb6903962dd656',
+      appletId: '61e6a98586e8fa4e45322344',
+      categoryId: null,
+      description:
+        'Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
+      id: '61e6dd6fd2d27f6294c2c19c',
+      image:
+        'https://mindlogger-applet-contents.s3.amazonaws.com/image/ia6cUjbrU7xgfX3BRU1sKx.jpeg',
+      keywords: ['Applet name', 'Applet name 123'],
+      name: 'Applet name',
+      subCategoryId: null,
+    },
+    {
+      accountId: '61fab3cf47e81b88522ef832',
+      appletId: '61fab6d647e81b88522ef90f',
+      categoryId: null,
+      description:
+        'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English.',
+      id: '61fd29467c9676d5377c10dd',
+      image:
+        'https://mindlogger-applet-contents.s3.amazonaws.com/image/3z85ruP3ZiC44izmmEafNN.jpeg',
+      keywords: ['applet name (anita)'],
+      name: 'applet name (anita)',
+      subCategoryId: null,
+    },
+    {
+      accountId: '614c3eb5e913a1a4f2a8b695',
+      appletId: '614c4400e913a1a4f2a8b738',
+      categoryId: null,
+      description:
+        'Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+      id: '615eec78541ef0ad3d99f2f0',
+      image: '',
+      keywords: ['multiple item types'],
+      name: 'multiple item types',
+      subCategoryId: null,
+      version: 'v1.5.8',
+    },
+    {
+      accountId: '6257ebb23646a059f4f062a2',
+      appletId: '6257fbdb3646a059f4f062af',
+      categoryId: null,
+      description: 'My applet description',
+      id: '636544ed64d8af5b305cd29f',
+      image:
+        'https://mindlogger-applet-contents.s3.amazonaws.com/image/jhKcAPaBYj4uzYj3qJ8W3P.jpeg',
+      keywords: ['My applet', 'ml_account'],
+      name: 'My applet',
+      subCategoryId: null,
+    },
+    {
+      accountId: '61111658e8616e97459a03fd',
+      appletId: '61162fd4d51900374827f9e9',
+      categoryId: null,
+      description:
+        'Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy.',
+      id: '61163085d51900374827f9f3',
+      image: '',
+      keywords: ['unique id ', 'my id'],
+      name: 'My Applet added from libraby',
+      subCategoryId: null,
+    },
+    {
+      accountId: '629872ba7680030fb93f3d44',
+      appletId: '629a0dfc7680030fb93f45d9',
+      categoryId: null,
+      description: 'my applet macos',
+      id: '629a2cd77680030fb93f480c',
+      image:
+        'https://mindlogger-applet-contents.s3.amazonaws.com/image/eirwbYNPntHg1zDiHUwVLv.jpeg',
+      keywords: ['applet', 'macos', 'v0.16.1'],
+      name: 'my applet macos',
+      subCategoryId: null,
+      version: 'v2.0.1',
+    },
+    {
+      accountId: '62960db77680030fb93f2d80',
+      appletId: '629618487680030fb93f31e2',
+      categoryId: null,
+      description:
+        'My applet ML3 description It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distributio',
+      id: '62977bf77680030fb93f38ff',
+      image:
+        'https://mindlogger-applet-contents.s3.amazonaws.com/image/7zN2XZ9ptvu7QwtS9ofhD3.jpeg',
+      keywords: ['one keyword'],
+      name: 'My applet ML3',
+      subCategoryId: null,
+    },
+    {
+      accountId: '62960db77680030fb93f2d80',
+      appletId: '629753b17680030fb93f369d',
+      categoryId: null,
+      description:
+        'My applet ML3 description It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distributio',
+      id: '62977f897680030fb93f3933',
+      image:
+        'https://mindlogger-applet-contents.s3.amazonaws.com/image/7zN2XZ9ptvu7QwtS9ofhD3.jpeg',
+      keywords: ['one keyword', 'two keywords', 'three keywords'],
+      name: 'My applet ML3 (web)',
+      subCategoryId: null,
+    },
+    {
+      accountId: '60ca1b939be5394dcd1d9bff',
+      appletId: '60f6a5c0ef7ced81c54c7c33',
+      categoryId: null,
+      description:
+        'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English.',
+      id: '60f6a5f4ef7ced81c54c7c3d',
+      image: '',
+      keywords: ['applet to transfe'],
+      name: 'applet to transfer',
+      subCategoryId: null,
+    },
+    {
+      accountId: '5fea2bf78b203d76fda191c8',
+      appletId: '6135cc284416066d3ae4311e',
+      categoryId: null,
+      description: 'Applet_Dropdown',
+      id: '6154853898e6efa61867309f',
+      image: '',
+      keywords: ['dropdown'],
+      name: 'Applet_Dropdown',
+      subCategoryId: null,
+    },
+    {
+      accountId: '60ab77b3baf6e462af369787',
+      appletId: '60b9f6d55a14862a34c1da7f',
+      categoryId: null,
+      description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      id: '60c742e49be5394dcd1d7ed9',
+      image: '',
+      keywords: ['Applet_sub_scale', 'yahoo2'],
+      name: 'Applet_sub_scale (yahoo2)',
+      subCategoryId: null,
+    },
+  ],
+  totalCount: 12,
+};
