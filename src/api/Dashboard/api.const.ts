252509ab8940f3c905a695a7eb963af6baee293f
IhorNesterenko99 <inesterenko@scnsoft.com>
2023-02-24 19:35:18 +0200

    feat: M2-648 Connect new api to the applet table

--- src/api/Dashboard/api.const.ts
+++ src/api/Dashboard/api.const.ts
@@ -3,7 +3,7 @@ import { ServerUrlOption } from './api.types';
 // TODO: Move to .env
 const PROD_SERVER = 'https://api-dev.cmiml.net';
 const STAGING_SERVER = 'https://api-dev.cmiml.net';
-const DEV_SERVER = 'https://api-test.cmiml.net';
+const DEV_SERVER = 'https://api-dev.cmiml.net';
 
 export const BASE_API_URL = process.env.NODE_ENV === 'production' ? PROD_SERVER : STAGING_SERVER;
 
