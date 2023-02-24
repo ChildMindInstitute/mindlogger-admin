5cb9e3b131fac6ce498adb858075898bd5746841
Danilenka, Kiryl <kdanilenka@scnsoft.com>
2023-02-24 12:10:03 +0300

    fix/styles-fixes

--- src/features/Applet/Schedule/Legend/Legend.styles.ts
+++ src/features/Applet/Schedule/Legend/Legend.styles.ts
@@ -23,7 +23,8 @@ export const StyledSelect = styled(Box)`
   }
 
   .MuiBox-root {
-    & > svg {
+    & > svg,
+    .icon-wrapper {
       display: none;
     }
   }
@@ -98,5 +99,5 @@ export const StyledSearchContainer = styled(Box)`
 `;
 
 export const StyledIconBtn = styled(StyledClearedButton)`
-  padding: ${theme.spacing(1.6)};
+  padding: ${theme.spacing(1)};
 `;
