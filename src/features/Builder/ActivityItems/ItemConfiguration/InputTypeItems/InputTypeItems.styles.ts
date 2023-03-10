a696947363a15248e45a355cfd4b26b4397fc528
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2023-03-10 15:44:53 +0300

    added ItemOptionContainer

---
+++ src/features/Builder/ActivityItems/ItemConfiguration/InputTypeItems/InputTypeItems.styles.ts
@@ -0,0 +1,12 @@
+import { styled } from '@mui/material';
+
+import { StyledFlexColumn } from 'styles/styledComponents';
+import { variables } from 'styles/variables';
+import theme from 'styles/theme';
+
+export const StyledItemOptionContainer = styled(StyledFlexColumn)`
+  background: ${variables.palette.surface1};
+  padding: ${theme.spacing(3, 3, 2.1, 3)};
+  border-radius: ${variables.borderRadius.lg2};
+  margin-bottom: ${theme.spacing(2.4)};
+`;
