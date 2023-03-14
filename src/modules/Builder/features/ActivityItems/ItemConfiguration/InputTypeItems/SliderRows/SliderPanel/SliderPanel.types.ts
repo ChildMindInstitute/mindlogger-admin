129351c72090d057b27fd24a52c151677aac979a
muhurov <muhurov@gmail.com>
2023-03-14 18:46:07 +0300

    refactor: M2-729 Builder - Activity Items tab - Item configuration - Slider/Slider rows - Slider

--- src/modules/Builder/features/ActivityItems/ItemConfiguration/InputTypeItems/SliderRows/SliderPanel/SliderPanel.types.ts
+++ src/modules/Builder/features/ActivityItems/ItemConfiguration/InputTypeItems/SliderRows/SliderPanel/SliderPanel.types.ts
@@ -2,6 +2,6 @@ import { FieldValues, UseControllerProps } from 'react-hook-form';
 
 export type SliderPanelProps<T extends FieldValues> = {
   label: string;
-  isMultiple: boolean | undefined;
+  isMultiple?: boolean;
   onRemove: () => void;
 } & UseControllerProps<T>;
