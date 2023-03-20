bbf077a4e7e252682507d1f177d03a7e603a696b
nmialik <nmialik@scnsoft.com>
2023-03-20 13:36:19 +0100

    PR fixes

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const webpack = require('webpack');
-const CircularDependencyPlugin = require('circular-dependency-plugin');
-
-const MAX_CYCLES = 5;
-let numCyclesDetected = 0;
 
 module.exports = function override(config) {
   config.resolve.fallback = {
@@ -16,25 +12,6 @@ module.exports = function override(config) {
     new webpack.ProvidePlugin({
       process: 'process/browser',
       Buffer: ['buffer', 'Buffer'],
-    }),
-    new CircularDependencyPlugin({
-      exclude: /node_modules/,
-      onStart({ compilation }) {
-        numCyclesDetected = 0;
-      },
-      onDetected({ module: webpackModuleRecord, paths, compilation }) {
-        numCyclesDetected++;
-        compilation.warnings.push(new Error(paths.join(' -> ')));
-      },
-      onEnd({ compilation }) {
-        if (numCyclesDetected > MAX_CYCLES) {
-          compilation.errors.push(
-            new Error(
-              `Detected ${numCyclesDetected} cycles which exceeds configured limit of ${MAX_CYCLES}`,
-            ),
-          );
-        }
-      },
     }),
   );
 
