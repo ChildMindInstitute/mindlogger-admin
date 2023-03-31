ca174f8ce480021e4a8f4eda83e0c7f61812571a
Danilenka, Kiryl <kdanilenka@scnsoft.com>
2023-03-31 11:39:03 +0300

    feature/M2-1095-respondents-api

--- src/shared/components/Search/Search.tsx
+++ src/shared/components/Search/Search.tsx
@@ -3,13 +3,17 @@ import debounce from 'lodash.debounce';
 
 import { Svg } from 'shared/components/Svg';
 
+import { DEBOUNCE_VALUE } from 'shared/consts';
 import { StyledTextField, StyledIcon } from './Search.styles';
 import { SearchProps } from './Search.types';
 
 export const Search = ({ onSearch, ...props }: SearchProps) => (
   <StyledTextField
     {...props}
-    onChange={debounce((event: ChangeEvent<HTMLInputElement>) => onSearch(event.target.value), 700)}
+    onChange={debounce(
+      (event: ChangeEvent<HTMLInputElement>) => onSearch(event.target.value),
+      DEBOUNCE_VALUE,
+    )}
     startAdornment={
       <StyledIcon>
         <Svg id="search" />
--- src/shared/consts.ts
+++ src/shared/consts.ts
@@ -48,3 +48,5 @@ export enum Roles {
   Editor = 'editor',
   Reviewer = 'reviewer',
 }
+
+export const DEBOUNCE_VALUE = 700;
