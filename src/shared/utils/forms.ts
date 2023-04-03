b294055a66a5198463a6a0c7282e7b3045961f5a
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2023-04-03 17:18:50 +0300

    fixed dictionary fields in applet body

---
+++ src/shared/utils/forms.ts
@@ -0,0 +1,9 @@
+import i18n from 'i18n';
+
+export const getDictionaryText = (description?: string | Record<string, string>) => {
+  if (!description) return '';
+
+  const { language } = i18n;
+
+  return typeof description === 'object' ? description[language] ?? '' : description ?? '';
+};
