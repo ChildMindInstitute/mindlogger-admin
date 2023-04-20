f2d90116b1dc9df3fe4ea3ce0282315adf904dd6
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2023-04-20 18:46:21 +0300

    fixed color for error text

--- src/shared/styles/styledComponents/ErrorText.ts
+++ src/shared/styles/styledComponents/ErrorText.ts
@@ -9,5 +9,7 @@ export const StyledErrorText = styled(StyledLabelMedium)`
   margin-top: ${({ marginTop }: { marginTop?: number }) =>
     marginTop?.toString() ? theme.spacing(marginTop) : theme.spacing(-1.5)};
   margin-bottom: ${theme.spacing(1.2)};
-  color: ${variables.palette.semantic.error};
+  && {
+    color: ${variables.palette.semantic.error};
+  }
 `;
