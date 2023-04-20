1df5b4d8d0f4d90b4904808fb54df73778635eb8
Danilenka, Kiryl <kdanilenka@scnsoft.com>
2023-04-20 14:19:35 +0300

    dataviz-table-mock

--- src/modules/Dashboard/features/RespondentData/RespondentDataSummary/Report/ReportTable/ReportTable.tsx
+++ src/modules/Dashboard/features/RespondentData/RespondentDataSummary/Report/ReportTable/ReportTable.tsx
@@ -1,11 +1,12 @@
 import { useTranslation } from 'react-i18next';
+import { Box } from '@mui/material';
 
 import { Search } from 'shared/components';
 import { useTable } from 'shared/hooks';
 import { Table } from 'modules/Dashboard/components';
-import { variables } from 'shared/styles';
+import { theme, variables } from 'shared/styles';
 
-import { getHeadCells } from './ReportTable.const';
+import { getHeadCells, rows } from './ReportTable.const';
 import { StyledTableWrapper } from './ReportTable.styles';
 
 export const ReportTable = () => {
@@ -14,15 +15,15 @@ export const ReportTable = () => {
   const { searchValue, handleSearch, ...tableProps } = useTable(async () => null);
 
   return (
-    <>
+    <Box sx={{ mt: theme.spacing(2.4) }}>
       <Search
         background={variables.palette.outline_alfa8}
         placeholder={t('search')}
         onSearch={handleSearch}
       />
       <StyledTableWrapper>
-        <Table rows={[]} columns={getHeadCells()} count={0} {...tableProps} />
+        <Table rows={rows} columns={getHeadCells()} count={3} {...tableProps} />
       </StyledTableWrapper>
-    </>
+    </Box>
   );
 };
