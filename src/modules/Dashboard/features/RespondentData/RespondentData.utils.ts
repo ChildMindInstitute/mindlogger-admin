917bad99c58ba6e896ac298ea667774dd4a2a493
wildkiomi <whisann.lie14@gmail.com>
2023-04-26 11:10:45 +0400

    M2-1545 Changed item type to item content

--- src/modules/Dashboard/features/RespondentData/RespondentData.utils.ts
+++ src/modules/Dashboard/features/RespondentData/RespondentData.utils.ts
@@ -6,7 +6,5 @@ import { UNSUPPORTED_ITEMS } from './consts';
 
 export const isItemUnsupported = (type: ItemResponseType) => UNSUPPORTED_ITEMS.includes(type);
 
-export const getItemLabel = (type: ItemResponseType) => `${type}ItemTask`;
-
 export const getRespondentLabel = (secretId: string | undefined, nickname: string | undefined) =>
   secretId ? `${t('user')}: ${secretId} (${nickname})` : '';
