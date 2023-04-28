232e5210a198e5c933c316882cb9189f630b80f6
IhorNesterenko99 <116339462+IhorNesterenko99@users.noreply.github.com>
2023-04-28 18:20:25 +0300

    feat: M2-1417 Dashboard: Last Edit time displays in UTC+0 format (Respondents tab) (#346)

--- src/modules/Dashboard/features/Applets/Table/AppletItem/AppletItem.utils.tsx
+++ src/modules/Dashboard/features/Applets/Table/AppletItem/AppletItem.utils.tsx
@@ -59,12 +59,3 @@ export const getActions = ({
     tooltipTitle: t('shareWithTheLibrary'),
   },
 ];
-
-const MINUTES_TO_MILLISECONDS_MULTIPLIER = 60 * 1000;
-
-export const getDateInUserTimezone = (dateString: string) => {
-  const date = new Date(dateString);
-  const userDateOffsetMinutes = new Date().getTimezoneOffset();
-
-  return new Date(date.getTime() - userDateOffsetMinutes * MINUTES_TO_MILLISECONDS_MULTIPLIER);
-};
