51f3eeae1f3a164901cba2e97de24dd851f29080
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2023-04-29 15:03:12 +0300

    kept lang key in local storage after logout

--- src/shared/utils/storage.ts
+++ src/shared/utils/storage.ts
@@ -1,35 +1,9 @@
 import secureLocalStorage from 'react-secure-storage';
 
-class SecureLocalStorage {
-  setItem(key: string, value: string | object | number | boolean) {
-    secureLocalStorage.setItem(key, value);
-  }
+export const clearLocalStorageExceptSetupKeys = () => {
+  const lang = secureLocalStorage.getItem('lang');
+  secureLocalStorage.clear();
+  secureLocalStorage.setItem('lang', lang as string);
+};
 
-  getItem(key: string): string | object | number | boolean | null {
-    return secureLocalStorage.getItem(key);
-  }
-
-  removeItem(key: string): void {
-    secureLocalStorage.removeItem(key);
-  }
-
-  clear(shouldKeepSetupKeys = false): void {
-    // Preserve necessary keys
-    if (!shouldKeepSetupKeys) {
-      secureLocalStorage.clear();
-
-      return;
-    }
-
-    const lang = secureLocalStorage.getItem('lang');
-    secureLocalStorage.clear();
-    secureLocalStorage.setItem('lang', lang as string);
-  }
-}
-
-/**
- * Create an instance of secureLocalStorage
- */
-const secureMLLocalStorage = new SecureLocalStorage();
-
-export { secureMLLocalStorage as storage };
+export { secureLocalStorage as storage };
