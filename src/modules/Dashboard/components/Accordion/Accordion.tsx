f695db7179aa9ec0799e811833cdb8532f38bb73
Danilenka, Kiryl <kdanilenka@scnsoft.com>
2023-05-03 10:44:10 +0300

    feature/M2-1674-subscales-layout

--- src/modules/Dashboard/components/Accordion/Accordion.tsx
+++ src/modules/Dashboard/components/Accordion/Accordion.tsx
@@ -8,13 +8,13 @@ import { AccordionProps } from './Accordion.types';
 export const Accordion = ({ children, title }: AccordionProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleExpande = () => {
+  const handleItemExpand = () => {
     setIsOpen((prevState) => !prevState);
   };
 
   return (
     <StyledItem>
-      <StyledHeader onClick={handleExpande}>
+      <StyledHeader onClick={handleItemExpand}>
         <StyledSvg id={isOpen ? 'navigate-up' : 'navigate-down'} width={20} height={20} />
         <StyledTitleBoldMedium color={variables.palette.on_surface_variant}>
           {title}
