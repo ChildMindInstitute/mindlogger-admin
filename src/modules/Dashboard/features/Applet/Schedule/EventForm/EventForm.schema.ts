0b91ceb51dec0292b4c92b2e427aaf04d7a14111
IhorNesterenko99 <116339462+IhorNesterenko99@users.noreply.github.com>
2023-05-12 13:42:06 +0300

    feat: M2-1271 Schedule: No error for 00:00 Idle time in Duration time picker (#407)

--- src/modules/Dashboard/features/Applet/Schedule/EventForm/EventForm.schema.ts
+++ src/modules/Dashboard/features/Applet/Schedule/EventForm/EventForm.schema.ts
@@ -1,18 +1,18 @@
 import * as yup from 'yup';
 
 import i18n from 'i18n';
-import { NotificationType, TimerType } from 'modules/Dashboard/api';
+import { NotificationType } from 'modules/Dashboard/api';
 
 import {
   getTimeComparison,
   getNotificationTimeComparison,
   getNotificationsValidation,
+  getTimerDurationCheck,
 } from './EventForm.utils';
 
 export const EventFormSchema = () => {
   const { t } = i18n;
   const activityRequired = t('activityRequired');
-  const timerDurationCheck = t('timerDurationCheck');
   const selectValidPeriod = t('selectValidPeriod');
 
   const notificationSchema = yup.object().shape({
@@ -24,18 +24,8 @@ export const EventFormSchema = () => {
   return yup
     .object({
       activityOrFlowId: yup.string().required(activityRequired),
-      timerDuration: yup.string().when('timerType', {
-        is: TimerType.Timer,
-        then: yup.string().test('is-valid-duration', timerDurationCheck, (value) => {
-          if (!value) {
-            return false;
-          }
-          const [hours, minutes] = value.split(':');
-
-          return Number(hours) > 0 || Number(minutes) > 0;
-        }),
-        otherwise: yup.string(),
-      }),
+      timerDuration: getTimerDurationCheck(),
+      idleTime: getTimerDurationCheck(),
       startTime: getTimeComparison(selectValidPeriod),
       endTime: getTimeComparison(''),
       notifications: yup.array().of(notificationSchema),
