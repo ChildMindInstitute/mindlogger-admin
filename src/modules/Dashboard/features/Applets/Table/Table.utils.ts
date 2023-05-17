f4c71ae272c71c7329a3096158651bf5289a8f5b
wildkiomi <whisann.lie14@gmail.com>
2023-05-17 17:29:33 +0400

    Create folder row

--- src/modules/Dashboard/features/Applets/Table/Table.utils.ts
+++ src/modules/Dashboard/features/Applets/Table/Table.utils.ts
@@ -2,10 +2,27 @@ import { FolderApplet } from 'redux/modules';
 import { Order } from 'shared/types';
 
 const descendingComparator = (a: FolderApplet, b: FolderApplet, orderBy: string) => {
+  if (b.isNew || a.isNew) {
+    return 0;
+  }
   if (b[orderBy as keyof FolderApplet]! < a[orderBy as keyof FolderApplet]!) {
     return -1;
   }
   if (b[orderBy as keyof FolderApplet]! > a[orderBy as keyof FolderApplet]!) {
+    return 1;
+  }
+
+  return 0;
+};
+
+const comparePinned = (a: FolderApplet, b: FolderApplet) => {
+  if (a.pinOrder && b.pinOrder) {
+    return 0;
+  }
+  if (a.pinOrder) {
+    return -1;
+  }
+  if (b.pinOrder) {
     return 1;
   }
 
@@ -26,12 +43,15 @@ export const sortRows = (
   rows: FolderApplet[],
   compareFunction: (a: FolderApplet, b: FolderApplet) => number,
 ) => {
-  const result = rows.filter((row) => !row.parentId).sort(compareFunction);
-  const folders = rows.filter((row) => row.isFolder);
+  const result = rows
+    .filter(({ parentId, isFolder }) => !parentId || isFolder)
+    .sort(compareFunction);
+  const folders = rows.filter(({ isFolder }) => isFolder);
   folders.forEach((folder) => {
-    const children = rows.filter((row) => folder.id === row.parentId).sort(compareFunction);
-    const index = result.findIndex((row) => row.id === folder.id);
-    insert(index + 1, result, children);
+    const children = rows.filter(({ parentId }) => folder.id === parentId);
+    const sortedChildren = children.sort(compareFunction).sort(comparePinned);
+    const index = result.findIndex(({ id }) => id === folder.id);
+    insert(index + 1, result, sortedChildren);
   });
 
   return result;
