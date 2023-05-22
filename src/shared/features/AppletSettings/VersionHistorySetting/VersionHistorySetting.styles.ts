3b0d453bc3bce182d44723da5e5c8f0afbf69fb1
nmialik <nmialik@scnsoft.com>
2023-05-22 20:06:00 +0200

    M2-1910: Applet settings - Version history - Get versions

--- src/shared/features/AppletSettings/VersionHistorySetting/VersionHistorySetting.styles.ts
+++ src/shared/features/AppletSettings/VersionHistorySetting/VersionHistorySetting.styles.ts
@@ -1,13 +1,5 @@
 import { Select, styled } from '@mui/material';
 
-import { variables, theme } from 'shared/styles';
-
 export const StyledVersionSelect = styled(Select)`
   max-width: 54.6rem;
-  margin-bottom: ${theme.spacing(2.4)};
-  color: ${variables.palette.on_surface_variant};
-
-  .MuiTypography-root {
-    color: ${variables.palette.on_surface_variant};
-  }
 `;
