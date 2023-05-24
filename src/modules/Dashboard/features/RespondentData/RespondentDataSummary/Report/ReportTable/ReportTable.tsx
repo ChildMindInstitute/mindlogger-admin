db3f16e9901f1a9bfd05dec98fd77cbdceba49f1
nmialik <nmialik@scnsoft.com>
2023-05-24 11:27:15 +0200

    M2-2002: Dataviz - Table search

--- src/modules/Dashboard/features/RespondentData/RespondentDataSummary/Report/ReportTable/ReportTable.tsx
+++ src/modules/Dashboard/features/RespondentData/RespondentDataSummary/Report/ReportTable/ReportTable.tsx
@@ -1,20 +1,63 @@
+import { useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Box } from '@mui/material';
+import { format } from 'date-fns';
 
-import { Search } from 'shared/components';
-import { useTable } from 'shared/hooks';
+import { DEFAULT_ROWS_PER_PAGE, Search } from 'shared/components';
 import { Table } from 'modules/Dashboard/components';
 import { theme, variables } from 'shared/styles';
+import { Order } from 'shared/types';
+import { DateFormats } from 'shared/consts';
 
 import { getHeadCells } from './ReportTable.const';
 import { StyledTableWrapper } from './ReportTable.styles';
 import { ReportTableProps } from './ReportTable.types';
-import { getRows } from './ReportTable.utils';
+import { filterReportTable, getComparator, getRows, stableSort } from './ReportTable.utils';
 
 export const ReportTable = ({ answers = [] }: ReportTableProps) => {
   const { t } = useTranslation('app');
 
-  const { searchValue, handleSearch, ...tableProps } = useTable(async () => null);
+  const [page, setPage] = useState(1);
+  const [order, setOrder] = useState<Order>('desc');
+  const [orderBy, setOrderBy] = useState('');
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSearch = (searchValue: string) => {
+    setSearchValue(searchValue);
+  };
+
+  const handleChangePage = (_: unknown, page: number) => {
+    setPage(page);
+  };
+
+  const handleRequestSort = (_: unknown, property: string) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const visibleRows = useMemo(() => {
+    const currentPage = page - 1;
+    const formattedAnswers = answers
+      .map(({ date, value }) => ({
+        date: format(new Date(date), DateFormats.DayMonthYear),
+        time: format(new Date(date), DateFormats.Time),
+        response: value as string,
+      }))
+      .filter(
+        ({ date, time, response }) =>
+          filterReportTable(response, searchValue?.toLowerCase()) ||
+          filterReportTable(date, searchValue?.toLowerCase()) ||
+          filterReportTable(time, searchValue?.toLowerCase()),
+      );
+
+    const visibleAnswers = stableSort(formattedAnswers, getComparator(order, orderBy)).slice(
+      currentPage * DEFAULT_ROWS_PER_PAGE,
+      currentPage * DEFAULT_ROWS_PER_PAGE + DEFAULT_ROWS_PER_PAGE,
+    );
+
+    return getRows(visibleAnswers);
+  }, [answers, searchValue, page, order, orderBy]);
 
   return (
     <Box sx={{ mt: theme.spacing(2.4) }}>
@@ -24,7 +67,16 @@ export const ReportTable = ({ answers = [] }: ReportTableProps) => {
         onSearch={handleSearch}
       />
       <StyledTableWrapper>
-        <Table rows={getRows(answers)} columns={getHeadCells()} count={3} {...tableProps} />
+        <Table
+          page={page}
+          order={order}
+          orderBy={orderBy}
+          rows={visibleRows}
+          columns={getHeadCells()}
+          handleRequestSort={handleRequestSort}
+          handleChangePage={handleChangePage}
+          count={answers.length}
+        />
       </StyledTableWrapper>
     </Box>
   );
--- src/modules/Dashboard/features/RespondentData/RespondentDataSummary/Report/ReportTable/ReportTable.types.ts
+++ src/modules/Dashboard/features/RespondentData/RespondentDataSummary/Report/ReportTable/ReportTable.types.ts
@@ -1,5 +1,11 @@
 import { ItemAnswer } from '../Report.types';
 
+export type TextItemAnswer = {
+  date: string;
+  time: string;
+  response: string;
+};
+
 export type ReportTableProps = {
   answers?: ItemAnswer[];
 };
