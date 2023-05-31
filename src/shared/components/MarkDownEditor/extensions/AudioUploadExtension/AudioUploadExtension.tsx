704c1b3b561a16bfe363d3e36aa73f37b9b20ca4
Aleksandr Eremin <aleksandr.i.eremin@yandex.ru>
2023-05-31 15:06:19 +0300

    fixed preview for uploaded files, added correct label for uploaded files, added restriction for video format (#469)

--- src/shared/components/MarkDownEditor/extensions/AudioUploadExtension/AudioUploadExtension.tsx
+++ src/shared/components/MarkDownEditor/extensions/AudioUploadExtension/AudioUploadExtension.tsx
@@ -2,24 +2,22 @@ import MdEditor, { InsertContentGenerator } from 'md-editor-rt';
 import { Paper } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
-import { Svg } from 'shared/components';
+import { SourceLinkModalForm, Svg } from 'shared/components';
 import { StyledFlexColumn, StyledTitleSmall } from 'shared/styles';
 import { ALLOWED_AUDIO_FILE_TYPES } from 'shared/consts';
 
 import { StyledIconCenter, StyledMenuItem, StyledMenuList } from '../Extensions.styles';
 import { SourceLinkModal } from '../../SourceLinkModal';
 import { useUploadMethods } from '../Extensions.hooks';
-import { InsertContentExtensionProps, InsertHandlerProps } from '../Extensions.types';
+import { InsertContentExtensionProps } from '../Extensions.types';
 
 const DropdownToolbar = MdEditor.DropdownToolbar;
 
 export const AudioUploadExtension = ({ onInsert }: InsertContentExtensionProps) => {
   const { t } = useTranslation('app');
-  const insertHandler = ({ values }: InsertHandlerProps) => {
+  const insertHandler = ({ label, address }: SourceLinkModalForm) => {
     const generator: InsertContentGenerator = () => ({
-      targetValue: `<figure><figcaption>${values?.label || ''}:</figcaption><audio controls src="${
-        values?.address || ''
-      }"></audio></figure>`,
+      targetValue: `<figure><figcaption>${label}:</figcaption><audio controls src="${address}"></audio></figure>`,
       select: false,
       deviationStart: 0,
       deviationEnd: 0,
@@ -57,6 +55,7 @@ export const AudioUploadExtension = ({ onInsert }: InsertContentExtensionProps) 
                   <StyledTitleSmall onClick={onUploadClick}>
                     {t('uploadAudio')}
                     <input
+                      key={inputRef.current?.toString()}
                       ref={inputRef}
                       hidden
                       accept={ALLOWED_AUDIO_FILE_TYPES}
