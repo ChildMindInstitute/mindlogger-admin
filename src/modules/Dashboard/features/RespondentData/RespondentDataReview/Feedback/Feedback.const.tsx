c266f1f0fa798cd15e18eca850fdaa997ee10400
nmialik <nmialik@scnsoft.com>
2023-06-01 22:05:13 +0200

    M2-2157: Dataviz - Notes - Fix api calls

--- src/modules/Dashboard/features/RespondentData/RespondentDataReview/Feedback/Feedback.const.tsx
+++ src/modules/Dashboard/features/RespondentData/RespondentDataReview/Feedback/Feedback.const.tsx
@@ -1,11 +1,12 @@
+import { Activity } from '../RespondentDataReview.types';
 import { FeedbackAssessment } from './FeedbackAssessment';
 import { FeedbackNotes } from './FeedbackNotes';
 import { FeedbackReviewed } from './FeedbackReviewed';
 
-export const tabs = [
+export const getTabs = (selectedActivity: Activity) => [
   {
     labelKey: 'notes',
-    content: <FeedbackNotes />,
+    content: <FeedbackNotes activity={selectedActivity} />,
   },
   {
     labelKey: 'assessment',
