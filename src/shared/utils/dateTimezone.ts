2fce9cd3a99ce335ecfb01b9443a19f4a50411f7
IhorNesterenko99 <inesterenko@scnsoft.com>
2023-06-14 12:16:41 +0300

    fix: M2-2265 Calendar: Scheduled Weekly and Monthly events are shifting in the "UTC -" timezones

---
+++ src/shared/utils/dateTimezone.ts
@@ -0,0 +1,24 @@
+const MINUTES_TO_MILLISECONDS_MULTIPLIER = 60 * 1000;
+
+const getNormalizeTimezoneData = (dateString: string) => {
+  const dateTime = new Date(dateString).getTime();
+  const userDateOffsetMilliseconds =
+    new Date().getTimezoneOffset() * MINUTES_TO_MILLISECONDS_MULTIPLIER;
+
+  return {
+    dateTime,
+    userDateOffsetMilliseconds,
+  };
+};
+
+export const getDateInUserTimezone = (dateString: string) => {
+  const { dateTime, userDateOffsetMilliseconds } = getNormalizeTimezoneData(dateString);
+
+  return new Date(dateTime - userDateOffsetMilliseconds);
+};
+
+export const getNormalizedTimezoneDate = (dateString: string) => {
+  const { dateTime, userDateOffsetMilliseconds } = getNormalizeTimezoneData(dateString);
+
+  return new Date(dateTime + userDateOffsetMilliseconds);
+};
