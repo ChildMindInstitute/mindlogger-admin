794b2f8ccc9c818d407e3dfae3424636177311ea
nmialik <nmialik@scnsoft.com>
2023-06-16 13:37:23 +0200

    fix PR

--- src/modules/Dashboard/features/RespondentData/RespondentDataSummary/Report/Charts/Charts.const.ts
+++ src/modules/Dashboard/features/RespondentData/RespondentDataSummary/Report/Charts/Charts.const.ts
@@ -8,7 +8,6 @@ export const locales = {
 export const BAR_CHART_LABEL_WIDTH_Y = 56;
 export const LABEL_WIDTH_Y = 180;
 export const MAX_LABEL_CHARS_Y = 24;
-export const MIN_LABEL_CHARS_Y = 5;
 
 export const MS_PER_HOUR = 1000 * 60 * 60;
 export const MS_PER_DAY = MS_PER_HOUR * 24;
