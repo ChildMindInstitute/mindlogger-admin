1d955c2e33c0fc0f7192ad9e47d99e13d42b9978
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2023-06-20 14:50:34 +0300

    refined loop processing

--- src/shared/utils/exportData/prepareData.ts
+++ src/shared/utils/exportData/prepareData.ts
@@ -19,30 +19,36 @@ export const prepareData = (
   getDecryptedAnswers: (data: ExtendedExportAnswer) => DecryptedActivityData,
 ) => {
   const parsedAnswers = getParsedAnswers(data, getDecryptedAnswers);
-  const reportData = [];
-  const activityJourneyData = [];
-  const flattenAnswers = parsedAnswers
-    .flatMap((item) => item.decryptedAnswers)
-    ?.filter((item) => !item.activityItem?.config?.skippableItem);
-  const flattenEvents = parsedAnswers.flatMap((item) => {
-    const decryptedAnswersObject = getDecryptedAnswersObject(item.decryptedAnswers);
 
-    return item.decryptedEvents.map((event) => ({
-      ...event,
-      ...decryptedAnswersObject[event.screen],
-    }));
-  });
+  return parsedAnswers.reduce(
+    (acc, data) => {
+      const answers = data.decryptedAnswers.reduce((filteredAcc, item) => {
+        if (item.activityItem?.config?.skippableItem) return filteredAcc;
 
-  for (const item of flattenAnswers) {
-    reportData.push(getReportCSVObject(item));
-  }
+        return filteredAcc.concat(getReportCSVObject(item));
+      }, [] as ReturnType<typeof getReportCSVObject>[]);
+      const reportData = acc.reportData.concat(...answers);
 
-  for (const event of flattenEvents) {
-    activityJourneyData.push(getJourneyCSVObject(event));
-  }
+      const decryptedAnswersObject = getDecryptedAnswersObject(data.decryptedAnswers);
+      const events = data.decryptedEvents.map((event) =>
+        getJourneyCSVObject({
+          ...event,
+          ...decryptedAnswersObject[event.screen],
+        }),
+      );
+      const activityJourneyData = acc.activityJourneyData.concat(...events);
 
-  return {
-    reportData,
-    activityJourneyData,
-  };
+      return {
+        reportData,
+        activityJourneyData,
+      };
+    },
+    {
+      reportData: [],
+      activityJourneyData: [],
+    } as {
+      reportData: ReturnType<typeof getReportCSVObject>[];
+      activityJourneyData: ReturnType<typeof getJourneyCSVObject>[];
+    },
+  );
 };
