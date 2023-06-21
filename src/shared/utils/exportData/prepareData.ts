f9aa4cdf2a2e86c81f1f56d597885d2f5caa9c99
Aleksandr Eremin <aleksandr.i.eremin@yandex.ru>
2023-06-21 10:37:43 +0300

    Feature/m2 2007 parsing activity user journey for export data (#542)
    * added activity user journey csv
    
    * added helpers
    
    * refined loop processing

--- src/shared/utils/exportData/prepareData.ts
+++ src/shared/utils/exportData/prepareData.ts
@@ -1,30 +1,54 @@
 import {
   DecryptedActivityData,
+  DecryptedAnswerData,
   ExportActivity,
   ExportAnswer,
   ExtendedExportAnswer,
 } from 'shared/types';
+import { getObjectFromList } from 'shared/utils/builderHelpers';
 
 import { getParsedAnswers } from '../getParsedAnswers';
 import { getReportCSVObject } from './getReportCSVObject';
+import { getJourneyCSVObject } from './getJourneyCSVObject';
+
+const getDecryptedAnswersObject = (decryptedAnswers: DecryptedAnswerData[]) =>
+  getObjectFromList(decryptedAnswers, (item) => `${item.activityId}/${item.activityItem.id}`);
 
 export const prepareData = (
   data: { activities: ExportActivity[]; answers: ExportAnswer[] },
   getDecryptedAnswers: (data: ExtendedExportAnswer) => DecryptedActivityData,
 ) => {
   const parsedAnswers = getParsedAnswers(data, getDecryptedAnswers);
-  const reportData = [];
-  const activityJourneyData = [];
-  const flattenAnswers = parsedAnswers
-    .flatMap((item) => item.decryptedAnswers)
-    ?.filter((item) => !item.activityItem?.config?.skippableItem);
-  for (const item of flattenAnswers) {
-    reportData.push(getReportCSVObject(item));
-    activityJourneyData.push({});
-  }
 
-  return {
-    reportData,
-    activityJourneyData,
-  };
+  return parsedAnswers.reduce(
+    (acc, data) => {
+      const answers = data.decryptedAnswers.reduce((filteredAcc, item) => {
+        if (item.activityItem?.config?.skippableItem) return filteredAcc;
+
+        return filteredAcc.concat(getReportCSVObject(item));
+      }, [] as ReturnType<typeof getReportCSVObject>[]);
+      const reportData = acc.reportData.concat(...answers);
+
+      const decryptedAnswersObject = getDecryptedAnswersObject(data.decryptedAnswers);
+      const events = data.decryptedEvents.map((event) =>
+        getJourneyCSVObject({
+          ...event,
+          ...decryptedAnswersObject[event.screen],
+        }),
+      );
+      const activityJourneyData = acc.activityJourneyData.concat(...events);
+
+      return {
+        reportData,
+        activityJourneyData,
+      };
+    },
+    {
+      reportData: [],
+      activityJourneyData: [],
+    } as {
+      reportData: ReturnType<typeof getReportCSVObject>[];
+      activityJourneyData: ReturnType<typeof getJourneyCSVObject>[];
+    },
+  );
 };
