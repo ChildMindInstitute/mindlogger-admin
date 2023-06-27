d28da65411e2d4d3366ef67348848cfa5aa2d5ae
wildkiomi <whisann.lie14@gmail.com>
2023-06-27 17:53:39 +0400

    Type fix

--- src/shared/utils/exportData/getMediaObject.ts
+++ src/shared/utils/exportData/getMediaObject.ts
@@ -1,6 +1,8 @@
-import { DecryptedAnswerData } from 'shared/types';
+import { DecryptedAnswerData, ExtendedExportAnswerWithoutEncryption } from 'shared/types';
 
 import { getAnswerValue } from '../getAnswerValue';
 
-export const getMediaObject = ({ answer }: DecryptedAnswerData) =>
+export const getMediaObject = ({
+  answer,
+}: DecryptedAnswerData<ExtendedExportAnswerWithoutEncryption>) =>
   (getAnswerValue(answer) as string).split('.net/')[1];
