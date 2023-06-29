fa6782540f38101e46fdcbcffa9ac5ee32724806
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2023-06-29 09:12:02 +0300

    added validations on ActivityAbout page

---
+++ src/modules/Builder/features/ActivityAbout/ActivityAbout.hooks.ts
@@ -0,0 +1,28 @@
+import { ItemFormValues } from 'modules/Builder/types';
+import { getObjectFromList, getTextBetweenBrackets } from 'shared/utils';
+import { useEffect } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { useCurrentActivity } from 'modules/Builder/hooks';
+
+const checkIfItemsHaveVariables = (items: ItemFormValues[]) => {
+  const itemsObject = getObjectFromList(items, ({ name }) => name);
+
+  return items.some(({ question }) => {
+    const variableNames = getTextBetweenBrackets(question);
+
+    return variableNames.some((variable) => !!itemsObject[variable]);
+  });
+};
+export const useCheckIfItemsHaveVariables = () => {
+  const { setValue, watch } = useFormContext();
+  const { fieldName } = useCurrentActivity();
+  const activityItems = watch(`${fieldName}.items`) ?? [];
+  const hasVariableAmongItems = checkIfItemsHaveVariables(activityItems);
+
+  useEffect(() => {
+    if (!hasVariableAmongItems) return;
+    setValue(`${fieldName}.isSkippable`, false);
+  }, [hasVariableAmongItems]);
+
+  return hasVariableAmongItems;
+};
