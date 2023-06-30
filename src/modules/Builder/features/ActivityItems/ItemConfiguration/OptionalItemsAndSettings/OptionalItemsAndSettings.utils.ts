9a6eff36538073294cdda0b02094646bd9dbe42e
muhurov <muhurov@gmail.com>
2023-06-30 12:33:32 +0300

    Fix: M2-2149 - Builder - Item Flow: saved Item Flow conditions are disappeared if new Applet saved and user stayed in the Builder

--- src/modules/Builder/features/ActivityItems/ItemConfiguration/OptionalItemsAndSettings/OptionalItemsAndSettings.utils.ts
+++ src/modules/Builder/features/ActivityItems/ItemConfiguration/OptionalItemsAndSettings/OptionalItemsAndSettings.utils.ts
@@ -1,4 +1,6 @@
 import { pluck } from 'shared/utils';
 
+import { DEFAULT_OPTION_VALUE } from './OptionalItemsAndSettings.const';
+
 export const getOptionValue = (options: unknown[]) =>
-  Math.max(...(pluck(options, 'value') || 0)) + 1;
+  options?.length ? Math.max(...pluck(options, 'value')) + 1 : DEFAULT_OPTION_VALUE;
