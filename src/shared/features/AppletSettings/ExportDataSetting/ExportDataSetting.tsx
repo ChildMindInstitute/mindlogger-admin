226006a01ce03a0f6fc10f13823e024fb1cf8bcc
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2023-07-10 18:34:36 +0300

    added modal while data exporting in applet settings

--- src/shared/features/AppletSettings/ExportDataSetting/ExportDataSetting.tsx
+++ src/shared/features/AppletSettings/ExportDataSetting/ExportDataSetting.tsx
@@ -3,12 +3,13 @@ import { useTranslation } from 'react-i18next';
 import { Box } from '@mui/material';
 
 import { AppletPasswordPopup } from 'modules/Dashboard/features/Applet';
-import { Svg } from 'shared/components';
+import { Modal, Svg } from 'shared/components';
 import { applet } from 'shared/state';
 import { getExportDataApi } from 'api';
 import { useAsync } from 'shared/hooks';
 import { useDecryptedActivityData } from 'modules/Dashboard/hooks';
 import { exportDataSucceed } from 'shared/utils';
+import { StyledBodyLarge, StyledLinearProgress, StyledModalWrapper, theme } from 'shared/styles';
 
 import {
   StyledAppletSettingsButton,
@@ -20,7 +21,7 @@ export const ExportDataSetting = () => {
   const { t } = useTranslation('app');
   const { result: appletData } = applet.useAppletData() ?? {};
   const getDecryptedAnswers = useDecryptedActivityData();
-
+  const [dataIsExporting, setDataIsExporting] = useState(false);
   const [passwordModalVisible, setPasswordModalVisible] = useState(false);
 
   const { execute } = useAsync(
@@ -28,16 +29,21 @@ export const ExportDataSetting = () => {
     exportDataSucceed({
       getDecryptedAnswers,
       callback: () => {
-        setPasswordModalVisible(false);
+        setDataIsExporting(false);
       },
     }),
     console.warn,
   );
 
   const handleDataExportHandler = () => {
-    if (appletData?.id) {
-      execute({ appletId: appletData.id });
-    }
+    if (!appletData?.id) return;
+
+    setPasswordModalVisible(false);
+    setDataIsExporting(true);
+    execute({ appletId: appletData.id });
+  };
+  const handleExportClose = () => {
+    setDataIsExporting(false);
   };
 
   return (
@@ -61,6 +67,21 @@ export const ExportDataSetting = () => {
           appletId={appletData?.id ?? ''}
           encryption={appletData?.encryption}
         />
+      )}
+      {dataIsExporting && (
+        <Modal
+          open={dataIsExporting}
+          onClose={handleExportClose}
+          title={t('dataExport')}
+          buttonText={''}
+        >
+          <StyledModalWrapper>
+            <StyledBodyLarge sx={{ margin: theme.spacing(-2.4, 0, 2.4) }}>
+              {t('waitForRespondentDataDownload')}
+            </StyledBodyLarge>
+            <StyledLinearProgress />
+          </StyledModalWrapper>
+        </Modal>
       )}
     </>
   );
