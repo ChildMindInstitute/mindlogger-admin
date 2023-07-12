2c5b46b0c372fa0886316f43bfbdbfb020da2480
nmialik <115553085+nmialik@users.noreply.github.com>
2023-07-12 14:01:57 +0300

    M2-2602: Message does not require response (#638)

--- src/modules/Dashboard/features/RespondentData/RespondentData.consts.ts
+++ src/modules/Dashboard/features/RespondentData/RespondentData.consts.ts
@@ -14,6 +14,7 @@ export const UNSUPPORTED_ITEMS = [
   ItemResponseType.SingleSelectionPerRow,
   ItemResponseType.SliderRows,
   ItemResponseType.NumberSelection,
+  ItemResponseType.Message,
   ItemResponseType.Flanker,
   ItemResponseType.StabilityTracker,
   ItemResponseType.TouchPractice,
