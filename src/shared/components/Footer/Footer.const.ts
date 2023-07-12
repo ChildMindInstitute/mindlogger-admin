448742492e798da5880aa9e0f08b573ab60c6889
Hanna <35298264+wildkiomi@users.noreply.github.com>
2023-07-12 14:09:44 +0400

    M2-2560 Credits link (#636)

--- src/shared/components/Footer/Footer.const.ts
+++ src/shared/components/Footer/Footer.const.ts
@@ -3,3 +3,4 @@ export const MATTER_LAB_LINK = 'https://matter.childmind.org';
 export const PRIVACY_LINK = 'https://mindlogger.org/faq.html#are-my-data-secure';
 export const TERMS_LINK = 'https://mindlogger.org/terms';
 export const ABOUT_LINK = 'https://mindlogger.org';
+export const CREDITS_LINK = 'https://mindlogger.org';
