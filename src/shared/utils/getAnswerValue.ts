00747a590e0b158ae1bd308ca2bd3e42076579c3
muhurov <muhurov@gmail.com>
2023-07-12 15:22:27 +0300

    Revert "Fix: M2-2544 - Admin - Data export: Single selection, multiple selection option value is started with 1 instead of 0 as on prod"
    This reverts commit bf72d615207adb01af8a2cff6214b0ea87fb44ae.

--- src/shared/utils/getAnswerValue.ts
+++ src/shared/utils/getAnswerValue.ts
@@ -2,8 +2,8 @@ import { AnswerDTO } from 'shared/types';
 
 export const getAnswerValue = (answerValue: AnswerDTO) => {
   if (typeof answerValue === 'object') {
-    return answerValue?.value ?? '';
+    return answerValue?.value || '';
   }
 
-  return answerValue ?? '';
+  return answerValue || '';
 };
