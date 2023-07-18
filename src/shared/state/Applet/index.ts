1cd7a13bed85e47c5937ef8373073984fef5a5c8
muhurov <muhurov@gmail.com>
2023-07-18 18:28:40 +0300

    Fix: M2-2666 - Builder - Server configurations are empty after saving valid data, moving to the other builder tab and then moving back (#665)

--- src/shared/state/Applet/index.ts
+++ src/shared/state/Applet/index.ts
@@ -7,14 +7,14 @@ import * as thunk from './Applet.thunk';
 import { state as initialState } from './Applet.state';
 import { extraReducers } from './Applet.reducer';
 import { AppletSchema } from './Applet.schema';
-import { removeApplet } from './Applet.utils';
+import { removeApplet, updateReportConfig } from './Applet.utils';
 
 export * from './Applet.schema';
 
 const slice = createSlice({
   name: 'applet',
   initialState,
-  reducers: { removeApplet },
+  reducers: { removeApplet, updateReportConfig },
   extraReducers,
 });
 
