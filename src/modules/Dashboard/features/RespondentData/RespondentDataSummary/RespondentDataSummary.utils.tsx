222a10776d984c4a8a7e2e3d2e0b832515269695
nmialik <nmialik@scnsoft.com>
2023-07-25 19:09:50 +0200

    M2-2538: No available Data for this Activity yet

---
+++ src/modules/Dashboard/features/RespondentData/RespondentDataSummary/RespondentDataSummary.utils.tsx
@@ -0,0 +1,39 @@
+import i18n from 'i18n';
+import { DatavizActivity } from 'api';
+import { Svg } from 'shared/components';
+import { StyledTitleLarge, theme, variables } from 'shared/styles';
+
+export const getEmptyState = (selectedActivity?: DatavizActivity) => {
+  const { t } = i18n;
+
+  if (!selectedActivity) {
+    return (
+      <>
+        <Svg id="data" width="80" height="80" />
+        <StyledTitleLarge sx={{ mt: theme.spacing(1.6) }} color={variables.palette.outline}>
+          {t('selectTheActivityToReview')}
+        </StyledTitleLarge>
+      </>
+    );
+  }
+  if (!selectedActivity.hasAnswer) {
+    return (
+      <>
+        <Svg id="chart" width="80" height="80" />
+        <StyledTitleLarge sx={{ mt: theme.spacing(1.6) }} color={variables.palette.outline}>
+          {t('noDataForActivity')}
+        </StyledTitleLarge>
+      </>
+    );
+  }
+  if (!selectedActivity.isPerformanceTask) {
+    return (
+      <>
+        <Svg id="confused" width="80" height="80" />
+        <StyledTitleLarge sx={{ mt: theme.spacing(1.6) }} color={variables.palette.outline}>
+          {t('datavizNotSupportedForPerformanceTasks')}
+        </StyledTitleLarge>
+      </>
+    );
+  }
+};
