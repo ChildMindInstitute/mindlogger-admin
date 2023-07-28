c191c775f94db3bb5b0c6ec3feb7b2a49d99a5da
nmialik <nmialik@scnsoft.com>
2023-07-28 16:07:36 +0200

    M2-2692, M2-2693: Hide 'Builder' Icon and 'New Applet' icon from navigation panel, Add 'Editing' chip to breadcrumbs when User creates/edits Applet content

--- src/shared/hooks/useBreadcrumbs.tsx
+++ src/shared/hooks/useBreadcrumbs.tsx
@@ -80,6 +80,7 @@ export const useBreadcrumbs = (restCrumbs?: Breadcrumb[]) => {
       newBreadcrumbs.push({
         icon: appletData?.image || '',
         label: appletLabel,
+        chip: t('editing'),
         navPath: generatePath(isDashboard ? page.appletRespondents : page.builderApplet, {
           appletId,
         }),
