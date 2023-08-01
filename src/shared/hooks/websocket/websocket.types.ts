5f9dd6d8ebc43cedb2700ab28494a3949ef8a841
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2023-08-01 19:36:46 +0300

    synced types for websocket response

--- src/shared/hooks/websocket/websocket.types.ts
+++ src/shared/hooks/websocket/websocket.types.ts
@@ -1,7 +1,10 @@
+import { Encryption } from 'shared/utils';
+
 export type WebsocketAlertType = {
   id: string;
   applet_id: string;
   applet_name: string;
+  applet_image?: string;
   version: string;
   secret_id: string;
   activity_id: string;
@@ -9,4 +12,7 @@ export type WebsocketAlertType = {
   message: string;
   created_at: string;
   answer_id: string;
+  respondent_id?: string;
+  workspace_name?: string;
+  encryption?: Encryption;
 };
