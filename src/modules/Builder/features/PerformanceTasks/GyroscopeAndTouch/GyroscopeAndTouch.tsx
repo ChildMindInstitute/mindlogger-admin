61784c69a9c120630e6e9439d7ebf366edb394a4
muhurov <muhurov@gmail.com>
2023-08-02 13:10:25 +0300

    Fix: M2-2807 - Builder - CST, Flanker: Updating the Overview instructions with the markdown styles depends and relates to the Practice/Test instructions

--- src/modules/Builder/features/PerformanceTasks/GyroscopeAndTouch/GyroscopeAndTouch.tsx
+++ src/modules/Builder/features/PerformanceTasks/GyroscopeAndTouch/GyroscopeAndTouch.tsx
@@ -28,16 +28,19 @@ export const GyroscopeAndTouch = ({ type }: GyroscopeAndTouchProps) => {
           name={`${fieldName}.items.0.question`}
           title={t('gyroscopeAndTouchInstructions.overview.title')}
           description={t('gyroscopeAndTouchDesc.overview')}
+          instructionId="instruction-0"
         />
         <Instruction
           name={`${fieldName}.items.1.question`}
           title={t('gyroscopeAndTouchInstructions.practice.title')}
           description={t('gyroscopeAndTouchDesc.practice')}
+          instructionId="instruction-1"
         />
         <Instruction
           name={`${fieldName}.items.3.question`}
           title={t('gyroscopeAndTouchInstructions.test.title')}
           description={t('gyroscopeAndTouchDesc.test')}
+          instructionId="instruction-3"
         />
       </StyledPerformanceTaskBody>
     </>
