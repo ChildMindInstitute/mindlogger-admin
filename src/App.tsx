512dd564e0ae44a170171947a4e2f0393648f403
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2023-08-03 08:24:43 +0300

    moved RootComponent.tsx to routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,9 @@
-import { Suspense, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 
-import { store, useAppDispatch } from 'redux/store';
-import theme from 'shared/styles/theme';
-import { AppRoutes } from 'routes';
-import { Spinner } from 'shared/components';
-import { useWebsocket } from 'shared/hooks';
-import { alerts } from 'shared/state';
-import { auth } from 'modules/Auth';
-import { DEFAULT_ROWS_PER_PAGE } from 'shared/state/Alerts/Alerts.const';
-
-const RootComponent = () => {
-  const dispatch = useAppDispatch();
-  const isAuthorized = auth.useAuthorized();
-
-  useWebsocket();
-
-  useEffect(() => {
-    if (!isAuthorized) return;
-
-    dispatch(alerts.thunk.getAlerts({ limit: DEFAULT_ROWS_PER_PAGE }));
-  }, [isAuthorized]);
-
-  return (
-    <>
-      <CssBaseline />
-      <Suspense fallback={<Spinner />}>
-        <AppRoutes />
-      </Suspense>
-    </>
-  );
-};
+import { store } from 'redux/store';
+import { theme } from 'shared/styles';
+import { RootComponent } from 'routes/RootComponent';
 
 const App = () => (
   <Provider store={store}>
---
+++ src/routes/RootComponent/RootComponent.tsx
@@ -0,0 +1,32 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import { Suspense, useEffect } from 'react';
+
+import { useAppDispatch } from 'redux/store';
+import { auth } from 'modules/Auth';
+import { useWebsocket } from 'shared/hooks';
+import { alerts } from 'shared/state';
+import { DEFAULT_ROWS_PER_PAGE } from 'shared/state/Alerts/Alerts.const';
+import { Spinner } from 'shared/components';
+import { AppRoutes } from 'routes/index';
+
+export const RootComponent = () => {
+  const dispatch = useAppDispatch();
+  const isAuthorized = auth.useAuthorized();
+
+  useWebsocket();
+
+  useEffect(() => {
+    if (!isAuthorized) return;
+
+    dispatch(alerts.thunk.getAlerts({ limit: DEFAULT_ROWS_PER_PAGE }));
+  }, [isAuthorized]);
+
+  return (
+    <>
+      <CssBaseline />
+      <Suspense fallback={<Spinner />}>
+        <AppRoutes />
+      </Suspense>
+    </>
+  );
+};
