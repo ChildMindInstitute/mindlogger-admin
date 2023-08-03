f8dbac3489e2fc14a0a53adc5f59644fc06c7e28
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2023-08-03 13:47:47 +0300

    removed redundant RootComponent.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
+import { Suspense } from 'react';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import { store } from 'redux/store';
 import { theme } from 'shared/styles';
-import { RootComponent } from 'routes/RootComponent';
+import { Spinner } from 'shared/components';
+import { AppRoutes } from 'routes';
 
 const App = () => (
   <Provider store={store}>
     <ThemeProvider theme={theme}>
-      <RootComponent />
+      <CssBaseline />
+      <Suspense fallback={<Spinner />}>
+        <AppRoutes />
+      </Suspense>
     </ThemeProvider>
   </Provider>
 );
