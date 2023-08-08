3f81ca7a2748a026d8d4fdba876f811526012f7e
muhurov <muhurov@gmail.com>
2023-08-08 19:08:08 +0300

    Fix: M2-643 - Builder - Applet About Page: Loader doesn’t appear when user add an image (#752)

--- src/shared/components/Spinner/Spinner.tsx
+++ src/shared/components/Spinner/Spinner.tsx
@@ -1,9 +1,10 @@
 import { CircularProgress } from '@mui/material';
 
 import { StyledSpinner } from './Spinner.styles';
+import { SpinnerProps, SpinnerUiType } from './Spinner.types';
 
-export const Spinner = () => (
+export const Spinner = ({ uiType = SpinnerUiType.Primary }: SpinnerProps) => (
   <StyledSpinner className="spinner-container">
-    <CircularProgress size={60} />
+    <CircularProgress size={uiType === SpinnerUiType.Primary ? 60 : 50} />
   </StyledSpinner>
 );
---
+++ src/shared/components/Spinner/Spinner.types.ts
@@ -0,0 +1,8 @@
+export enum SpinnerUiType {
+  Primary = 'primary',
+  Secondary = 'secondary',
+}
+
+export type SpinnerProps = {
+  uiType?: SpinnerUiType;
+};
