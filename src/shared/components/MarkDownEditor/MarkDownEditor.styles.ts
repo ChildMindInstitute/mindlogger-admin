1655227e090639e5523d274f609b5ea7e10ee21d
IhorNesterenko99 <116339462+IhorNesterenko99@users.noreply.github.com>
2023-08-09 10:16:41 +0300

    fix: M2-2679 H1 and H2 are the same size in the markdown preview (#753)

--- src/shared/components/MarkDownEditor/MarkDownEditor.styles.ts
+++ src/shared/components/MarkDownEditor/MarkDownEditor.styles.ts
@@ -1,10 +1,19 @@
 import MdEditor from 'md-editor-rt';
 import { styled } from '@mui/material';
 
-import { theme } from 'shared/styles';
+import { theme, variables } from 'shared/styles';
 
 export const MarkDownEditor = styled(MdEditor)`
   &.md-editor-fullscreen {
     z-index: ${theme.zIndex.fab};
+  }
+
+  .md-editor-preview-wrapper {
+    h1 {
+      font-size: ${variables.font.size.xxl};
+    }
+    h2 {
+      font-size: ${variables.font.size.lrg};
+    }
   }
 `;
