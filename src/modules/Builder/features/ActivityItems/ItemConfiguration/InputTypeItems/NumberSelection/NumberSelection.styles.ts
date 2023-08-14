ca7a25540114a222fe562ed9a07b55036d04d178
muhurov <muhurov@gmail.com>
2023-08-14 17:28:34 +0300

    Fix: M2-2316 - Builder - Number selection: The validation message "Min value should be less than Max Value" has no margin bottom

--- src/modules/Builder/features/ActivityItems/ItemConfiguration/InputTypeItems/NumberSelection/NumberSelection.styles.ts
+++ src/modules/Builder/features/ActivityItems/ItemConfiguration/InputTypeItems/NumberSelection/NumberSelection.styles.ts
@@ -4,4 +4,8 @@ import { StyledFlexTopCenter } from 'shared/styles';
 
 export const StyledInputWrapper = styled(StyledFlexTopCenter)`
   width: 50%;
+
+  .MuiFormHelperText-root.Mui-error {
+    position: static;
+  }
 `;
