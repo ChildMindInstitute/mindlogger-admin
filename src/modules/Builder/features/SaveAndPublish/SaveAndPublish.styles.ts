2631977f4bc0816c5bd55c719064e03c81b61879
muhurov <muhurov@gmail.com>
2023-08-14 15:07:16 +0300

    Fix: M2-1967 - Builder - The "Save & Publish" button stays in hovered state until the page is refreshed after clicking on it at least once (#776)

--- src/modules/Builder/features/SaveAndPublish/SaveAndPublish.styles.ts
+++ src/modules/Builder/features/SaveAndPublish/SaveAndPublish.styles.ts
@@ -1,8 +1,13 @@
-import { Button } from '@mui/material';
-import { styled } from '@mui/system';
+import { Button, styled } from '@mui/material';
+
+import { variables } from 'shared/styles';
 
 export const StyledButton = styled(Button)`
   position: absolute;
   right: 2.4rem;
   top: 0.5rem;
+
+  &&:focus:not(:hover) {
+    background-color: ${variables.palette.primary};
+  }
 `;
