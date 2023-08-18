b6d8d7c16a567e4ad0e973eb2f8b9fe8c00ae1c7
DimitrySakalouski <dsakalouski@scnsoft.com>
2023-08-18 16:03:19 +0300

    fix(mixpanel): init tracker

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { Mixpanel, svgBuilder } from 'shared/utils';
-
+// todo: add after Mixpanel token is received - M2-3000
+// import { Mixpanel } from 'shared/utils';
+import svgBuilder from 'shared/utils/svgBuilder';
 import { svgSprite } from 'svgSprite';
+
 import App from './App';
 import './i18n';
 import reportWebVitals from './reportWebVitals';
@@ -12,7 +14,8 @@ document.body.appendChild(svgBuilder(svgSprite));
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-Mixpanel.init();
+// todo: add after Mixpanel token is received - M2-3000
+// Mixpanel.init();
 
 root.render(
   <React.StrictMode>
