4c7d751db70d32da9a548538fa8b573d148af9f0
IhorNesterenko99 <116339462+IhorNesterenko99@users.noreply.github.com>
2023-08-23 13:58:21 +0300

    feat: M2-3086 Add auth/logout API call on logout (#826)

--- src/shared/hooks/useLogout.ts
+++ src/shared/hooks/useLogout.ts
@@ -3,13 +3,15 @@ import { useNavigate } from 'react-router-dom';
 import { page } from 'resources';
 import { useAppDispatch } from 'redux/store';
 import { auth, workspaces } from 'redux/modules';
+import { logOutApi } from 'modules/Auth';
 
 export const useLogout = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
   //TODO: rewrite to reset the global state data besides the data needed in lock form
-  return () => {
+  return async () => {
+    await logOutApi();
     dispatch(workspaces.actions.setCurrentWorkspace(null));
     dispatch(auth.actions.resetAuthorization());
     navigate(page.login);
