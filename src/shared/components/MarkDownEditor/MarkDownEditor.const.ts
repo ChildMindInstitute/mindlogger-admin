2bc75355790667e79508a3fb978b7742d664ee49
IhorNesterenko99 <116339462+IhorNesterenko99@users.noreply.github.com>
2023-09-05 12:10:38 +0300

    fix: M2-3060 Preview for the video files in mp4 format added to Displayed Content section aren't displayed. (#881)

--- src/shared/components/MarkDownEditor/MarkDownEditor.const.ts
+++ src/shared/components/MarkDownEditor/MarkDownEditor.const.ts
@@ -3,7 +3,10 @@ import { VALID_VIDEO_FILE_TYPES } from 'shared/consts';
 export const LANGUAGE_BY_DEFAULT = 'en';
 export const THRESHOLD_SIZE = 75;
 const VALID_VIDEO_FORMATS_ARRAY = VALID_VIDEO_FILE_TYPES.map((format) => format.slice(1));
-export const VALID_VIDEO_FORMATS_REGEX = new RegExp(`\\.(${VALID_VIDEO_FORMATS_ARRAY.join('|')})$`);
+export const VALID_VIDEO_FORMATS_REGEX = new RegExp(
+  `\\.(${VALID_VIDEO_FORMATS_ARRAY.join('|')})$`,
+  'i',
+);
 
 export enum VideoSources {
   Youtube = 'youtube.com',
