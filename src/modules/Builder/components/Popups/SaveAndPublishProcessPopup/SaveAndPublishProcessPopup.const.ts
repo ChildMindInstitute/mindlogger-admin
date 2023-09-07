a90e8d107ae89a0226b132646f33f3b231043d88
muhurov <muhurov@gmail.com>
2023-09-07 15:28:38 +0300

    Feat: M2-3369 - Admin Panel - Add data-testid to "Save&Publish" modal

---
+++ src/modules/Builder/components/Popups/SaveAndPublishProcessPopup/SaveAndPublishProcessPopup.const.ts
@@ -0,0 +1,12 @@
+import { SaveAndPublishSteps } from './SaveAndPublishProcessPopup.types';
+
+export const saveAndPublishProcessTestIds = {
+  [SaveAndPublishSteps.AtLeastOneActivity]: '-empty-activities',
+  [SaveAndPublishSteps.AtLeastOneItem]: '-empty-activity-items',
+  [SaveAndPublishSteps.EmptyRequiredFields]: '-empty-required-fields',
+  [SaveAndPublishSteps.ErrorsInFields]: '-errors-in-fields',
+  [SaveAndPublishSteps.BeingCreated]: '-loading',
+  [SaveAndPublishSteps.Success]: '-success',
+  [SaveAndPublishSteps.Failed]: '-failed',
+  [SaveAndPublishSteps.ReportConfigSave]: '-report-config-save-changes',
+};
