6501bd68858bcbf7b656fd7195d3e61e49f755e4
nmialik <nmialik@scnsoft.com>
2023-09-18 13:29:50 +0200

    M2-3069: Update tooltip styling to match designs

--- src/shared/components/Tooltip/Tooltip.styles.tsx
+++ src/shared/components/Tooltip/Tooltip.styles.tsx
@@ -1,14 +1,19 @@
 import { styled, Tooltip, TooltipProps, tooltipClasses } from '@mui/material';
 
-import { variables } from 'shared/styles/variables';
+import { theme, variables } from 'shared/styles';
 
 export const StyledTooltip = styled(({ className, children, ...props }: TooltipProps) => (
   <Tooltip {...props} classes={{ popper: className }} children={children} />
 ))(() => ({
   [`& .${tooltipClasses.tooltip}`]: {
     backgroundColor: variables.palette.inverse_surface,
+    color: variables.palette.inverse_on_surface,
     textAlign: 'left',
-    fontSize: variables.font.size.sm,
-    maxWidth: '24rem',
+    fontSize: variables.font.size.md,
+    maxWidth: theme.spacing(24),
+    lineHeight: variables.font.lineHeight.md,
+    padding: theme.spacing(0.4, 0.8),
+    fontWeight: variables.font.weight.regular,
+    letterSpacing: variables.font.letterSpacing.sm,
   },
 }));
