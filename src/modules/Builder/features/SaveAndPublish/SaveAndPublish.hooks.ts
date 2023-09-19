f6b70ea8509e7a6ecf75597ad6a92e0ab93121a8
muhurov <muhurov@gmail.com>
2023-09-19 14:05:02 +0300

    Fix: M2-3499 - Builder - Cannot duplicate activity (#956)

--- src/modules/Builder/features/SaveAndPublish/SaveAndPublish.hooks.ts
+++ src/modules/Builder/features/SaveAndPublish/SaveAndPublish.hooks.ts
@@ -34,6 +34,7 @@ import {
   removeAppletExtraFields,
   removeActivityExtraFields,
   removeActivityFlowExtraFields,
+  removeActivityFlowItemExtraFields,
   remapSubscaleSettings,
   getActivityItems,
   getScoresAndReports,
@@ -73,7 +74,10 @@ export const useAppletData = () => {
           ({
             ...flow,
             description: getDictionaryObject(flow.description),
-            items: flow.items?.map(({ key, ...item }) => item),
+            items: flow.items?.map(({ key, ...item }) => ({
+              ...item,
+              ...removeActivityFlowItemExtraFields(),
+            })),
             ...removeActivityFlowExtraFields(),
           } as ActivityFlow),
       ),
