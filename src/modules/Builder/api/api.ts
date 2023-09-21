4e323340b723e379a74a52f51b9dad917acd51e9
Aleksandr Eremin <aleksandr.i.eremin@yandex.ru>
2023-09-21 18:29:53 +0300

    fixed api client imports (#978)

--- src/modules/Builder/api/api.ts
+++ src/modules/Builder/api/api.ts
@@ -1,4 +1,4 @@
-import { authApiClient } from 'shared/api/api';
+import { authApiClient } from 'shared/api/api.client';
 import { Response } from 'shared/api/api.types';
 
 import { GetThemesParams, Theme } from './api.types';
