0b81e74f453be5408bea49db0d329d77f7d44397
muhurov <muhurov@gmail.com>
2023-09-26 14:34:51 +0300

    Fix: M2-3590 - Dashboard - Incorrect Email validation error message placement

--- src/modules/Dashboard/features/Applet/AddUser/AddUserForm/AddUserForm.hooks.ts
+++ src/modules/Dashboard/features/Applet/AddUser/AddUserForm/AddUserForm.hooks.ts
@@ -1,24 +1,21 @@
 import { useState, useEffect } from 'react';
-import { FieldValues, UseFormSetError } from 'react-hook-form';
+import { FieldValues, Path } from 'react-hook-form';
+
+import { ErrorResponse } from 'shared/state';
 
 import { getErrorData, getErrorMessage } from 'shared/utils';
 
 import { Fields } from './AddUserForm.const';
+import { UseFormError } from './AddUserForm.types';
 
-export const useFormError = <T extends FieldValues>({
-  error,
-  setError,
-}: {
-  error: unknown | null;
-  setError: UseFormSetError<T>;
-}) => {
+export const useFormError = <T extends FieldValues>({ error, setError }: UseFormError<T>) => {
   const [hasCommonError, setHasCommonError] = useState(false);
 
   useEffect(() => {
     if (!error) return setHasCommonError(false);
 
-    const errorData = getErrorData(error);
-    const fieldName = errorData?.path?.at?.(-1);
+    const errorData: ErrorResponse = getErrorData(error);
+    const fieldName = errorData?.path?.at?.(-1) as Path<T>;
 
     if (!fieldName || (fieldName && !Fields[fieldName as keyof typeof Fields]))
       return setHasCommonError(true);
--- src/shared/utils/errors.ts
+++ src/shared/utils/errors.ts
@@ -1,8 +1,10 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse, AxiosError } from 'axios';
+
+import { ApiError } from 'shared/state';
 
 export const isError = (error: unknown): error is Error => error instanceof Error;
 
-export const getErrorData = (error: unknown) => {
+export const getErrorData = (error: Error | AxiosError<ApiError> | unknown) => {
   if (isError(error)) {
     if (axios.isAxiosError(error) && error?.response?.data?.result?.length) {
       return error.response?.data?.result[0];
@@ -12,14 +14,14 @@ export const getErrorData = (error: unknown) => {
   } else if (typeof error === 'object') {
     const err = error as AxiosResponse;
 
-    return err.data;
+    return err?.data;
   }
 
   return error;
 };
 
-export const getErrorMessage = (error: unknown) => {
+export const getErrorMessage = (error: Error | AxiosError<ApiError> | unknown) => {
   const errorData = getErrorData(error);
 
-  return errorData?.message ?? errorData?.data?.message ?? String(errorData);
+  return errorData?.message ?? String(errorData);
 };
