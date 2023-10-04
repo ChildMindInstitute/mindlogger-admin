f8c274459ec016f04ba66c3d5c9f9de0d5f8789d
Danilenka, Kiryl <kdanilenka@scnsoft.com>
2023-10-04 15:39:26 +0300

    fix/mocked-router-changed

--- src/shared/utils/renderWithProviders.tsx
+++ src/shared/utils/renderWithProviders.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import { Route, Routes, MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
 import { ExtendedRenderOptions, setupStore } from 'redux/store';
 
 export const renderWithProviders = (
