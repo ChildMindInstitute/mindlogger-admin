58f0692edfccbf6123cef4db97a47f98b4efcbfe
nmialik <115553085+nmialik@users.noreply.github.com>
2023-10-05 11:05:16 +0200

    Fix/m2 3685  deleted folder appers again if the focus is stayed on the folder right after creation (#1045)

--- src/shared/components/Actions/Actions.tsx
+++ src/shared/components/Actions/Actions.tsx
@@ -58,6 +58,7 @@ export const Actions = ({
                     isActive={active}
                     disabled={disabled}
                     onClick={onClick(action)}
+                    onMouseDown={(e) => e.preventDefault()} // prevent onBlur actions for folders
                     isVisible={isVisible || (hasStaticActions && !isVisible && isStatic)}
                     data-testid={dataTestid}
                   >
