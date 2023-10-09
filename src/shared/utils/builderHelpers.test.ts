d650ee71917cf895370f17459ce40689adf8e02d
IhorNesterenko99 <inesterenko@scnsoft.com>
2023-10-09 12:07:45 +0300

    feat: M2-3705 Write unit tests for some shared functions (builderHelpers.ts, dateTimezone.ts)

--- src/shared/utils/builderHelpers.test.ts
+++ src/shared/utils/builderHelpers.test.ts
@@ -1,103 +1,68 @@
 import { getEntityKey, getObjectFromList, getUniqueName } from './builderHelpers';
 
 describe('getEntityKey', () => {
-  it('should return the id when useIdFirst is true and id is present', () => {
-    const entity = { id: '123', key: 'abc' };
-    const result = getEntityKey(entity, true);
-    expect(result).toBe('123');
-  });
+  const entity1 = { id: '123', key: 'abc' };
+  const entity2 = { key: 'abc' };
+  const entity3 = { id: '', key: null };
 
-  it('should return the key when useIdFirst is true and id is not present', () => {
-    const entity = { key: 'abc' };
-    const result = getEntityKey(entity, true);
-    expect(result).toBe('abc');
-  });
-
-  it('should return an empty string when useIdFirst is true and neither id nor key is present', () => {
-    const entity = {};
-    const result = getEntityKey(entity, true);
-    expect(result).toBe('');
-  });
-
-  it('should return the key when useIdFirst is false and key is present', () => {
-    const entity = { id: '123', key: 'abc' };
-    const result = getEntityKey(entity, false);
-    expect(result).toBe('abc');
-  });
-
-  it('should return the id when useIdFirst is true and id is present, even if key is falsy', () => {
-    const entity = { id: '123', key: '' };
-    const result = getEntityKey(entity, true);
-    expect(result).toBe('123');
-  });
-
-  it('should return the key when useIdFirst is false and key is present, even if id is falsy', () => {
-    const entity = { id: '', key: 'abc' };
-    const result = getEntityKey(entity, false);
-    expect(result).toBe('abc');
-  });
-
-  it('should return an empty string when both id and key are falsy, regardless of useIdFirst', () => {
-    const entity = { id: '', key: '' };
-    const resultIdFirst = getEntityKey(entity, true);
-    const resultKeyFirst = getEntityKey(entity, false);
-    expect(resultIdFirst).toBe('');
-    expect(resultKeyFirst).toBe('');
+  test.each`
+    entity     | useIdFirst | expected       | description
+    ${entity1} | ${true}    | ${entity1.id}  | ${'returns id when useIdFirst is true and id is present'}
+    ${entity2} | ${true}    | ${entity2.key} | ${'returns the key when useIdFirst is true and id is not present'}
+    ${{}}      | ${true}    | ${''}          | ${'returns empty string when useIdFirst is true and no id or key'}
+    ${entity1} | ${false}   | ${entity1.key} | ${'returns the key when useIdFirst is false and key is present'}
+    ${entity3} | ${false}   | ${''}          | ${'returns an empty string when id and key are falsy'}
+  `('$description', ({ entity, useIdFirst, expected }) => {
+    expect(getEntityKey(entity, useIdFirst)).toBe(expected);
   });
 });
 
 describe('getObjectFromList', () => {
-  it('should return an empty object when items array is empty', () => {
-    const result = getObjectFromList([]);
-    expect(result).toEqual({});
-  });
+  const items = [
+    { id: '1', name: 'Item 1' },
+    { id: '2', name: 'Item 2' },
+  ];
+  const expected1 = {
+    '1': { id: '1', name: 'Item 1' },
+    '2': { id: '2', name: 'Item 2' },
+  };
+  const expected2 = {
+    'Item 1': { id: '1', name: 'Item 1' },
+    'Item 2': { id: '2', name: 'Item 2' },
+  };
+  const getUniqueKey = (item: { id: string; name: string }) => item.name;
 
-  it('should create an object with unique keys based on ids by default', () => {
-    const items = [
-      { id: '1', name: 'Item 1' },
-      { id: '2', name: 'Item 2' },
-    ];
-    const result = getObjectFromList(items);
-    expect(result).toEqual({
-      '1': { id: '1', name: 'Item 1' },
-      '2': { id: '2', name: 'Item 2' },
-    });
+  test.each`
+    items    | expected     | getUniqueKey    | description
+    ${[]}    | ${{}}        | ${undefined}    | ${'returns empty object when items array is empty'}
+    ${items} | ${expected1} | ${undefined}    | ${'creates an object with unique keys based on ids by default'}
+    ${items} | ${expected2} | ${getUniqueKey} | ${'creates an object with unique keys based on a custom key function'}
+  `('$description', ({ items, getUniqueKey, expected }) => {
+    expect(getObjectFromList(items, getUniqueKey)).toEqual(expected);
   });
-
-  it('should create an object with unique keys based on a custom key function', () => {
-    const items = [
-      { id: '1', name: 'Item 1' },
-      { id: '2', name: 'Item 2' },
-    ];
-    const result = getObjectFromList(items, (item) => item.name);
-    expect(result).toEqual({
-      'Item 1': { id: '1', name: 'Item 1' },
-      'Item 2': { id: '2', name: 'Item 2' },
-    });
-  });
-
-  // Add more test cases to cover various scenarios
 });
 
 describe('getUniqueName', () => {
-  it('should return the original name when it is unique', () => {
-    const result = getUniqueName({ name: 'UniqueName', existingNames: ['OtherName'] });
-    expect(result).toBe('UniqueName');
+  const name1 = 'Unique Name';
+  const name2 = 'Existing Name';
+  const name3 = 'Existing Name (5)';
+  const existingNames = [
+    'Other Name',
+    'Different Name',
+    'Existing Name',
+    'Existing Name (5)',
+    'Existing Name (6)',
+  ];
+  const expected1 = 'Existing Name (1)';
+  const expected2 = 'Existing Name_1';
+  const expected3 = 'Existing Name (7)';
+  test.each`
+    name     | existingNames    | withUnderscore | expected     | description
+    ${name1} | ${existingNames} | ${undefined}   | ${name1}     | ${'returns the original name when it is unique'}
+    ${name2} | ${existingNames} | ${undefined}   | ${expected1} | ${'appends index in parentheses if already exists'}
+    ${name2} | ${existingNames} | ${true}        | ${expected2} | ${'appends underscore index, withUnderscore: true'}
+    ${name3} | ${existingNames} | ${undefined}   | ${expected3} | ${'appends next available index in parentheses'}
+  `('$description', ({ name, existingNames, withUnderscore, expected }) => {
+    expect(getUniqueName({ name, existingNames, withUnderscore })).toBe(expected);
   });
-
-  it('should append index in parentheses when the name already exists', () => {
-    const result = getUniqueName({ name: 'ExistingName', existingNames: ['ExistingName'] });
-    expect(result).toBe('ExistingName (1)');
-  });
-
-  it('should append index with underscore when withUnderscore is true', () => {
-    const result = getUniqueName({
-      name: 'ExistingName',
-      existingNames: ['ExistingName', 'ExistingName_1'],
-      withUnderscore: true,
-    });
-    expect(result).toBe('ExistingName_2');
-  });
-
-  // Add more test cases to cover various scenarios
 });
