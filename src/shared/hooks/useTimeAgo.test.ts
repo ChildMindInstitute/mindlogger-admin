b3a0d826e73ef104a15f8e56939888ff130b29ef
wildkiomi <whisann.lie14@gmail.com>
2023-10-11 11:53:00 +0400

    M2-3727

--- src/shared/hooks/useTimeAgo.test.ts
+++ src/shared/hooks/useTimeAgo.test.ts
@@ -19,8 +19,12 @@ jest.mock('react-i18next', () => ({
 describe('useTimeAgo hook tests', () => {
   test('useTimeAgo should format date', () => {
     const { result } = renderHook(() => useTimeAgo());
-    const yesterdayDate = new Date(add(new Date(), { days: -1 }));
+    const today = new Date();
+    const yesterday = new Date(add(today, { days: -1 }));
+    const tomorrow = new Date(add(today, { days: 1 }));
 
-    expect(result.current.format(yesterdayDate)).toBe('1 day ago');
+    expect(result.current.format(yesterday)).toBe('1 day ago');
+    expect(result.current.format(today)).toBe('just now');
+    expect(result.current.format(tomorrow)).toBe('in 1 day');
   });
 });
