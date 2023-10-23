610f7f2fa0ba584225cdb20ca0e091dff4201c7f
muhurov <muhurov@gmail.com>
2023-10-23 15:08:59 +0300

    Tests: M2-3873 - Unit tests - useWindowSize, useIsTextNodeEllipsed

---
+++ src/shared/hooks/useWindowSize.test.ts
@@ -0,0 +1,31 @@
+import { act, renderHook } from '@testing-library/react';
+
+import { useWindowSize } from './useWindowSize';
+
+describe('useWindowSize', () => {
+  test('should return correct height and width for window', () => {
+    const { result } = renderHook(() => useWindowSize());
+
+    expect(result.current.width).toEqual(window.innerWidth);
+    expect(result.current.height).toEqual(window.innerHeight);
+  });
+
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+  test.each`
+    width          | height          | expectedSize                             | description
+    ${windowWidth} | ${1000}         | ${{ width: windowWidth, height: 1000 }}  | ${'should update height after resize'}
+    ${1000}        | ${windowHeight} | ${{ width: 1000, height: windowHeight }} | ${'should update width after resize'}
+    ${1000}        | ${1000}         | ${{ width: 1000, height: 1000 }}         | ${'should update height and width after resize'}
+  `('$description', ({ width, height, expectedSize }) => {
+    const { result } = renderHook(() => useWindowSize());
+
+    act(() => {
+      window.innerWidth = width;
+      window.innerHeight = height;
+      window.dispatchEvent(new Event('resize'));
+    });
+
+    expect(result.current).toEqual(expectedSize);
+  });
+});
