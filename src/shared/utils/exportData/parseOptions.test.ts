9bbc77620aa2bf9ee52d08857ed457d3498093a8
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2023-10-25 15:45:34 +0300

    M2-3901: added exceptional tests for parseOptions, fixed PR comments

--- src/shared/utils/exportData/parseOptions.test.ts
+++ src/shared/utils/exportData/parseOptions.test.ts
@@ -30,6 +30,51 @@ describe('parseOptions', () => {
       },
     ],
   };
+  const singleExpected = 'Opt1: 1 (score: 4), Opt2: 2 (score: 2)';
+  const singleScoreWithoutValues = {
+    paletteName: null,
+    options: [
+      {
+        ...single.options[0],
+        value: null,
+      },
+      {
+        ...single.options[1],
+        value: null,
+      },
+    ],
+  };
+  const singleScoreWithoutValuesExpected = 'Opt1 (score: 4), Opt2 (score: 2)';
+  const singleWithoutScore = {
+    paletteName: null,
+    options: [
+      {
+        ...single.options[0],
+        score: null,
+      },
+      {
+        ...single.options[1],
+        score: null,
+      },
+    ],
+  };
+  const singleWithoutScoreExpected = 'Opt1: 1, Opt2: 2';
+  const singleWithoutValues = {
+    paletteName: null,
+    options: [
+      {
+        ...single.options[0],
+        score: null,
+        value: null,
+      },
+      {
+        ...single.options[1],
+        score: null,
+        value: null,
+      },
+    ],
+  };
+  const singleWithoutValuesExpected = 'Opt1, Opt2';
   const multi = {
     paletteName: null,
     options: [
@@ -57,6 +102,7 @@ describe('parseOptions', () => {
       },
     ],
   };
+  const multiExpected = 'Opt1: 1 (score: 2), Opt2: 2 (score: 1)';
   const slider = {
     minLabel: 'min',
     maxLabel: 'max',
@@ -67,6 +113,19 @@ describe('parseOptions', () => {
     scores: [1, 2, 3, 4, 5, 6],
     alerts: null,
   };
+  const sliderExpected =
+    '0: 0 (score: 1), 1: 1 (score: 2), 2: 2 (score: 3), 3: 3 (score: 4), 4: 4 (score: 5), 5: 5 (score: 6)';
+  const sliderWithoutScore = {
+    minLabel: 'min',
+    maxLabel: 'max',
+    minValue: 0,
+    maxValue: 5,
+    minImage: null,
+    maxImage: null,
+    scores: null,
+    alerts: null,
+  };
+  const sliderWithoutScoreExpected = '0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5';
   const numberSelect = {
     minValue: 0,
     maxValue: 7,
@@ -187,23 +246,27 @@ describe('parseOptions', () => {
   };
 
   test.each`
-    responseType                                | responseValues      | expected                                                                                                  | description
-    ${ItemResponseType.SingleSelection}         | ${single}           | ${'Opt1: 1 (score: 4), Opt2: 2 (score: 2)'}                                                               | ${'single'}
-    ${ItemResponseType.MultipleSelection}       | ${multi}            | ${'Opt1: 1 (score: 2), Opt2: 2 (score: 1)'}                                                               | ${'multi'}
-    ${ItemResponseType.Slider}                  | ${slider}           | ${'0: 0 (score: 1), 1: 1 (score: 2), 2: 2 (score: 3), 3: 3 (score: 4), 4: 4 (score: 5), 5: 5 (score: 6)'} | ${'slider'}
-    ${ItemResponseType.Date}                    | ${null}             | ${undefined}                                                                                              | ${'date'}
-    ${ItemResponseType.NumberSelection}         | ${numberSelect}     | ${undefined}                                                                                              | ${'numberSelect'}
-    ${ItemResponseType.Time}                    | ${null}             | ${undefined}                                                                                              | ${'time'}
-    ${ItemResponseType.TimeRange}               | ${null}             | ${undefined}                                                                                              | ${'timeRange'}
-    ${ItemResponseType.SingleSelectionPerRow}   | ${singleSelectRows} | ${''}                                                                                                     | ${'singleSelectRows'}
-    ${ItemResponseType.MultipleSelectionPerRow} | ${multiSelectRows}  | ${''}                                                                                                     | ${'multiSelectRows'}
-    ${ItemResponseType.SliderRows}              | ${sliderRows}       | ${''}                                                                                                     | ${'sliderRows'}
-    ${ItemResponseType.Text}                    | ${null}             | ${undefined}                                                                                              | ${'text'}
-    ${ItemResponseType.Drawing}                 | ${drawing}          | ${undefined}                                                                                              | ${'drawing'}
-    ${ItemResponseType.Photo}                   | ${null}             | ${undefined}                                                                                              | ${'photo'}
-    ${ItemResponseType.Video}                   | ${null}             | ${undefined}                                                                                              | ${'video'}
-    ${ItemResponseType.Audio}                   | ${audio}            | ${undefined}                                                                                              | ${'audio'}
-    ${ItemResponseType.AudioPlayer}             | ${audioPlayer}      | ${undefined}                                                                                              | ${'audioPlayer'}
+    responseType                                | responseValues              | expected                            | description
+    ${ItemResponseType.SingleSelection}         | ${single}                   | ${singleExpected}                   | ${'single'}
+    ${ItemResponseType.SingleSelection}         | ${singleScoreWithoutValues} | ${singleScoreWithoutValuesExpected} | ${'single with score without values'}
+    ${ItemResponseType.SingleSelection}         | ${singleWithoutScore}       | ${singleWithoutScoreExpected}       | ${'single with values without score'}
+    ${ItemResponseType.SingleSelection}         | ${singleWithoutValues}      | ${singleWithoutValuesExpected}      | ${'single without values and scores'}
+    ${ItemResponseType.MultipleSelection}       | ${multi}                    | ${multiExpected}                    | ${'multi'}
+    ${ItemResponseType.Slider}                  | ${slider}                   | ${sliderExpected}                   | ${'slider'}
+    ${ItemResponseType.Slider}                  | ${sliderWithoutScore}       | ${sliderWithoutScoreExpected}       | ${'slider without score'}
+    ${ItemResponseType.Date}                    | ${null}                     | ${undefined}                        | ${'date'}
+    ${ItemResponseType.NumberSelection}         | ${numberSelect}             | ${undefined}                        | ${'numberSelect'}
+    ${ItemResponseType.Time}                    | ${null}                     | ${undefined}                        | ${'time'}
+    ${ItemResponseType.TimeRange}               | ${null}                     | ${undefined}                        | ${'timeRange'}
+    ${ItemResponseType.SingleSelectionPerRow}   | ${singleSelectRows}         | ${''}                               | ${'singleSelectRows'}
+    ${ItemResponseType.MultipleSelectionPerRow} | ${multiSelectRows}          | ${''}                               | ${'multiSelectRows'}
+    ${ItemResponseType.SliderRows}              | ${sliderRows}               | ${''}                               | ${'sliderRows'}
+    ${ItemResponseType.Text}                    | ${null}                     | ${undefined}                        | ${'text'}
+    ${ItemResponseType.Drawing}                 | ${drawing}                  | ${undefined}                        | ${'drawing'}
+    ${ItemResponseType.Photo}                   | ${null}                     | ${undefined}                        | ${'photo'}
+    ${ItemResponseType.Video}                   | ${null}                     | ${undefined}                        | ${'video'}
+    ${ItemResponseType.Audio}                   | ${audio}                    | ${undefined}                        | ${'audio'}
+    ${ItemResponseType.AudioPlayer}             | ${audioPlayer}              | ${undefined}                        | ${'audioPlayer'}
   `(
     'returns content for options for "$description": "$expected"',
     ({ responseType, responseValues, expected }) => {
