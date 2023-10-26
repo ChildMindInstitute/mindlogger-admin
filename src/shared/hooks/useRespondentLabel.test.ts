e739d687e7848d847d03520d74af0af0a87a98e1
nmialik <nmialik@scnsoft.com>
2023-10-26 13:19:20 +0200

    M2-3899: Unit tests for useDatavizFilters, useRespondentLabel

---
+++ src/shared/hooks/useRespondentLabel.test.ts
@@ -0,0 +1,61 @@
+import { renderHook } from '@testing-library/react';
+import * as routerDom from 'react-router-dom';
+
+import { users } from 'redux/modules';
+
+import { useRespondentLabel } from './useRespondentLabel';
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: jest.fn(),
+}));
+
+describe('useRespondentLabel', () => {
+  test('should return an empty string when respondentId is not provided', () => {
+    jest.spyOn(routerDom, 'useParams').mockReturnValue({ respondentId: undefined });
+    users.useRespondent = jest.fn().mockReturnValue({ details: undefined });
+
+    const { result } = renderHook(useRespondentLabel);
+
+    expect(result.current).toBe('');
+  });
+
+  test('should return an empty string when details are not available', () => {
+    jest.spyOn(routerDom, 'useParams').mockReturnValue({ respondentId: '123' });
+    users.useRespondent = jest.fn().mockReturnValue({ details: undefined });
+
+    const { result } = renderHook(() => useRespondentLabel());
+
+    expect(result.current).toBe('');
+  });
+
+  test('should construct and return the respondent label correctly (User: secret123)', () => {
+    jest.spyOn(routerDom, 'useParams').mockReturnValue({ respondentId: '123' });
+    const details = [
+      {
+        respondentSecretId: 'secret123',
+        respondentNickname: '',
+      },
+    ];
+    users.useRespondent = jest.fn().mockReturnValue({ details });
+
+    const { result } = renderHook(() => useRespondentLabel());
+
+    expect(result.current).toBe('User: secret123');
+  });
+
+  test('should construct and return the respondent label correctly (User: secret123 (John Doe))', () => {
+    jest.spyOn(routerDom, 'useParams').mockReturnValue({ respondentId: '123' });
+    const details = [
+      {
+        respondentSecretId: 'secret123',
+        respondentNickname: 'John Doe',
+      },
+    ];
+    users.useRespondent = jest.fn().mockReturnValue({ details });
+
+    const { result } = renderHook(() => useRespondentLabel());
+
+    expect(result.current).toBe('User: secret123 (John Doe)');
+  });
+});
