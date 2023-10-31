803ae562564b60b3756396a2ec81cb4095e0f9e8
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2023-10-31 18:53:16 +0300

    M2-3939: added tests for getFlag, getRawScores, getReportName, migratedData

---
+++ src/shared/utils/exportData/getFlag.test.ts
@@ -0,0 +1,28 @@
+import { ActivityStatus } from 'shared/consts';
+import { mockedExportContextItemData, mockedSingleActivityItem } from 'shared/mock';
+
+import { getFlag } from './getFlag';
+
+const singleItem = {
+  activityItem: mockedSingleActivityItem,
+  answer: {
+    value: 2,
+    text: 'Extra info',
+  },
+  items: [],
+  ...mockedExportContextItemData,
+};
+const missedSingleItem = {
+  ...singleItem,
+  scheduledDatetime: 1689755822,
+  startDatetime: null,
+};
+describe('getRawScores', () => {
+  test.each`
+    item                | expected                    | description
+    ${singleItem}       | ${ActivityStatus.Completed} | ${'should return flag comleted'}
+    ${missedSingleItem} | ${ActivityStatus.Missed}    | ${'should return flag missed'}
+  `('$description', ({ item, expected }) => {
+    expect(getFlag(item)).toBe(expected);
+  });
+});
---
+++ src/shared/utils/exportData/getRawScores.test.ts
@@ -0,0 +1,99 @@
+import { getRawScores } from './getRowScores';
+
+const singleResponseValues = {
+  paletteName: null,
+  options: [
+    {
+      id: '4bae594b-4385-402c-aa96-0f6438e7e642',
+      text: 'opt1',
+      image: null,
+      score: 3,
+      tooltip: null,
+      isHidden: false,
+      color: null,
+      alert: null,
+      value: 0,
+    },
+    {
+      id: 'b8b0a211-7f30-48af-bee5-54cbf53889bd',
+      text: 'opt2',
+      image: null,
+      score: 5,
+      tooltip: null,
+      isHidden: false,
+      color: null,
+      alert: null,
+      value: 1,
+    },
+    {
+      id: '3c75fa7f-3ae9-4b4f-ab29-81664418c430',
+      text: 'opt3',
+      image: null,
+      score: 1,
+      tooltip: null,
+      isHidden: false,
+      color: null,
+      alert: null,
+      value: 2,
+    },
+  ],
+};
+const multiResponseValues = {
+  paletteName: null,
+  options: [
+    {
+      id: 'e6f6b1c1-3ec2-45b2-8c34-6d0970b86d64',
+      text: 'opt1',
+      image: null,
+      score: 1,
+      tooltip: null,
+      isHidden: false,
+      color: null,
+      alert: null,
+      value: 0,
+    },
+    {
+      id: 'a64abb45-1ba3-4113-88b1-5e28459755dc',
+      text: 'opt2',
+      image: null,
+      score: 3,
+      tooltip: null,
+      isHidden: false,
+      color: null,
+      alert: null,
+      value: 1,
+    },
+    {
+      id: '1ce9e52d-28b3-4768-846c-83e378db59ef',
+      text: 'opt3',
+      image: null,
+      score: 0,
+      tooltip: null,
+      isHidden: false,
+      color: null,
+      alert: null,
+      value: 2,
+    },
+  ],
+};
+const sliderResponseValues = {
+  minLabel: 'min',
+  maxLabel: 'max',
+  minValue: 0,
+  maxValue: 5,
+  minImage: null,
+  maxImage: null,
+  scores: [1, 2, 3, 4, 5, 6],
+  alerts: null,
+};
+
+describe('getRawScores', () => {
+  test.each`
+    responseValues          | expected | description
+    ${singleResponseValues} | ${9}     | ${'should return raw scores for single item'}
+    ${multiResponseValues}  | ${4}     | ${'should return raw scores for multi item'}
+    ${sliderResponseValues} | ${21}    | ${'should return raw scores for slider item'}
+  `('$description', ({ responseValues, expected }) => {
+    expect(getRawScores(responseValues)).toBe(expected);
+  });
+});
