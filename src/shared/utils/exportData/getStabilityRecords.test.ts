a494c0f8369e6b244a198a7af2fb2e57c306785a
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2023-11-01 14:30:02 +0300

    M2-3940: added test with dynamic data

--- src/shared/utils/exportData/getStabilityRecords.test.ts
+++ src/shared/utils/exportData/getStabilityRecords.test.ts
@@ -2,26 +2,46 @@ import { getStabilityRecords } from './getStabilityRecords';
 
 const stabilityData = [
   {
-    timestamp: 1689145864376,
-    stimPos: [0.0013170829577251464],
+    timestamp: 1689145034537,
+    stimPos: [0],
     targetPos: [0],
-    userPos: [0.5568904736342366],
-    score: 0.06306843781471252,
-    lambda: 0.07563068437814713,
+    userPos: [0],
+    score: 0.06978260397911072,
+    lambda: 0.07569782603979111,
+    lambdaSlope: 20,
+  },
+  {
+    timestamp: 1689145034561,
+    stimPos: [0.00015208138993205722],
+    targetPos: [0],
+    userPos: [0.06140350877192979],
+    score: 0.1352205219268799,
+    lambda: 0.0763522052192688,
     lambdaSlope: 20,
   },
 ];
 const regularResult = [
   {
-    UTC_Timestamp: '1689145864.376',
-    epoch_time_in_seconds_start: '1689145864.376',
+    UTC_Timestamp: '1689145034.537',
+    epoch_time_in_seconds_start: '1689145034.537',
     lambda_slope: 20,
-    lambda_value: 0.07563068437814713,
-    score: 0.06306843781471252,
+    lambda_value: 0.07569782603979111,
+    score: 0.06978260397911072,
     seconds: '0',
-    stimulus_position: '0.0013170829577251464',
+    stimulus_position: '0',
     target_position: '0',
-    user_position: '0.5568904736342366',
+    user_position: '0',
+  },
+  {
+    UTC_Timestamp: '1689145034.561',
+    epoch_time_in_seconds_start: '',
+    lambda_slope: 20,
+    lambda_value: 0.0763522052192688,
+    score: 0.1352205219268799,
+    seconds: '0.024',
+    stimulus_position: '0.00015208138993205722',
+    target_position: '0',
+    user_position: '0.06140350877192979',
   },
 ];
 
