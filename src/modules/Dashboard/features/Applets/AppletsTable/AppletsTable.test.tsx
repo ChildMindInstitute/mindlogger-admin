96040967fc982ed557284c2f920019273eade9e6
Hanna <35298264+wildkiomi@users.noreply.github.com>
2023-11-06 14:36:57 +0400

    tests: M2-3959 AppletsTable (#1134)

---
+++ src/modules/Dashboard/features/Applets/AppletsTable/AppletsTable.test.tsx
@@ -0,0 +1,97 @@
+import { screen } from '@testing-library/react';
+
+import { renderWithProviders } from 'shared/utils';
+import { mockedApplet, mockedAppletId, mockedCurrentWorkspace } from 'shared/mock';
+import { base } from 'shared/state/Base';
+import { Roles } from 'shared/consts';
+
+import { AppletsTable } from './AppletsTable';
+import { getHeadCells } from '../Applets.const';
+import { AppletsTableProps } from './AppletsTable.types';
+import { AppletsContext } from '../Applets.context';
+
+const initialStateData = {
+  ...base.state,
+  data: null,
+};
+
+const preloadedState = {
+  workspaces: {
+    workspaces: initialStateData,
+    currentWorkspace: {
+      ...initialStateData,
+      ...mockedCurrentWorkspace,
+    },
+    roles: {
+      ...initialStateData,
+      data: {
+        [mockedAppletId]: [Roles.Manager],
+      },
+    },
+    applet: mockedApplet,
+    workspacesRoles: initialStateData,
+  },
+  applet: {
+    applet: {
+      ...initialStateData,
+      data: { result: mockedApplet },
+    },
+  },
+};
+
+const appletsTablelTestId = 'dashboard-applets-table-test-id';
+
+const mockedRows = [
+  mockedApplet,
+  {
+    id: 'testId',
+    name: 'testName',
+    displayName: 'displayedName',
+    isFolder: true,
+    foldersAppletCount: 0,
+  },
+];
+
+const mockContext = {
+  rows: mockedRows,
+  setRows: jest.fn(),
+  expandedFolders: [],
+  reloadData: jest.fn(),
+  handleFolderClick: jest.fn(),
+  fetchData: jest.fn(),
+};
+
+const getAppletsTable = (props: Partial<AppletsTableProps> = {}) => (
+  <AppletsContext.Provider value={mockContext}>
+    <AppletsTable
+      data-testid={appletsTablelTestId}
+      columns={getHeadCells()}
+      rows={mockedRows}
+      order="asc"
+      orderBy=""
+      emptyComponent={<>empty state</>}
+      handleRequestSort={jest.fn()}
+      page={1}
+      count={2}
+      rowsPerPage={30}
+      handleChangePage={jest.fn()}
+      headerContent={<>test header content</>}
+      handleReload={jest.fn()}
+      {...props}
+    />
+  </AppletsContext.Provider>
+);
+
+describe('AppletsTable component tests', () => {
+  test('should render empty component', () => {
+    renderWithProviders(getAppletsTable({ rows: [] }), { preloadedState });
+
+    expect(screen.getByText('empty state')).toBeInTheDocument();
+  });
+
+  test('should render table with rows', () => {
+    renderWithProviders(getAppletsTable(), { preloadedState });
+
+    expect(screen.getByTestId(appletsTablelTestId)).toBeInTheDocument();
+  });
+});
