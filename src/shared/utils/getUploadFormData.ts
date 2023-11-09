4956f7eb308b4cd32441d903948b222e627ac90e
Aleksandr Eremin <aleksandr.i.eremin@yandex.ru>
2023-11-09 00:48:36 -0800

    Feature/m2 4024 add server logger error boundary component (#1140)
    * M2-4024: added server logger in ErrorBoundary
    
    * M2-4024: added zip file for logger, removed redundant api
    
    * M2-4024: added shared zip config
    
    * M2-4024: added server logger for production env ONLY

--- src/shared/utils/getUploadFormData.ts
+++ src/shared/utils/getUploadFormData.ts
@@ -1,4 +1,4 @@
-export const getUploadFormData = (file: File) => {
+export const getUploadFormData = (file: File | Blob) => {
   const body = new FormData();
   body.append('file', file);
 
---
+++ src/shared/utils/logger.ts
@@ -0,0 +1,76 @@
+import JSZip from 'jszip';
+
+import { getUploadFormData } from 'shared/utils/getUploadFormData';
+
+import { BLOB_ZIP_OPTIONS } from './exportZip';
+import { postLogFile } from '../api/api';
+
+const DEVICE_ID = 'browser';
+
+export const getOS = () => {
+  let osName = 'unknown';
+  let osFormerName = 'unknown';
+  if (navigator.userAgent.indexOf('Win') !== -1) osName = 'Windows';
+  if (navigator.userAgent.indexOf('Mac') !== -1) osName = 'Macintosh';
+  if (navigator.userAgent.indexOf('Linux') !== -1) osName = 'Linux';
+  if (navigator.userAgent.indexOf('Android') !== -1) osName = 'Android';
+  if (navigator.userAgent.indexOf('like Mac') !== -1) osName = 'iOS';
+
+  if (navigator.appVersion.indexOf('Win') !== -1) osFormerName = 'Windows';
+  if (navigator.appVersion.indexOf('Mac') !== -1) osFormerName = 'MacOS';
+  if (navigator.appVersion.indexOf('X11') !== -1) osFormerName = 'UNIX';
+  if (navigator.appVersion.indexOf('Linux') !== -1) osFormerName = 'Linux';
+
+  return `${osName}/${osFormerName}`;
+};
+
+export const sendLogFile = async ({
+  error,
+  formData,
+}: {
+  error: TypeError;
+  formData?: Record<string, unknown>;
+}) => {
+  const { platform, appVersion, appName, userAgent, hardwareConcurrency, language } = navigator;
+  const time = new Date().getTime();
+  const { href } = location;
+  const urlObject = new URL(href);
+  const { pathname } = urlObject;
+  const logObject = {
+    time,
+    details: {
+      os: getOS(),
+      platform,
+      appVersion,
+      appName,
+      userAgent,
+      hardwareConcurrency,
+      language,
+    },
+    href: location.href,
+    formData: formData ?? '',
+    errorMessage: error?.message ?? '',
+    errorStack: error?.stack ?? '',
+  };
+  const logString = JSON.stringify(logObject, undefined, 4);
+  const file = new File([logString], `log_${time}.txt`, {
+    type: 'text/plain',
+    lastModified: time,
+  });
+  const fileId = `${pathname.replace(/\//g, '_')}_${time}`;
+  const zip = new JSZip();
+  zip.file(`${fileId}.txt`, file);
+  const content = await zip.generateAsync(BLOB_ZIP_OPTIONS);
+  const fileFormData = getUploadFormData(content);
+
+  try {
+    const result = await postLogFile({
+      deviceId: DEVICE_ID,
+      fileId: `${fileId}.zip`,
+      file: fileFormData,
+    });
+    console.warn('[LOGGER]', result);
+  } catch (error) {
+    console.warn('Error while file logging:', error);
+  }
+};
