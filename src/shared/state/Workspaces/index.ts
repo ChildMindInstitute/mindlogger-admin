f749c69da4a52548eac4896bc0ad67641e44b6bc
muhurov <muhurov@gmail.com>
2023-11-14 12:46:20 +0300

    M2-3047: removed excess code

--- src/shared/state/Workspaces/index.ts
+++ src/shared/state/Workspaces/index.ts
@@ -37,12 +37,4 @@ export const workspaces = {
     ),
   useWorkspacesRolesData: (): WorkspacesSchema['workspacesRoles']['data'] =>
     useAppSelector(({ workspaces: { workspacesRoles } }) => workspacesRoles?.data),
-  useWorkspacesRolesResponseStatus: (): MetaStatus =>
-    useAppSelector(
-      ({
-        workspaces: {
-          workspacesRoles: { status },
-        },
-      }) => status,
-    ),
 };
