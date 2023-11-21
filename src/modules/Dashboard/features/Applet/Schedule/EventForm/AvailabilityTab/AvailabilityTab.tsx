7049aa6920d890d4f461aedde1514542f46cfcdd
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2023-11-21 12:24:31 +0300

    M2-4158: removed useless import

--- src/modules/Dashboard/features/Applet/Schedule/EventForm/AvailabilityTab/AvailabilityTab.tsx
+++ src/modules/Dashboard/features/Applet/Schedule/EventForm/AvailabilityTab/AvailabilityTab.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useFormContext, useWatch } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { addDays } from 'date-fns';
