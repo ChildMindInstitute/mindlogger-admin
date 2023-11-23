f7da43c58cb10b7264dabdbe83883e3b7b9a919c
wildkiomi <whisann.lie14@gmail.com>
2023-11-23 14:19:49 +0400

    M2-4205: Score Content tests

--- src/modules/Builder/features/ActivitySettings/ScoresAndReports/ScoreContent/ScoreContent.test.tsx
+++ src/modules/Builder/features/ActivitySettings/ScoresAndReports/ScoreContent/ScoreContent.test.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import { fireEvent, screen, waitFor, within } from '@testing-library/react';
 
 import { renderWithAppletFormData } from 'shared/utils/renderWithAppletFormData';
