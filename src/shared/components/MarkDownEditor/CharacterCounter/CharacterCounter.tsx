aa3e24c856ee3fd69cba79f691c774cf42d9b68b
IhorNesterenko99 <inesterenko@scnsoft.com>
2023-11-24 18:41:01 +0200

    M2-4228: import fix

--- src/shared/components/MarkDownEditor/CharacterCounter/CharacterCounter.tsx
+++ src/shared/components/MarkDownEditor/CharacterCounter/CharacterCounter.tsx
@@ -2,10 +2,10 @@ import { useTranslation } from 'react-i18next';
 
 import { StyledTitleSmall } from 'shared/styles/styledComponents';
 import theme from 'shared/styles/theme';
+import { variables } from 'shared/styles/variables';
 
 import { THRESHOLD_SIZE } from '../MarkDownEditor.const';
 import { CharacterCounterProps } from './CharacterCounter.types';
-import { variables } from '../../../styles';
 
 export const CharacterCounter = ({ inputSize, disabled }: CharacterCounterProps) => {
   const counter = `${inputSize}/${THRESHOLD_SIZE}`;
