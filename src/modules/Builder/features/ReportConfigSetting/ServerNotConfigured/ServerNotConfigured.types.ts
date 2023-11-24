6ee622e1a4103b1164ad75a30eea69918e6e282b
IhorNesterenko99 <inesterenko@scnsoft.com>
2023-11-24 18:34:52 +0200

    M2-4228: Hide "Report Config" from the "Applet Settings" of the Applet page of the Dashboard, and keep it only within the Editing mode (Builder)

---
+++ src/modules/Builder/features/ReportConfigSetting/ServerNotConfigured/ServerNotConfigured.types.ts
@@ -0,0 +1,4 @@
+export type ServerNotConfiguredProps = {
+  appletId: string;
+  'data-testid'?: string;
+};
--- src/shared/components/MarkDownEditor/CharacterCounter/CharacterCounter.tsx
+++ src/shared/components/MarkDownEditor/CharacterCounter/CharacterCounter.tsx
@@ -5,13 +5,16 @@ import theme from 'shared/styles/theme';
 
 import { THRESHOLD_SIZE } from '../MarkDownEditor.const';
 import { CharacterCounterProps } from './CharacterCounter.types';
+import { variables } from '../../../styles';
 
-export const CharacterCounter = ({ inputSize }: CharacterCounterProps) => {
+export const CharacterCounter = ({ inputSize, disabled }: CharacterCounterProps) => {
   const counter = `${inputSize}/${THRESHOLD_SIZE}`;
   const { t } = useTranslation('app');
 
   return (
-    <StyledTitleSmall sx={{ m: theme.spacing(0, 1.6) }}>
+    <StyledTitleSmall
+      sx={{ m: theme.spacing(0, 1.6), ...(disabled && { color: variables.palette.disabled }) }}
+    >
       {counter} {t('characters')}
     </StyledTitleSmall>
   );
