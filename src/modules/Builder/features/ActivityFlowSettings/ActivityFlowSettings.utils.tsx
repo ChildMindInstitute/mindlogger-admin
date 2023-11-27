a641a9efdd896aa2e8e9783d0487ee89dcb51a76
IhorNesterenko99 <116339462+IhorNesterenko99@users.noreply.github.com>
2023-11-27 10:41:36 +0200

    M2-4228: Hide "Report Config" from the "Applet Settings" of the Applet page of the Dashboard, and keep it only within the Editing mode (Builder) (#1230)

--- src/modules/Builder/features/ActivityFlowSettings/ActivityFlowSettings.utils.tsx
+++ src/modules/Builder/features/ActivityFlowSettings/ActivityFlowSettings.utils.tsx
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { ActivityFlowFormValues } from 'modules/Builder/types';
 import { Svg } from 'shared/components/Svg';
-import { ReportConfigSetting } from 'shared/features/AppletSettings';
+import { ReportConfigSetting } from 'modules/Builder/features/ReportConfigSetting';
 import { SettingParam } from 'shared/utils';
 
 const { t } = i18n;
