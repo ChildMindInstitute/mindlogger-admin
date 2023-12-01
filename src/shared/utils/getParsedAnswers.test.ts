8fc215e495028961777a46099ab544b2ff1d39b9
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2023-12-01 15:25:11 +0300

    M2-3997: added coverage for getAnswersWithPublicUrls

--- src/shared/utils/getParsedAnswers.test.ts
+++ src/shared/utils/getParsedAnswers.test.ts
@@ -1,6 +1,19 @@
-import { remapFailedAnswers } from './getParsedAnswers';
+import axios from 'axios';
+
+import { remapFailedAnswers, getAnswersWithPublicUrls } from './getParsedAnswers';
+
+jest.mock('api');
 
 describe('getParsedAnswers', () => {
+  const mockedAxios = axios.create();
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+  beforeAll(() => {
+    jest.restoreAllMocks();
+  });
+
   describe('remapFailedAnswers', () => {
     test('should return filtered out answers', () => {
       const parsedAnswers = [
@@ -71,6 +84,141 @@ describe('getParsedAnswers', () => {
               time: 'time',
             },
           ],
+        },
+      ]);
+    });
+  });
+
+  describe('getAnswersWithPublicUrls', () => {
+    test('should return answers with public urls', async () => {
+      jest.spyOn(mockedAxios, 'post').mockImplementation(
+        () =>
+          new Promise((res) =>
+            res({
+              data: {
+                result: ['publicDrawingUrl', 'publicAudioUrl', 'publicVideoUrl', 'publicPhotoUrl'],
+              },
+            }),
+          ),
+      );
+
+      const parsedAnswers = [
+        {
+          decryptedAnswers: [
+            {
+              appletId: 'appletId',
+              activityItem: {
+                name: 'activityItemName',
+                responseType: 'drawing',
+              },
+              answer: {
+                value: {
+                  uri: 'privateUrl',
+                },
+              },
+            },
+            {
+              appletId: 'appletId',
+              activityItem: {
+                name: 'activityItemName',
+                responseType: 'audio',
+              },
+              answer: {
+                value: 'private_url',
+              },
+            },
+            {
+              appletId: 'appletId',
+              activityItem: {
+                name: 'activityItemName',
+                responseType: 'video',
+              },
+              answer: {
+                value: 'private_url',
+              },
+            },
+            {
+              appletId: 'appletId',
+              activityItem: {
+                name: 'activityItemName',
+                responseType: 'photo',
+              },
+              answer: {
+                value: 'private_url',
+              },
+            },
+            {
+              appletId: 'appletId',
+              activityItem: {
+                name: 'activityItemName',
+              },
+              answer: {
+                value: 'text',
+              },
+            },
+          ],
+          decryptedEvents: [],
+        },
+      ];
+      /* eslint-disable @typescript-eslint/ban-ts-comment */
+      // @ts-ignore
+      const result = await getAnswersWithPublicUrls(parsedAnswers);
+      expect(result).toEqual([
+        {
+          decryptedAnswers: [
+            {
+              appletId: 'appletId',
+              activityItem: {
+                name: 'activityItemName',
+                responseType: 'drawing',
+              },
+              answer: {
+                value: {
+                  uri: 'publicDrawingUrl',
+                },
+              },
+            },
+            {
+              appletId: 'appletId',
+              activityItem: {
+                name: 'activityItemName',
+                responseType: 'audio',
+              },
+              answer: {
+                value: 'publicAudioUrl',
+              },
+            },
+            {
+              appletId: 'appletId',
+              activityItem: {
+                name: 'activityItemName',
+                responseType: 'video',
+              },
+              answer: {
+                value: 'publicVideoUrl',
+              },
+            },
+            {
+              appletId: 'appletId',
+              activityItem: {
+                name: 'activityItemName',
+                responseType: 'photo',
+              },
+              answer: {
+                value: 'publicPhotoUrl',
+              },
+            },
+            {
+              appletId: 'appletId',
+              activityItem: {
+                name: 'activityItemName',
+              },
+              answer: {
+                value: 'text',
+              },
+            },
+          ],
+          decryptedEvents: [],
         },
       ]);
     });
