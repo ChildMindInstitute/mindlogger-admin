b4c6420846786e42291e76d4bccf638ca884a3c6
nmialik <nmialik@scnsoft.com>
2023-12-06 22:25:45 +0100

    M2-4323: Unit tests for Language/SelectLanguage components

---
+++ src/shared/components/Footer/Language/Language.test.tsx
@@ -0,0 +1,58 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import { TFunction } from 'i18next';
+import { render, screen, fireEvent } from '@testing-library/react';
+
+import { Language } from './Language';
+
+jest.mock('react-i18next', () => ({
+  useTranslation: (): { t: TFunction; i18n: { changeLanguage: () => Promise<unknown> } } => ({
+    t: (str: string) => str,
+    i18n: {
+      changeLanguage: () => new Promise(() => {}),
+    },
+  }),
+  initReactI18next: {
+    type: '3rdParty',
+    init: () => {},
+  },
+}));
+
+const dataTestid = 'select-language-popup';
+
+describe('Language', () => {
+  test('renders Language component with default language', () => {
+    render(<Language />);
+    const language = screen.getByText('English');
+    expect(language).toBeInTheDocument();
+  });
+
+  test('select language popup should be closed by click cross', () => {
+    render(<Language />);
+    const language = screen.getByText('English');
+    expect(language).toBeInTheDocument();
+
+    fireEvent.click(language);
+    expect(screen.queryByTestId(dataTestid)).toBeInTheDocument();
+    const closePopupButton = screen.queryByTestId(`${dataTestid}-close-button`);
+
+    fireEvent.click(closePopupButton);
+    expect(screen.queryByTestId(dataTestid)).not.toBeInTheDocument();
+  });
+
+  test('interface language should be changed', () => {
+    render(<Language />);
+    const language = screen.getByText('English');
+    fireEvent.click(language);
+    expect(screen.queryByTestId(dataTestid)).toBeInTheDocument();
+
+    const french = screen.getByText('Français');
+    fireEvent.click(french);
+
+    const ok = screen.getByText('ok');
+    fireEvent.click(ok);
+
+    expect(screen.queryByTestId(dataTestid)).not.toBeInTheDocument();
+    expect(screen.getByText('Français')).toBeInTheDocument();
+  });
+});
