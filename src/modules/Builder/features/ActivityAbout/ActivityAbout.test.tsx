cd6a6b36c71aa267c221cbc98625017215f21d21
wildkiomi <whisann.lie14@gmail.com>
2023-12-07 14:14:24 +0400

    M2-4275: M2-4276: About applet and activity

--- src/modules/Builder/features/ActivityAbout/ActivityAbout.test.tsx
+++ src/modules/Builder/features/ActivityAbout/ActivityAbout.test.tsx
@@ -4,14 +4,131 @@ import { fireEvent, screen, waitFor } from '@testing-library/react';
 
 import { renderWithAppletFormData } from 'shared/utils/renderWithAppletFormData';
 import { page } from 'resources';
-import { mockedActivityId, mockedAppletFormData, mockedAppletId } from 'shared/mock';
+import {
+  mockedActivityId,
+  mockedApplet,
+  mockedAppletFormData,
+  mockedAppletId,
+  mockedCurrentWorkspace,
+  mockedMultiSelectFormValues,
+  mockedSingleSelectFormValues,
+  mockedSliderFormValues,
+} from 'shared/mock';
+import { initialStateData } from 'shared/state';
+import { Roles } from 'shared/consts';
 
 import { ActivityAbout } from './ActivityAbout';
 
 const route = `/builder/${mockedAppletId}/activities/${mockedActivityId}/about`;
 const routePath = page.builderAppletActivityAbout;
 
+const preloadedState = {
+  workspaces: {
+    workspaces: initialStateData,
+    currentWorkspace: {
+      ...initialStateData,
+      ...mockedCurrentWorkspace,
+    },
+    roles: {
+      ...initialStateData,
+      data: {
+        [mockedAppletId]: [Roles.Manager],
+      },
+    },
+    applet: mockedApplet,
+    workspacesRoles: initialStateData,
+  },
+  applet: {
+    applet: {
+      ...initialStateData,
+      data: { result: mockedApplet },
+    },
+  },
+  themes: {
+    themes: {
+      ...initialStateData,
+      data: {
+        result: [
+          {
+            name: 'Default',
+            logo: null,
+            backgroundImage: null,
+            primaryColor: '#0067a0',
+            secondaryColor: '#fff',
+            tertiaryColor: '#404040',
+            id: '9b023afd-e5f9-403c-b154-fc8f35fcf3ab',
+            public: true,
+            allowRename: true,
+          },
+        ],
+      },
+    },
+  },
+};
+
+const appletFormData = {
+  ...mockedAppletFormData,
+  activities: [
+    {
+      ...mockedAppletFormData.activities[0],
+      items: [mockedSingleSelectFormValues, mockedMultiSelectFormValues, mockedSliderFormValues],
+      showAllAtOnce: true,
+      isSkippable: true,
+      responseIsEditable: false,
+    },
+  ],
+};
+
 describe('ActivityAbout', () => {
+  test('should render all fields for a new applet', () => {
+    const route = `/builder/new-applet/activities/${mockedActivityId}/about`;
+    renderWithAppletFormData({
+      children: <ActivityAbout />,
+      appletFormData: mockedAppletFormData,
+      options: { route, routePath, preloadedState },
+    });
+
+    const fieldsDataTestIds = [
+      'builder-activity-about-name',
+      'builder-activity-about-description',
+      'builder-activity-about-image',
+      'builder-activity-about-splash-screen',
+      'builder-activity-about-show-all',
+      'builder-activity-about-skippable',
+      'builder-activity-about-response-editable',
+      'builder-activity-about-reviewable',
+    ];
+
+    fieldsDataTestIds.forEach((dataTestId) =>
+      expect(screen.getByTestId(dataTestId)).toBeInTheDocument(),
+    );
+  });
+
+  test('should render all fields for exisiting applet with default values', () => {
+    renderWithAppletFormData({
+      children: <ActivityAbout />,
+      appletFormData,
+      options: { route, routePath, preloadedState },
+    });
+
+    const activityName = screen.getByLabelText('Activity Name') as HTMLInputElement;
+    expect(activityName.value).toBe('New Activity');
+    const activityDescription = screen.getByLabelText('Activity Description') as HTMLInputElement;
+    expect(activityDescription.value).toBe('');
+    const showAllAtOnce = screen.getByLabelText('Show all questions at once (For web app only)');
+    expect(showAllAtOnce).toBeChecked();
+    const isSkippable = screen.getByLabelText('Allow to skip all items');
+    expect(isSkippable).toBeChecked();
+    const isEditable = screen.getByLabelText(
+      'Disable the respondent\'s ability to change the response',
+    );
+    expect(isEditable).toBeChecked();
+    const isReviewable = screen.getByLabelText(
+      'This Activity is only displayed in the Admin panel for the reviewer to provide responses about respondent\'s data. Only single selection, multiple selection, and slider items are supported.',
+    );
+    expect(isReviewable).not.toBeDisabled();
+  });
+
   test('shouldn\'t turn activity to reviewer one', () => {
     renderWithAppletFormData({
       children: <ActivityAbout />,
