6a9d7b4eb46788e604d3bf1ed06d608f900874fa
Hanna <35298264+wildkiomi@users.noreply.github.com>
2023-12-15 14:53:22 +0400

    M2-4373: M2-4374: Remove and confirm schedule popups (#1297)

---
+++ src/modules/Dashboard/features/Applet/Schedule/ConfirmScheduledAccessPopup/ConfirmScheduledAccessPopup.test.tsx
@@ -0,0 +1,51 @@
+import { fireEvent, screen } from '@testing-library/react';
+
+import { renderWithProviders } from 'shared/utils';
+
+import { ConfirmScheduledAccessPopup } from './ConfirmScheduledAccessPopup';
+
+const onCloseMock = jest.fn();
+const onSubmitMock = jest.fn();
+
+describe('ConfirmScheduledAccessPopup', () => {
+  test('should render and submit', () => {
+    renderWithProviders(
+      <ConfirmScheduledAccessPopup
+        open={true}
+        onClose={onCloseMock}
+        onSubmit={onSubmitMock}
+        isLoading={false}
+        activityName="New Activity"
+      />,
+    );
+
+    const popup = screen.getByTestId('dashboard-calendar-confirm-scheduled-access-popup');
+    expect(popup).toBeInTheDocument();
+    expect(popup).toHaveTextContent(
+      'Activity New Activity will no longer be always available, and the Activity will be a scheduled event. Are you sure you want to continue?',
+    );
+
+    fireEvent.click(screen.getByText('Confirm'));
+
+    expect(onSubmitMock).toBeCalled();
+  });
+
+  test('should show spinner and disable submit when isLoading true', () => {
+    renderWithProviders(
+      <ConfirmScheduledAccessPopup
+        open={true}
+        onClose={onCloseMock}
+        onSubmit={onSubmitMock}
+        isLoading={true}
+        activityName="New Activity"
+      />,
+    );
+
+    expect(screen.getByTestId('spinner')).toBeVisible();
+    expect(screen.getByText('Confirm')).toBeDisabled();
+
+    fireEvent.click(screen.getByText('Cancel'));
+
+    expect(onCloseMock).toBeCalled();
+  });
+});
