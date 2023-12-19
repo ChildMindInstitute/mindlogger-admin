c71879359844dbb3d44ca12caf4bfc791e1dee98
muhurov <muhurov@gmail.com>
2023-12-19 16:46:10 +0300

    M2-4443: fixed Add Audio clickability

--- src/modules/Builder/features/ActivityItems/ItemConfiguration/InputTypeItems/AudioPlayer/AudioPlayer.tsx
+++ src/modules/Builder/features/ActivityItems/ItemConfiguration/InputTypeItems/AudioPlayer/AudioPlayer.tsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Box } from '@mui/material';
 
-import { StyledTitleMedium, StyledBodyErrorText, theme, variables } from 'shared/styles';
+import { theme } from 'shared/styles';
 import { Svg } from 'shared/components/Svg';
-import { MLPlayer } from 'modules/Builder/components/MLPlayer';
+
 import { MediaType } from 'modules/Builder/components/MediaUploader';
 import {
   SharedToggleItemProps,
@@ -12,20 +11,14 @@ import {
 } from 'modules/Builder/components/ToggleItemContainer';
 import { useCustomFormContext } from 'modules/Builder/hooks';
 
-import { AddAudio } from './AddAudio';
-import { UploadAudio } from './UploadAudio';
-import { RecordAudio } from './RecordAudio';
-import { RemoveAudioPopup } from './RemoveAudioPopup';
 import { StyledName, StyledNameWrapper } from './AudioPlayer.styles';
 import { AudioPlayerProps } from './AudioPlayer.types';
 import { getNameByUrl } from './AudioPlayer.utils';
+import { AudioPlayerContent } from './AudioPlayerContent';
 
 export const AudioPlayer = ({ name }: AudioPlayerProps) => {
   const { t } = useTranslation('app');
-  const [isUploadPopupOpened, setUploadPopupOpened] = useState(false);
-  const [isRecordPopupOpened, setRecordPopupOpened] = useState(false);
-  const [isRemoveAudioPopupOpened, setRemoveAudioPopupOpened] = useState(false);
-  const { setValue, watch, trigger, getFieldState } = useCustomFormContext();
+  const { watch, trigger } = useCustomFormContext();
 
   const urlName = `${name}.responseValues.file`;
   const url = watch(urlName);
@@ -33,37 +26,12 @@ export const AudioPlayer = ({ name }: AudioPlayerProps) => {
     url ? { url, name: getNameByUrl(url) } : null,
   );
 
-  const { error } = getFieldState(urlName);
-
   useEffect(() => {
     trigger(urlName);
     url && setMedia({ ...media, name: getNameByUrl(url) });
   }, [url]);
 
-  const onClearMedia = () => {
-    setMedia(null);
-    setValue(urlName, undefined);
-  };
-  const onCloseUploadPopup = () => setUploadPopupOpened(false);
-  const onCloseRecordPopup = () => setRecordPopupOpened(false);
-
-  const onCloseRemoveAudioPopup = () => setRemoveAudioPopupOpened(false);
-
-  const handleUploadAudio = (url?: string) => {
-    setValue(urlName, url ?? media?.url);
-    onCloseUploadPopup();
-  };
-  const handleUploadRecord = (url?: string) => {
-    setValue(urlName, url);
-    onCloseRecordPopup();
-  };
-
-  const handleRemoveAudio = () => {
-    onClearMedia();
-    onCloseRemoveAudioPopup();
-  };
-
-  const HeaderContent = ({ open }: SharedToggleItemProps) =>
+  const HeaderContent = ({ open, media }: SharedToggleItemProps & { media: MediaType | null }) =>
     !open && !!media ? (
       <StyledNameWrapper>
         {media && <Svg id="check" width={18} height={18} />}
@@ -71,49 +39,14 @@ export const AudioPlayer = ({ name }: AudioPlayerProps) => {
       </StyledNameWrapper>
     ) : null;
 
-  const Content = () => (
-    <Box sx={{ mt: theme.spacing(1) }}>
-      <StyledTitleMedium color={variables.palette.on_surface} sx={{ mb: theme.spacing(2.4) }}>
-        {t('audioPlayerDescription')}
-      </StyledTitleMedium>
-      {url ? (
-        <MLPlayer media={media} onRemove={() => setRemoveAudioPopupOpened(true)} />
-      ) : (
-        <AddAudio
-          onUploadAudio={() => setUploadPopupOpened(true)}
-          onRecordAudio={() => setRecordPopupOpened(true)}
-        />
-      )}
-      {error && (
-        <StyledBodyErrorText sx={{ mt: theme.spacing(2.4) }}>{error?.message}</StyledBodyErrorText>
-      )}
-      <UploadAudio
-        open={isUploadPopupOpened}
-        media={media}
-        onChange={setMedia}
-        onUpload={handleUploadAudio}
-        onClose={onCloseUploadPopup}
-      />
-      <RecordAudio
-        open={isRecordPopupOpened}
-        onChange={setMedia}
-        onUpload={handleUploadRecord}
-        onClose={onCloseRecordPopup}
-      />
-      <RemoveAudioPopup
-        open={isRemoveAudioPopupOpened}
-        onClose={onCloseRemoveAudioPopup}
-        onRemove={handleRemoveAudio}
-      />
-    </Box>
-  );
-
   return (
     <>
       <ToggleItemContainer
         title={t('audioPlayer')}
         HeaderContent={HeaderContent}
-        Content={Content}
+        Content={AudioPlayerContent}
+        headerContentProps={{ media }}
+        contentProps={{ media, setMedia, name }}
       />
     </>
   );
