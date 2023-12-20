8ff7c7a8ea068a77239b3d8c5113cf03aa67d9b6
nmialik <nmialik@scnsoft.com>
2023-12-20 12:02:18 +0100

    M2-4457: Unit tests for LinkForm

---
+++ src/modules/Dashboard/features/Applet/AddUser/LinkGenerator/LinkForm/LinkForm.test.tsx
@@ -0,0 +1,126 @@
+import { generatePath } from 'react-router-dom';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
+import mockAxios from 'jest-mock-axios';
+
+import { renderWithProviders } from 'shared/utils';
+
+import { page } from 'resources';
+import { mockedAppletData } from 'shared/mock';
+
+import { LinkForm } from './LinkForm';
+
+const dataTestid = 'dashboard-add-users-generate-link';
+const link = 'https://example.com/join/8bac1a7e-9c72-472a-a722-21ece63af1e1';
+const setInviteLink = jest.fn();
+const getProps = (requireLogin = false) => ({
+  setInviteLink,
+  inviteLink: {
+    link,
+    requireLogin,
+  },
+});
+
+Object.assign(navigator, {
+  clipboard: {
+    writeText: jest.fn(),
+  },
+});
+
+const checkDeleteButtonAndHelperText = () => {
+  expect(screen.getByText('Delete invite link')).toBeInTheDocument();
+  expect(
+    screen.getByText('Delete this link no longer allow anyone to access url'),
+  ).toBeInTheDocument();
+};
+
+describe('LinkForm', () => {
+  test('renders LinkForm component when login is not required, opens popup, calls deleteAppletPublicLink callback', async () => {
+    mockAxios.delete.mockResolvedValueOnce(null);
+    renderWithProviders(<LinkForm {...getProps()} />, {
+      routePath: page.appletAddUser,
+      route: generatePath(page.appletAddUser, {
+        appletId: mockedAppletData.id,
+      }),
+    });
+
+    expect(
+      screen.getByText(
+        'Share the following link for respondents to take assessment without account.',
+      ),
+    ).toBeInTheDocument();
+
+    const inputContainer = screen.getByTestId(`${dataTestid}-url`);
+    expect(inputContainer).toBeInTheDocument();
+
+    const input = inputContainer.querySelector('input') as HTMLElement;
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveValue(link);
+
+    expect(screen.getByTestId(`${dataTestid}-url-copy`)).toBeInTheDocument();
+
+    const deleteButton = screen.getByTestId(`${dataTestid}-url-delete`);
+    expect(deleteButton).toBeInTheDocument();
+
+    checkDeleteButtonAndHelperText();
+
+    fireEvent.click(deleteButton);
+
+    expect(screen.getByTestId(`${dataTestid}-delete-popup`)).toBeInTheDocument();
+
+    const popupDeleteButton = screen.getByTestId(`${dataTestid}-delete-popup-submit-button`);
+    expect(popupDeleteButton).toBeInTheDocument();
+    fireEvent.click(popupDeleteButton);
+
+    expect(mockAxios.delete).toHaveBeenNthCalledWith(
+      1,
+      `/applets/${mockedAppletData.id}/access_link`,
+      {
+        signal: undefined,
+      },
+    );
+
+    await waitFor(() => expect(setInviteLink).toHaveBeenCalled());
+  });
+
+  test('renders LinkForm component when login is required, calls copyPublicLink', () => {
+    renderWithProviders(<LinkForm {...getProps(true)} />);
+
+    expect(
+      screen.getByText('Share the following link to invite anyone to this study.'),
+    ).toBeInTheDocument();
+
+    const inputContainer = screen.getByTestId(`${dataTestid}-url`);
+    expect(inputContainer).toBeInTheDocument();
+
+    const input = inputContainer.querySelector('input') as HTMLElement;
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveValue(link);
+
+    const copyButton = screen.getByTestId(`${dataTestid}-url-copy`);
+    expect(copyButton).toBeInTheDocument();
+    expect(screen.getByTestId(`${dataTestid}-url-delete`)).toBeInTheDocument();
+
+    checkDeleteButtonAndHelperText();
+
+    fireEvent.click(copyButton);
+
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(link);
+  });
+
+  test('renders LinkForm component when inviteLink is null', () => {
+    renderWithProviders(<LinkForm setInviteLink={setInviteLink} inviteLink={null} />);
+
+    expect(
+      screen.getByText(
+        'Share the following link for respondents to take assessment without account.',
+      ),
+    ).toBeInTheDocument();
+
+    const inputContainer = screen.getByTestId(`${dataTestid}-url`);
+    expect(inputContainer).toBeInTheDocument();
+
+    const input = inputContainer.querySelector('input') as HTMLElement;
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveValue('');
+  });
+});
