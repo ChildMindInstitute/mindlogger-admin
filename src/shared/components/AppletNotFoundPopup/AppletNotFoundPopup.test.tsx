54af35e1382964d2a8654d8fe58cdfd29beb69e5
wildkiomi <whisann.lie14@gmail.com>
2023-12-20 14:41:30 +0400

    M2-4426: M2-4427: Unit tests for popups

--- src/shared/components/AppletNotFoundPopup/AppletNotFoundPopup.test.tsx
+++ src/shared/components/AppletNotFoundPopup/AppletNotFoundPopup.test.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
 import { fireEvent, screen } from '@testing-library/react';
 
 import { renderWithProviders } from 'shared/utils';
---
+++ src/shared/features/AppletSettings/ShareAppletSetting/ShareAppletSetting.test.tsx
@@ -0,0 +1,9 @@
+import { renderWithProviders } from 'shared/utils';
+
+import { ShareAppletSetting } from './ShareAppletSetting';
+
+describe('ShareAppletSetting', () => {
+  test('should render', () => {
+    renderWithProviders(<ShareAppletSetting />);
+  });
+});
