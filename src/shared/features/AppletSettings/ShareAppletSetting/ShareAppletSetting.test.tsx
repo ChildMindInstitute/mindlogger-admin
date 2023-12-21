acdedd6f0677aaff832c4072888cffb4ac6e154b
wildkiomi <whisann.lie14@gmail.com>
2023-12-21 12:20:43 +0400

    M2-4426: M2-4427: Popups

--- src/shared/features/AppletSettings/ShareAppletSetting/ShareAppletSetting.test.tsx
+++
@@ -1,9 +0,0 @@
-import { renderWithProviders } from 'shared/utils';
-
-import { ShareAppletSetting } from './ShareAppletSetting';
-
-describe('ShareAppletSetting', () => {
-  test('should render', () => {
-    renderWithProviders(<ShareAppletSetting />);
-  });
-});
