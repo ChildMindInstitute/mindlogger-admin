b00f3d3be971e2856e5fdbb8d9da4ad42387ea9e
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2023-12-27 18:19:50 +0300

    M2-4392: reverted i18n resources

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,13 +1,16 @@
 import { Resource } from 'i18next';
 
+import appEn from './app-en.json';
+import appFr from './app-fr.json';
+
 export const resources: Resource = {
   en: {
     translation: { notUse: 'not-used' },
-    app: await import('./app-en.json'),
+    app: appEn,
   },
   fr: {
     translation: { notUse: 'not-used' },
-    app: await import('./app-fr.json'),
+    app: appFr,
   },
 };
 
