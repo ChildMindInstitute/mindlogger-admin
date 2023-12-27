4057574e9daa74c9aedfdf86774a47b853567f85
IhorNesterenko99 <inesterenko@scnsoft.com>
2023-12-27 18:16:36 +0200

    M2-4513: 'That applet name already exists' error is not displayed on the UI after duplicating (with unit tests)

--- src/modules/Dashboard/features/Applet/Popups/DuplicatePopups/DuplicatePopups.test.tsx
+++ src/modules/Dashboard/features/Applet/Popups/DuplicatePopups/DuplicatePopups.test.tsx
@@ -2,10 +2,11 @@ import { fireEvent, waitFor } from '@testing-library/react';
 import mockAxios from 'jest-mock-axios';
 
 import { renderWithProviders } from 'shared/utils';
-import { mockedApplet, mockedPassword } from 'shared/mock';
+import { mockedApplet, mockedAppletData, mockedPassword } from 'shared/mock';
 import * as encryptionFunctions from 'shared/utils/encryption';
+import * as builderHooks from 'modules/Builder/hooks';
 
-import { DuplicatePopups } from '.';
+import { DuplicatePopups } from './DuplicatePopups';
 
 const preloadedState = {
   popups: {
@@ -27,16 +28,39 @@ const mockedEncryption = {
   accountId: '12345',
 };
 
+jest.mock('modules/Builder/hooks', () => ({
+  ...jest.requireActual('modules/Builder/hooks'),
+  useAppletPrivateKeySetter: jest.fn(),
+}));
+
 describe('DuplicatePopups', () => {
   afterEach(() => {
     mockAxios.reset();
     jest.restoreAllMocks();
   });
 
+  test('should show an error if the name already exists in database', async () => {
+    mockAxios.post.mockResolvedValueOnce({ data: { result: { name: 'name' } } });
+    mockAxios.post.mockResolvedValueOnce({ data: { result: { name: 'name (1)' } } });
+
+    const { getByTestId, getByText } = renderWithProviders(<DuplicatePopups />, {
+      preloadedState,
+    });
+
+    await waitFor(() => {
+      expect(getByTestId('dashboard-applets-duplicate-popup-name')).toBeInTheDocument();
+      fireEvent.click(getByText('Submit'));
+    });
+
+    await waitFor(() => expect(getByText(/That applet name already exists/)).toBeInTheDocument());
+  });
+
   test('should duplicate and open success modal', async () => {
     mockAxios.post.mockResolvedValueOnce({ data: { result: { name: 'name' } } });
-    mockAxios.post.mockResolvedValueOnce(null);
+    mockAxios.post.mockResolvedValueOnce({ data: { result: { name: 'name' } } });
+    mockAxios.post.mockResolvedValueOnce({ data: { result: mockedAppletData } });
     jest.spyOn(encryptionFunctions, 'getEncryptionToServer').mockReturnValue(mockedEncryption);
+    jest.spyOn(builderHooks, 'useAppletPrivateKeySetter').mockReturnValue(() => jest.fn());
 
     const { getByTestId, getByLabelText, getByText } = renderWithProviders(<DuplicatePopups />, {
       preloadedState,
@@ -46,6 +70,7 @@ describe('DuplicatePopups', () => {
       expect(getByTestId('dashboard-applets-duplicate-popup-name')).toBeInTheDocument();
       fireEvent.click(getByText('Submit'));
     });
+
     await waitFor(() => {
       fireEvent.change(getByLabelText('Password'), {
         target: { value: mockedPassword },
