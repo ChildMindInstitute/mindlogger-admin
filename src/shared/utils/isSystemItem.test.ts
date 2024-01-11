bf5b22b4e5ddc91f3fc60dab899f571f2638672f
muhurov <muhurov@gmail.com>
2024-01-11 10:40:13 +0300

    M2-4706: tests fixes

--- src/shared/utils/isSystemItem.test.ts
+++ src/shared/utils/isSystemItem.test.ts
@@ -1,14 +1,18 @@
 import { LookupTableItems } from 'shared/consts';
+import { ElementType } from 'shared/types';
 
 import { isSystemItem } from './isSystemItem';
 
 describe('isSystemItem', () => {
   test.each`
-    name                              | expected | description
-    ${LookupTableItems.Age_screen}    | ${true}  | ${'should return true when name is LookupTableItems.Age_screen'}
-    ${LookupTableItems.Gender_screen} | ${true}  | ${'should return true when name is LookupTableItems.Gender_screen'}
-    ${'name'}                         | ${false} | ${'should return false when name is not a system item'}
-  `('$description', ({ name, expected }) => {
-    expect(isSystemItem(name)).toEqual(expected);
+    name                              | allowEdit | expected | description
+    ${LookupTableItems.Age_screen}    | ${true}   | ${false} | ${'should return false when name is LookupTableItems.Age_screen and item is editable'}
+    ${LookupTableItems.Gender_screen} | ${true}   | ${false} | ${'should return false when name is LookupTableItems.Gender_screen and item is editable'}
+    ${LookupTableItems.Age_screen}    | ${false}  | ${true}  | ${'should return true when name is LookupTableItems.Age_screen and item is not editable'}
+    ${LookupTableItems.Gender_screen} | ${false}  | ${true}  | ${'should return true when name is LookupTableItems.Gender_screen and item is not editable'}
+    ${'name'}                         | ${true}   | ${false} | ${'should return false when name is not a system item and item is editable'}
+    ${'name'}                         | ${false}  | ${false} | ${'should return false when name is not a system item and item is not editable'}
+  `('$description', ({ name, allowEdit, expected }) => {
+    expect(isSystemItem({ name, allowEdit, type: ElementType.Item })).toEqual(expected);
   });
 });
