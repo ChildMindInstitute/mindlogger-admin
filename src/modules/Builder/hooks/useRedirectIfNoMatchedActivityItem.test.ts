9600384eb05265f956d4cac9e345d322a3d8d3c3
muhurov <muhurov@gmail.com>
2024-01-30 13:55:01 +0300

    M2-5002 Unit tests - uncovered Builder hooks (#1443)
    * M2-4437: M2-4736: "Save Changes?" popup appears after saving the Performance task or Single/Multiple Select Item and then try going to the dashboard without changes
    
    * M2-4437: M2-4736: test fixes
    
    * M2-5002: tests for the rest builder hooks
    
    ---------
    
    Co-authored-by: IhorNesterenko99 <inesterenko@scnsoft.com>

---
+++ src/modules/Builder/hooks/useRedirectIfNoMatchedActivityItem.test.ts
@@ -0,0 +1,83 @@
+import { generatePath } from 'react-router-dom';
+import { renderHook } from '@testing-library/react';
+import { v4 as uuidv4 } from 'uuid';
+
+import { page } from 'resources';
+import {
+  mockedActivityId,
+  mockedAppletData,
+  mockedAppletId,
+  mockedSingleSelectFormValues,
+} from 'shared/mock';
+import { Path } from 'shared/utils';
+
+import { useRedirectIfNoMatchedActivityItem } from './useRedirectIfNoMatchedActivityItem';
+
+const mockedItemId = mockedSingleSelectFormValues.id;
+const mockedCommonParams = {
+  appletId: mockedAppletId,
+  activityId: mockedActivityId,
+};
+const mockedParamsWithActivityItem = {
+  ...mockedCommonParams,
+  itemId: mockedItemId,
+};
+const mockedParamsWithoutActivityItem = {
+  ...mockedCommonParams,
+  itemId: uuidv4(),
+};
+const mockedParamsNewAppletWithActivityItem = {
+  ...mockedCommonParams,
+  appletId: Path.NewApplet,
+  itemId: mockedItemId,
+};
+const mockedParamsNewAppletWithoutActivityItem = {
+  ...mockedCommonParams,
+  appletId: Path.NewApplet,
+  itemId: uuidv4(),
+};
+const pathToActivityItems = generatePath(page.builderAppletActivityItems, mockedCommonParams);
+const pathToActivityItemsNewApplet = generatePath(page.builderAppletActivityItems, {
+  ...mockedCommonParams,
+  appletId: Path.NewApplet,
+});
+
+const mockedUseNavigate = jest.fn();
+const mockedUseParams = jest.fn();
+const mockedGetValues = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUseNavigate,
+  useParams: () => mockedUseParams(),
+}));
+jest.mock('react-hook-form', () => ({
+  ...jest.requireActual('react-hook-form'),
+  useFormContext: () => ({
+    getValues: () => mockedGetValues(),
+    watch: () => mockedGetValues(),
+  }),
+}));
+
+describe('useRedirectIfNoMatchedActivityItem', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test.each`
+    params                                      | activityFlows                           | toBeCalledWith                  | description
+    ${mockedParamsWithActivityItem}             | ${mockedAppletData.activities[0].items} | ${undefined}                    | ${'doesn\'t redirect if activity item exists'}
+    ${mockedParamsNewAppletWithActivityItem}    | ${mockedAppletData.activities[0].items} | ${undefined}                    | ${'doesn\'t redirect if applet is new and activity item exists'}
+    ${mockedParamsWithoutActivityItem}          | ${mockedAppletData.activities[0].items} | ${pathToActivityItems}          | ${'should redirect if activity item doesn\'t exist'}
+    ${mockedParamsNewAppletWithoutActivityItem} | ${mockedAppletData.activities[0].items} | ${pathToActivityItemsNewApplet} | ${'should redirect if applet is new and activity item doesn\'t exist'}
+  `('$description', ({ params, activityFlows, toBeCalledWith }) => {
+    mockedUseParams.mockReturnValue(params);
+    mockedGetValues.mockReturnValue(activityFlows);
+
+    renderHook(useRedirectIfNoMatchedActivityItem);
+
+    toBeCalledWith
+      ? expect(mockedUseNavigate).toBeCalledWith(toBeCalledWith)
+      : expect(mockedUseNavigate).not.toBeCalled();
+  });
+});
