fa41600bc390b307076faad2a6aca972002abc3f
nmialik <nmialik@scnsoft.com>
2024-02-13 12:31:20 +0100

    M2-4937: test fixes

--- src/modules/Library/features/AppletsCatalog/AppletsCatalog.test.tsx
+++ src/modules/Library/features/AppletsCatalog/AppletsCatalog.test.tsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import { screen, waitFor, act } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import * as reduxHooks from 'redux/store/hooks';
