a1db969af0c178630632ac8ec7b5fa63f0abde45
nmialik <nmialik@scnsoft.com>
2024-02-15 16:13:49 +0100

    M2-5119: Unit tests for Library/Cart (utils, hooks, component)

---
+++ src/modules/Library/features/Cart/Cart.hooks.test.ts
@@ -0,0 +1,43 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import { act, renderHook } from '@testing-library/react';
+
+import * as reduxHooks from 'redux/store/hooks';
+import { library } from 'redux/modules';
+import { STORAGE_SELECTED_KEY } from 'modules/Library/consts';
+
+import { useClearCart } from './Cart.hooks';
+
+const mockSessionStorageSelectedKey = 'mockSessionStorageSelectedKey';
+const mockDispatch = () => Promise.resolve('');
+
+jest.mock('redux/store/hooks', () => ({
+  useAppDispatch: jest.fn(),
+}));
+
+describe('useClearCart', () => {
+  beforeEach(() => {
+    sessionStorage.setItem(STORAGE_SELECTED_KEY, mockSessionStorageSelectedKey);
+
+    jest.spyOn(reduxHooks, 'useAppDispatch').mockReturnValue(mockDispatch);
+    jest.clearAllMocks();
+  });
+
+  afterEach(() => {
+    sessionStorage.clear();
+  });
+
+  test('removes item from sessionStorage and dispatches thunk', () => {
+    const spyPostAppletsToCarts = jest.spyOn(library.thunk, 'postAppletsToCart');
+    expect(sessionStorage.getItem(STORAGE_SELECTED_KEY)).toEqual(mockSessionStorageSelectedKey);
+
+    const { result } = renderHook(() => useClearCart());
+
+    act(() => {
+      result.current();
+    });
+
+    expect(spyPostAppletsToCarts).toHaveBeenCalledWith([]);
+    expect(sessionStorage.getItem(STORAGE_SELECTED_KEY)).toEqual(null);
+  });
+});
