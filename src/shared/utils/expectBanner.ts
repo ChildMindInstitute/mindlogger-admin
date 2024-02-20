8e4c6fd1cff397056f1f2c8a4695d254184c3b5b
Paul Hoepfner-Homme <read.ishmael@gmail.com>
2024-02-20 23:12:55 +0000

    M2-5309: fix broken tests after rebase
    Also add ability to _not_ expect a banner to the `expectBanner` utility.

--- src/shared/utils/expectBanner.ts
+++ src/shared/utils/expectBanner.ts
@@ -5,8 +5,15 @@ import { AppStore } from 'redux/store';
  * Jest utility to determine if the banner having the given key has been rendered
  * based on the current state of Redux store.
  */
-export const expectBanner = (store: AppStore, key: keyof typeof BannerType) => {
-  expect(
+export const expectBanner = (
+  store: AppStore,
+  key: keyof typeof BannerType,
+  expectPresence = true,
+) => {
+  const expectation = expect(
     store.getState().banners.data.banners.find((payload) => payload.key === key),
-  ).toBeDefined();
+  );
+
+  const matcher = expectPresence ? expectation : expectation.not;
+  matcher.toBeDefined();
 };
