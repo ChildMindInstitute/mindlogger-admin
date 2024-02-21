a293d14677fad5aa437ae00dddddbec5892836c5
nmialik <nmialik@scnsoft.com>
2024-02-21 19:31:08 +0100

    M2-5354: Unit tests for ActivityFlowSettings

---
+++ src/modules/Builder/features/ActivityFlowSettings/ActivityFlowSettings.hooks.test.ts
@@ -0,0 +1,63 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import { useCustomFormContext } from 'modules/Builder/hooks';
+import { renderHookWithProviders } from 'shared/utils';
+import { page } from 'resources';
+
+import { useActivityFlow } from './ActivityFlowSettings.hooks';
+
+const routePath = page.builderAppletActivityFlowItemSettings;
+
+jest.mock('modules/Builder/hooks', () => ({
+  ...jest.requireActual('modules/Builder/hooks'),
+  useCustomFormContext: jest.fn(),
+}));
+
+describe('useActivityFlow', () => {
+  const mockWatch = jest.fn();
+  const mockActivityFlows = [
+    {
+      id: '123',
+      name: 'ActivityFlow 1',
+      description: 'Description 1',
+      items: [{ activityKey: 'activity1' }],
+    },
+    {
+      id: '456',
+      name: 'ActivityFlow 2',
+      description: 'Description 2',
+      items: [{ activityKey: 'activity2' }],
+    },
+  ];
+
+  beforeEach(() => {
+    useCustomFormContext.mockReturnValue({ watch: mockWatch });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('should return currentActivityFlow based on activityFlowId', () => {
+    mockWatch.mockReturnValueOnce(mockActivityFlows);
+
+    const { result } = renderHookWithProviders(useActivityFlow, {
+      route: '/builder/appletId/activity-flows/123/settings',
+      routePath,
+    });
+
+    expect(useCustomFormContext).toHaveBeenCalled();
+    expect(result.current).toEqual(mockActivityFlows[0]);
+  });
+
+  test('should return undefined if activityFlowId is not found', () => {
+    mockWatch.mockReturnValueOnce(mockActivityFlows);
+
+    const { result } = renderHookWithProviders(useActivityFlow, {
+      route: '/builder/appletId/activity-flows/789/settings',
+      routePath,
+    });
+
+    expect(result.current).toBeUndefined();
+  });
+});
---
+++ src/modules/Builder/features/ActivityFlowSettings/ActivityFlowSettings.test.tsx
@@ -0,0 +1,87 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { renderWithProviders } from 'shared/utils';
+import { useCustomFormContext } from 'modules/Builder/hooks';
+import { page } from 'resources';
+
+import { ActivityFlowSettings } from './ActivityFlowSettings';
+
+const mockWatch = jest.fn();
+const mockUseNavigate = jest.fn();
+
+const routePath = page.builderAppletActivityFlowItemSettings;
+const dataTestid = 'builder-activity-flows-settings-report-config';
+const mockActivityFlows = [
+  {
+    id: '123',
+    name: 'ActivityFlow 1',
+    description: 'Description 1',
+    items: [{ activityKey: 'activity1' }],
+  },
+];
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockUseNavigate,
+}));
+
+jest.mock('modules/Builder/hooks', () => ({
+  useRedirectIfNoMatchedActivityFlow: jest.fn(),
+  ...jest.requireActual('modules/Builder/hooks'),
+}));
+
+jest.mock('modules/Builder/hooks', () => ({
+  ...jest.requireActual('modules/Builder/hooks'),
+  useCustomFormContext: jest.fn(),
+  useRedirectIfNoMatchedActivityFlow: jest.fn(),
+}));
+
+const testActivityFlowSettings = async ({ disableButton }) => {
+  expect(screen.getByText('Activity Flow Settings')).toBeInTheDocument();
+  expect(screen.getByText('Reports')).toBeInTheDocument();
+
+  const reportConfig = screen.getByTestId(dataTestid);
+
+  expect(reportConfig).toBeInTheDocument();
+
+  disableButton
+    ? expect(reportConfig).toHaveAttribute('disabled')
+    : expect(reportConfig).not.toHaveAttribute('disabled');
+  expect(screen.getByText('Report Configuration')).toBeInTheDocument();
+
+  if (!disableButton) {
+    await userEvent.click(reportConfig);
+    expect(mockUseNavigate).toHaveBeenCalledWith(
+      '/builder/appletId/activity-flows/123/settings/report-configuration',
+    );
+  }
+};
+
+describe('ActivityFlowSettings', () => {
+  beforeEach(() => {
+    useCustomFormContext.mockReturnValue({ watch: mockWatch });
+    mockWatch.mockReturnValueOnce(mockActivityFlows);
+    jest.clearAllMocks();
+  });
+
+  test('renders ActivityFlowSettings component with mock data and enabled report config', () => {
+    renderWithProviders(<ActivityFlowSettings />, {
+      route: '/builder/appletId/activity-flows/123/settings',
+      routePath,
+    });
+
+    testActivityFlowSettings({ disableButton: false });
+  });
+
+  test('renders ActivityFlowSettings component with mock data and disabled report config', () => {
+    renderWithProviders(<ActivityFlowSettings />, {
+      route: '/builder/appletId/activity-flows/456/settings',
+      routePath,
+    });
+
+    testActivityFlowSettings({ disableButton: true });
+  });
+});
