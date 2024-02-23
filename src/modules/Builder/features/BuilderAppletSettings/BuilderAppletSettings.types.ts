12536d213d483d3428a0d7674bc48cbcce1f6ea0
Tamara Chu <tamara.chu@metalabdesign.com>
2024-02-23 18:11:38 +0000

    Remove unused export

--- src/modules/Builder/features/BuilderAppletSettings/BuilderAppletSettings.types.ts
+++ src/modules/Builder/features/BuilderAppletSettings/BuilderAppletSettings.types.ts
@@ -1,6 +1,5 @@
 import { Roles } from 'shared/consts';
 import { ReportConfigFormValues } from 'modules/Builder/features/ReportConfigSetting';
-import { SingleApplet } from 'redux/modules';
 
 export type GetSettings = {
   appletName?: string;
