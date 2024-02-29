7d2eb8ffbb769625547fb22c1fce6e218ba5ac80
Tamara Chu <tamara.chu@metalabdesign.com>
2024-02-28 21:29:05 -0800

    Remove unused props from ActionsProps type

--- src/shared/components/NavigationMenu/Actions/Actions.types.ts
+++ src/shared/components/NavigationMenu/Actions/Actions.types.ts
@@ -1,8 +1,3 @@
-import { Item, NavigationItem } from '../NavigationMenu.types';
-
 export type ActionsProps = {
-  title?: string;
-  items?: Item[];
   isCompact: boolean;
-  onItemClick?: (value: NavigationItem) => void;
 };
