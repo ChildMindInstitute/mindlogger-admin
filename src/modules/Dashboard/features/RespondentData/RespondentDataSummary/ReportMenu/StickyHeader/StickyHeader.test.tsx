1394ecb946c2fd86367fc433f6f3bc33e217a8bd
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2024-03-12 17:44:43 +0300

    M2-4660: updated naming

--- src/modules/Dashboard/features/RespondentData/RespondentDataSummary/ReportMenu/StickyHeader/StickyHeader.test.tsx
+++ src/modules/Dashboard/features/RespondentData/RespondentDataSummary/ReportMenu/StickyHeader/StickyHeader.test.tsx
@@ -51,13 +51,16 @@ describe('StickyHeader', () => {
       },
     );
 
-    const c1 = getByText('Activities');
-    expect(c1).toBeInTheDocument();
-    expect(c1.parentElement?.parentElement).toHaveStyle({ minHeight: '9.6rem', boxShadow: 'none' });
-    expect(c1).toHaveStyle({ fontSize: '3.2rem' });
+    const title = getByText('Activities');
+    expect(title).toBeInTheDocument();
+    expect(title.parentElement?.parentElement).toHaveStyle({
+      minHeight: '9.6rem',
+      boxShadow: 'none',
+    });
+    expect(title).toHaveStyle({ fontSize: '3.2rem' });
 
-    const c2 = getByText('User: 3921968c-3903-4872-8f30-a6e6a10cef36 (Mocked Respondent)');
-    expect(c2).toBeInTheDocument();
-    expect(c2).toHaveStyle({ position: 'relative', padding: 0 });
+    const description = getByText('User: 3921968c-3903-4872-8f30-a6e6a10cef36 (Mocked Respondent)');
+    expect(description).toBeInTheDocument();
+    expect(description).toHaveStyle({ position: 'relative', padding: 0 });
   });
 });
