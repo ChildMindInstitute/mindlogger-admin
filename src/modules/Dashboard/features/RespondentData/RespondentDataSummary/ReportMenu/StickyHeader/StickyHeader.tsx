8e74d632eb6a6cc359132fe0e351bea429c3d44e
Aleksandr Eremin <aleksandr.i.eremin@yandex.ru>
2024-03-12 17:55:02 +0300

    M2-4660: [Admin Panel] Summary Tab: the header ‘Activities' and 'Respondent: [Secret User ID] ([First Name] [Last Name])’ are NOT fixed to the top of the panel (#1558)
    * M2-4660: setup sticky header for activity menu in summary tab in dataviz
    
    * M2-4660: moved sticky header from report menu to the separate component to reduce activity list rerender
    
    * M2-4660: added tests
    
    * M2-4660: added tests
    
    * M2-4660: updated naming
    
    * M2-4660: fixed non-null assertion

---
+++ src/modules/Dashboard/features/RespondentData/RespondentDataSummary/ReportMenu/StickyHeader/StickyHeader.tsx
@@ -0,0 +1,48 @@
+import { useTranslation } from 'react-i18next';
+
+import { ContentWithTooltip, DataTableItem } from 'shared/components';
+import { StyledStickyHeader, theme, StyledStickyHeadline, variables } from 'shared/styles';
+import {
+  useRespondentLabel,
+  useHeaderSticky,
+  OFFSET_TO_SET_STICKY,
+  OFFSET_TO_UNSET_STICKY,
+} from 'shared/hooks';
+
+import { StickyHeaderProps } from './StickyHeader.types';
+import { StyledHeaderContainer } from './StickyHeader.styles';
+
+export const StickyHeader = ({
+  containerRef,
+  'data-testid': dataTestid = '',
+}: StickyHeaderProps) => {
+  const { t } = useTranslation();
+  const respondentLabel = useRespondentLabel();
+  const isHeaderSticky = useHeaderSticky(
+    containerRef,
+    OFFSET_TO_SET_STICKY / 2,
+    OFFSET_TO_UNSET_STICKY / 2,
+  );
+  const contentTestId = `sticky-header-content-${dataTestid}`;
+
+  return (
+    <StyledStickyHeader
+      isSticky={isHeaderSticky}
+      sx={{
+        p: theme.spacing(0, 4),
+      }}
+    >
+      <StyledHeaderContainer isSticky={isHeaderSticky}>
+        <StyledStickyHeadline isSticky={isHeaderSticky} color={variables.palette.on_surface}>
+          {t('activities')}
+        </StyledStickyHeadline>
+        <ContentWithTooltip
+          value={respondentLabel}
+          item={{ label: respondentLabel } as DataTableItem}
+          styles={{ position: 'relative', padding: 0 }}
+          data-testid={contentTestId}
+        />
+      </StyledHeaderContainer>
+    </StyledStickyHeader>
+  );
+};
