e7cd5c718db3de3a9bfb87e8eebe68bdf2605130
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2024-03-26 14:01:39 +0300

    M2-5915: added validation for items in usage

--- src/modules/Builder/features/ActivityItemsFlow/ItemFlow/SummaryRow/SummaryRow.utils.ts
+++ src/modules/Builder/features/ActivityItemsFlow/ItemFlow/SummaryRow/SummaryRow.utils.ts
@@ -2,6 +2,7 @@ import i18n from 'i18n';
 import { ItemFormValues } from 'modules/Builder/types';
 import { ConditionalLogicMatch } from 'shared/consts';
 import { getEntityKey } from 'shared/utils';
+import { ConditionalLogic } from 'shared/state/Applet';
 
 import { ITEMS_RESPONSE_TYPES_TO_SHOW } from './SummaryRow.const';
 
@@ -18,10 +19,34 @@ export const getMatchOptions = () => [
   },
 ];
 
-export const getItemsOptions = (items: ItemFormValues[]) =>
+export const getItemsOptions = ({
+  items,
+  itemsInUsage,
+}: {
+  items: ItemFormValues[];
+  itemsInUsage: Set<unknown>;
+}) =>
   items?.reduce((optionList: { value: string; labelKey: string }[], item) => {
-    if (item.responseType && ITEMS_RESPONSE_TYPES_TO_SHOW.includes(item.responseType))
-      return [...optionList, { value: getEntityKey(item), labelKey: item.name }];
+    if (item.responseType && ITEMS_RESPONSE_TYPES_TO_SHOW.includes(item.responseType)) {
+      const value = getEntityKey(item);
+      const disabled = itemsInUsage.has(value);
+      const tooltip = disabled ? t('conditionalLogicValidation.usageInSummaryRow') : undefined;
+
+      return [...optionList, { value, labelKey: item.name, disabled, tooltip }];
+    }
 
     return optionList;
   }, []);
+
+export const getItemsInUsage = ({
+  conditionalLogic,
+  itemKey,
+}: {
+  conditionalLogic: ConditionalLogic[];
+  itemKey: string;
+}) =>
+  conditionalLogic.reduce((acc, conditional) => {
+    if (!conditional.itemKey || conditional.itemKey === itemKey) return acc;
+
+    return acc.add(conditional.itemKey);
+  }, new Set());
