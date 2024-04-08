8fb565bd3b9f6574da743af5a84a872a842c3fb7
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2024-04-08 16:34:38 +0300

    M2-6146: updated unit tests

--- src/modules/Builder/features/ActivityItems/ItemConfiguration/__mocks__/mock.tsx
+++ src/modules/Builder/features/ActivityItems/ItemConfiguration/__mocks__/mock.tsx
@@ -602,8 +602,6 @@ export const mockedItemTypes = [
 ];
 export const mockedItemTypeGroups = ['select', 'matrixSelect', 'input', 'record', 'display'];
 export const mockedItemTypesMobileOnly = [
-  'singleSelectRows',
-  'multiSelectRows',
   'sliderRows',
   'drawing',
   'photo',
