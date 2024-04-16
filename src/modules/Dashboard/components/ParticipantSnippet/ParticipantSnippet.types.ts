600f74a75371e41c426b85b4b59f5c793bc0ae90
farmerpaul <paul.hh@metalab.com>
2024-04-16 16:53:02 -0300

    M2-6246: fix tsc errors
    Assign ParticipantSnippetProps default type argument in props definition
    to simplify usage.

--- src/modules/Dashboard/components/ParticipantSnippet/ParticipantSnippet.types.ts
+++ src/modules/Dashboard/components/ParticipantSnippet/ParticipantSnippet.types.ts
@@ -1,7 +1,8 @@
 import { BoxProps } from '@mui/material';
 import { ElementType } from 'react';
+import { BoxTypeMap } from '@mui/system';
 
-export type ParticipantSnippetProps<T extends ElementType> = {
+export type ParticipantSnippetProps<T extends ElementType = BoxTypeMap['defaultComponent']> = {
   secretId?: string | null;
   nickname?: string | null;
   tag?: string | null;
