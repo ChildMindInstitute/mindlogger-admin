975624833ce43f362b973453da529513489bc226
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2024-04-20 11:32:22 +0300

    M2-5522: added navigation from builder without prompt when applet was removed

--- src/shared/components/AppletNotFoundPopup/AppletNotFoundPopup.hooks.tsx
+++ src/shared/components/AppletNotFoundPopup/AppletNotFoundPopup.hooks.tsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import { alerts, applet } from 'shared/state';
 import { useAppDispatch } from 'redux/store';
 import { page } from 'resources';
 import { DEFAULT_ROWS_PER_PAGE } from 'shared/consts';
+import { LocationStateKeys } from 'shared/types';
 import { useCheckIfAppletHasNotFoundError } from 'shared/hooks/useCheckIfAppletHasNotFoundError';
 
 export const useNotFoundPopup = () => {
   const [appletNotFoundPopupVisible, setAppletNotFoundPopupVisible] = useState(false);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
   const hasNotFoundError = useCheckIfAppletHasNotFoundError();
 
   const handleClose = () => {
@@ -21,7 +23,9 @@ export const useNotFoundPopup = () => {
     dispatch(applet.actions.resetApplet());
     dispatch(alerts.actions.resetAlerts());
     dispatch(alerts.thunk.getAlerts({ limit: DEFAULT_ROWS_PER_PAGE }));
-    navigate(page.dashboardApplets);
+    navigate(page.dashboardApplets, {
+      state: { ...location.state, [LocationStateKeys.ShouldNavigateWithoutPrompt]: true },
+    });
   };
 
   useEffect(() => {
--- src/shared/components/NoPermissionPopup/NoPermissionPopup.hooks.ts
+++ src/shared/components/NoPermissionPopup/NoPermissionPopup.hooks.ts
@@ -1,11 +1,13 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 
-import { alerts, applet } from 'shared/state';
+import { alerts, applet, workspaces } from 'shared/state';
 import { popups } from 'modules/Dashboard/state';
 import { useAppDispatch } from 'redux/store';
 import { page } from 'resources';
 import { DEFAULT_ROWS_PER_PAGE } from 'shared/consts';
+import { LocationStateKeys } from 'shared/types';
 import { forbiddenState } from 'shared/state/ForbiddenState';
+import { Workspace, auth } from 'redux/modules';
 import {
   checkIfAppletUrlPassed,
   checkIfDashboardAppletsUrlPassed,
@@ -20,13 +22,25 @@ export const useNoPermissionPopup = (): UseNoPermissionPopupReturn => {
   const { pathname } = useLocation();
   const isDashboardApplets = checkIfDashboardAppletsUrlPassed(pathname);
   const isBuilder = checkIfAppletUrlPassed(pathname);
+  const userData = auth.useData();
+  const { id } = userData?.user || {};
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     dispatch(forbiddenState.actions.clearForbiddenError());
     dispatch(applet.actions.resetApplet());
     dispatch(alerts.actions.resetAlerts());
     dispatch(alerts.thunk.getAlerts({ limit: DEFAULT_ROWS_PER_PAGE }));
     dispatch(popups.actions.resetPopupsVisibility());
+    const result = await dispatch(workspaces.thunk.getWorkspaces());
+
+    if (workspaces.thunk.getWorkspaces.fulfilled.match(result)) {
+      const workspacesData = result.payload.data.result as Workspace[];
+      const workspace = workspacesData.find((item) => item.ownerId === id);
+      dispatch(workspaces.actions.setCurrentWorkspace(workspace ?? null));
+      navigate(page.dashboardApplets, { state: { [LocationStateKeys.Workspace]: workspace } });
+
+      return;
+    }
 
     if (isDashboardApplets) {
       window.location.reload();
