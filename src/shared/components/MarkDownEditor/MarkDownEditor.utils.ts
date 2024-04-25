11c841715c99d0f065c369a2c6259d2491d4f46c
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2024-04-25 21:43:11 +0300

    M2-6365: made images in markdown lazy loaded

--- src/shared/components/MarkDownEditor/MarkDownEditor.utils.ts
+++ src/shared/components/MarkDownEditor/MarkDownEditor.utils.ts
@@ -106,6 +106,7 @@ config({
         md.renderer.rules.image = function (tokens, idx, options, env, self) {
           const token = tokens[idx];
           const src = token.attrGet('src');
+          token.attrPush(['loading', 'lazy']);
           const description = token.children?.[0]?.content;
 
           if (src && VIDEO_LINK_REGEX.test(src)) {
