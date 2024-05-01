d13cf413b1b7322b1764d420a5d09c9b4dac292f
Marty <2614025+mbanting@users.noreply.github.com>
2024-05-01 01:15:37 -0700

    M2-6263: disabled ts check

--- src/modules/Dashboard/components/ActivityGrid/ActivityGrid.hooks.test.tsx
+++ src/modules/Dashboard/components/ActivityGrid/ActivityGrid.hooks.test.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import { renderHook } from '@testing-library/react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { isValidElement } from 'react';
