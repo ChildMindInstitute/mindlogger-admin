98523b51d5e6fce9a2894a825163eae5adcba9fa
Carlos Chacon <carlos.chacon@metalab.com>
2024-05-03 11:36:51 -0600

    M2-6472: Add new feature flags

--- src/shared/types/featureFlags.ts
+++ src/shared/types/featureFlags.ts
@@ -5,6 +5,10 @@ import { LDFlagValue } from 'launchdarkly-react-client-sdk';
 export const FeatureFlagsKeys = {
   enableMultiInformant: 'enableMultiInformant',
   enableMultiInformantTakeNow: 'enableMultiInformantTakeNow',
+  // TODO: M2-6519 Activity Filter Sort flag cleanup
+  enableActivityFilterSort: 'enableActivityFilterSort',
+  // TODO: M2-6518 Assign Activity flag cleanup
+  enableActivityAssign: 'enableActivityAssign',
 };
 
 export type FeatureFlags = Partial<Record<keyof typeof FeatureFlagsKeys, LDFlagValue>>;
