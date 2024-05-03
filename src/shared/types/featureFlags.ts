bac0fd88be13a449378117e9b5f19a231eb7074d
Carlos Chacon <carlos.chacon@metalab.com>
2024-05-03 17:22:55 -0600

    Update src/shared/types/featureFlags.ts
    Co-authored-by: Kenroy Gobourne <kenroy.gobourne@gmail.com>

--- src/shared/types/featureFlags.ts
+++ src/shared/types/featureFlags.ts
@@ -5,7 +5,7 @@ import { LDFlagValue } from 'launchdarkly-react-client-sdk';
 export const FeatureFlagsKeys = {
   enableMultiInformant: 'enableMultiInformant',
   enableMultiInformantTakeNow: 'enableMultiInformantTakeNow',
-  // TODO: M2-6519 Activity Filter Sort flag cleanup
+  // TODO: https://mindlogger.atlassian.net/browse/M2-6519 Activity Filter Sort flag cleanup
   enableActivityFilterSort: 'enableActivityFilterSort',
   // TODO: M2-6518 Assign Activity flag cleanup
   enableActivityAssign: 'enableActivityAssign',
