a0048686312acc7bd8991ff9355d582108053141
nmialik <nmialik@scnsoft.com>
2024-05-15 01:16:46 +0200

    M2-6543: Managers tab: pressing 'Back' button

--- src/modules/Dashboard/features/Managers/Popups/ManagersRemoveAccessPopup/ManagersRemoveAccessPopup.test.tsx
+++ src/modules/Dashboard/features/Managers/Popups/ManagersRemoveAccessPopup/ManagersRemoveAccessPopup.test.tsx
@@ -1,17 +1,24 @@
-import { waitFor, screen, fireEvent } from '@testing-library/react';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import mockAxios from 'jest-mock-axios';
 
 import { renderWithProviders } from 'shared/utils/renderWithProviders';
 import { mockedAppletId } from 'shared/mock';
 import { page } from 'resources';
+import * as useAsyncHooks from 'shared/hooks/useAsync';
 
 import { ManagersRemoveAccessPopup } from '.';
 
 const refetchManagersMock = jest.fn();
 const onCloseMock = jest.fn();
 
-const route = `/dashboard/${mockedAppletId}/managers`;
-const routePath = page.appletManagers;
+const dataTestid = 'dashboard-managers-remove-access-popup';
+const routeWithAppletId = `/dashboard/${mockedAppletId}/managers`;
+const routePathWithAppletId = page.appletManagers;
+const routeWithoutAppletId = '/dashboard/managers';
+const routePathWithoutAppletId = page.dashboardManagers;
 const user = {
   id: '1',
   firstName: 'firstName',
@@ -36,18 +43,78 @@ describe('ManagersRemoveAccessPopup component tests', () => {
   test('ManagersRemoveAccessPopup should open with applets list', async () => {
     renderWithProviders(<ManagersRemoveAccessPopup {...commonProps} />);
 
-    await waitFor(() => expect(screen.getByText(user.applets[0].displayName)).toBeInTheDocument());
+    expect(screen.getByText(user.applets[0].displayName)).toBeInTheDocument();
   });
 
   test('ManagersRemoveAccessPopup should remove access with appletId', async () => {
     mockAxios.delete.mockResolvedValueOnce(null);
 
     renderWithProviders(<ManagersRemoveAccessPopup {...commonProps} />, {
-      route,
-      routePath,
+      route: routeWithAppletId,
+      routePath: routePathWithAppletId,
     });
 
-    fireEvent.click(screen.getByText('Remove'));
-    await waitFor(() => expect(screen.getByText('Ok')).toBeInTheDocument());
+    const backButton = screen.queryByRole('button', {
+      name: 'Back',
+    });
+    expect(backButton).not.toBeInTheDocument();
+
+    await userEvent.click(screen.getByText('Remove'));
+    expect(screen.getByText('Ok')).toBeInTheDocument();
+  });
+
+  test('ManagersRemoveAccessPopup should remove access without appletId', async () => {
+    const useAsyncSpy = jest.spyOn(useAsyncHooks, 'useAsync');
+    const mockExecute = jest.fn();
+    useAsyncSpy.mockImplementation(() => ({
+      execute: mockExecute,
+      setError: jest.fn(),
+      error: 'Error message',
+    }));
+
+    renderWithProviders(<ManagersRemoveAccessPopup {...commonProps} />, {
+      route: routeWithoutAppletId,
+      routePath: routePathWithoutAppletId,
+    });
+
+    // step 1
+    expect(
+      screen.queryByRole('button', {
+        name: 'Back',
+      }),
+    ).not.toBeInTheDocument();
+
+    const removeAccessButton = screen.getByRole('button', {
+      name: 'Remove Access',
+    });
+    expect(removeAccessButton).toBeDisabled();
+
+    const checkbox = screen.getByTestId(`${dataTestid}-checkbox-0`);
+    const input = checkbox.querySelector('input') as HTMLInputElement;
+
+    await userEvent.click(input);
+    expect(removeAccessButton).not.toBeDisabled();
+
+    await userEvent.click(removeAccessButton);
+
+    // step 2
+    expect(
+      screen.getByRole('button', {
+        name: 'Back',
+      }),
+    ).toBeInTheDocument();
+
+    await userEvent.click(
+      screen.getByRole('button', {
+        name: 'Remove',
+      }),
+    );
+
+    expect(mockExecute).toHaveBeenCalledWith({
+      appletIds: ['2e46fa32-ea7c-4a76-b49b-1c97d795bb9a'],
+      userId: '1',
+    });
+
+    expect(screen.getByText('Error message')).toBeInTheDocument();
   });
 });
