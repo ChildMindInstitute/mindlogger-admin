1eda5ca6a578fbe7d0a6195a1b0ace543c622a03
IhorNesterenko99 <inesterenko@scnsoft.com>
2024-05-16 18:39:12 +0300

    feat: Summary tab Report Menu added Activity Flows support (M2-6537)

--- src/modules/Dashboard/features/RespondentData/RespondentDataSummary/ReportMenu/ReportMenu.test.tsx
+++ src/modules/Dashboard/features/RespondentData/RespondentDataSummary/ReportMenu/ReportMenu.test.tsx
@@ -4,13 +4,21 @@ import * as reactHookForm from 'react-hook-form';
 import { endOfDay, startOfDay, subDays } from 'date-fns';
 
 import { ReportMenu } from './ReportMenu';
+import { ReportMenuProps } from './ReportMenu.types';
 
 jest.mock('shared/hooks/useRespondentLabel', () => ({
   useRespondentLabel: () => 'user: Jane Doe',
 }));
 
 const mockedActivity = {
-  id: 'd65e8a64-a023-4830-9c84-7433c4b96440',
+  id: 'activity-2',
+  name: 'Activity 1',
+  isPerformanceTask: false,
+  hasAnswer: true,
+};
+
+const mockedFlow = {
+  id: 'flow-2',
   name: 'Activity 1',
   isPerformanceTask: false,
   hasAnswer: true,
@@ -33,20 +41,46 @@ const mockActivities = [
   },
 ];
 
+const mockFlows = [
+  {
+    id: 'flow-1',
+    name: 'Flow 1',
+    hasAnswer: true,
+    lastAnswerDate: '2023-09-26T10:10:05.162083',
+  },
+  {
+    id: 'flow-2',
+    name: 'Flow 2',
+    hasAnswer: true,
+    lastAnswerDate: '2023-09-25T10:10:05.162083',
+  },
+];
+
 const mockedSetValue = jest.fn();
 const mockSetIsLoading = jest.fn();
 const mockGetIdentifiersVersions = jest.fn();
 const mockFetchAnswers = jest.fn();
 
-const renderReportMenu = () =>
+const renderReportMenu = (props?: Partial<ReportMenuProps>) =>
   render(
     <ReportMenu
       activities={mockActivities}
+      flows={[]}
       getIdentifiersVersions={mockGetIdentifiersVersions}
       fetchAnswers={mockFetchAnswers}
       setIsLoading={mockSetIsLoading}
+      {...props}
     />,
   );
+
+const testActivities = () => {
+  expect(screen.getByText('Activities')).toBeInTheDocument();
+  expect(screen.getByText('Activity 1')).toBeInTheDocument();
+  expect(screen.getByText('Activity 2')).toBeInTheDocument();
+
+  const activityElements = screen.getAllByTestId(/^respondents-summary-activity/);
+  expect(activityElements.length).toBe(2);
+};
 
 describe('ReportMenu Component', () => {
   beforeEach(() => {
@@ -55,26 +89,43 @@ describe('ReportMenu Component', () => {
     jest.spyOn(reactHookForm, 'useFormContext').mockReturnValue({ setValue: mockedSetValue });
   });
 
-  test('renders the component with activities', () => {
+  test('renders the component with activities and empty flows', () => {
     jest.spyOn(reactHookForm, 'useWatch').mockReturnValue([mockedActivity]);
     renderReportMenu();
 
-    expect(screen.getByText('Activities')).toBeInTheDocument();
-    expect(screen.getByText('Activity 1')).toBeInTheDocument();
-    expect(screen.getByText('Activity 2')).toBeInTheDocument();
+    testActivities();
+    const preselectedActivity = screen.getByTestId('respondents-summary-activity-1');
+    expect(preselectedActivity).toHaveStyle({ backgroundColor: '#e8f0f7' });
 
-    const activityElements = screen.getAllByTestId(/^respondents-summary-activity/);
-    expect(activityElements.length).toBe(2);
+    expect(screen.queryByText('Activity Flows')).not.toBeInTheDocument();
   });
 
-  test('invokes set selected activity when an activity is clicked', async () => {
+  test('renders the component with activities and flows', () => {
+    jest.spyOn(reactHookForm, 'useWatch').mockReturnValue([mockedActivity]);
+    renderReportMenu({ flows: mockFlows });
+
+    testActivities();
+
+    expect(screen.getByText('Activity Flows')).toBeInTheDocument();
+    expect(screen.getByText('Flow 1')).toBeInTheDocument();
+    expect(screen.getByText('Flow 2')).toBeInTheDocument();
+
+    const flowElements = screen.getAllByTestId(/^respondents-summary-flow/);
+    expect(flowElements.length).toBe(2);
+  });
+
+  test('invokes set selected entity when an Activity is clicked', async () => {
     jest.spyOn(reactHookForm, 'useWatch').mockReturnValue([mockedActivity]);
     renderReportMenu();
 
     const activityElement = screen.getByTestId('respondents-summary-activity-1');
     await userEvent.click(activityElement);
 
-    expect(mockedSetValue).toHaveBeenCalledWith('selectedActivity', mockActivities[1]);
+    const chosenActivity = {
+      ...mockActivities[1],
+      isFlow: false,
+    };
+    expect(mockedSetValue).toHaveBeenCalledWith('selectedEntity', chosenActivity);
 
     //set startDate end endDate to 1 week from the most recent response
     const expectedEndDate = endOfDay(new Date('2023-09-25'));
@@ -83,8 +134,33 @@ describe('ReportMenu Component', () => {
     expect(mockedSetValue).toHaveBeenCalledWith('endDate', expectedEndDate);
 
     expect(mockSetIsLoading).toHaveBeenCalledWith(true);
-    expect(mockGetIdentifiersVersions).toHaveBeenCalledWith({ activity: mockActivities[1] });
-    expect(mockFetchAnswers).toHaveBeenCalledWith({ activity: mockActivities[1] });
+    expect(mockGetIdentifiersVersions).toHaveBeenCalledWith({ entity: chosenActivity });
+    expect(mockFetchAnswers).toHaveBeenCalledWith({ entity: chosenActivity });
+    expect(mockSetIsLoading).toHaveBeenCalledWith(false);
+  });
+
+  test('invokes set selected entity when Flow is clicked', async () => {
+    jest.spyOn(reactHookForm, 'useWatch').mockReturnValue([mockedFlow]);
+    renderReportMenu({ flows: mockFlows });
+
+    const flowElement = screen.getByTestId('respondents-summary-flow-0');
+    await userEvent.click(flowElement);
+
+    const chosenFlow = {
+      ...mockFlows[0],
+      isFlow: true,
+    };
+    expect(mockedSetValue).toHaveBeenCalledWith('selectedEntity', chosenFlow);
+
+    //set startDate end endDate to 1 week from the most recent response
+    const expectedEndDate = endOfDay(new Date('2023-09-26'));
+    const expectedStartDate = startOfDay(subDays(expectedEndDate, 6));
+    expect(mockedSetValue).toHaveBeenCalledWith('startDate', expectedStartDate);
+    expect(mockedSetValue).toHaveBeenCalledWith('endDate', expectedEndDate);
+
+    expect(mockSetIsLoading).toHaveBeenCalledWith(true);
+    expect(mockGetIdentifiersVersions).toHaveBeenCalledWith({ entity: chosenFlow });
+    expect(mockFetchAnswers).toHaveBeenCalledWith({ entity: chosenFlow });
     expect(mockSetIsLoading).toHaveBeenCalledWith(false);
   });
 });
