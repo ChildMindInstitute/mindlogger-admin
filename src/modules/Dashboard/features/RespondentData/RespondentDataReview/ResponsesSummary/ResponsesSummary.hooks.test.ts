1a6e1830e59890126573f2c84dc3102f8b25d3aa
IhorNesterenko99 <inesterenko@scnsoft.com>
2024-05-16 18:17:10 +0300

    fix: Responses Summary - changed endDateTime to endDatetime (M2-6537)

--- src/modules/Dashboard/features/RespondentData/RespondentDataReview/ResponsesSummary/ResponsesSummary.hooks.test.ts
+++ src/modules/Dashboard/features/RespondentData/RespondentDataReview/ResponsesSummary/ResponsesSummary.hooks.test.ts
@@ -3,14 +3,14 @@ import { renderHookWithProviders } from 'shared/utils/renderHookWithProviders';
 import { useResponsesSummary } from './ResponsesSummary.hooks';
 
 describe('useResponsesSummary', () => {
-  const endDateTime = '2024-04-10T10:00:00';
+  const endDatetime = '2024-04-10T10:00:00';
   const createdAt = '2024-04-10T09:00:00';
   const identifier = 'test-identifier';
   const version = '1.0';
 
   test('should return a correct array with review description objects', () => {
     const { result } = renderHookWithProviders(() =>
-      useResponsesSummary({ endDateTime, createdAt, identifier, version }),
+      useResponsesSummary({ endDatetime, createdAt, identifier, version }),
     );
 
     expect(result.current).toEqual([
