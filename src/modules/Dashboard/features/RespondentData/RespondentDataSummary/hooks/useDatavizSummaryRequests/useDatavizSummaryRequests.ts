605b66b23bef1e1dae2967be3d59b0b91c15dc43
IhorNesterenko99 <inesterenko@scnsoft.com>
2024-05-17 10:12:38 +0300

    feat: added support of Flow useDatavizSummaryRequests hook (M2-6537)

--- src/modules/Dashboard/features/RespondentData/RespondentDataSummary/hooks/useDatavizSummaryRequests/useDatavizSummaryRequests.ts
+++ src/modules/Dashboard/features/RespondentData/RespondentDataSummary/hooks/useDatavizSummaryRequests/useDatavizSummaryRequests.ts
@@ -1,7 +1,14 @@
 import { useParams } from 'react-router-dom';
 import { useFormContext } from 'react-hook-form';
 
-import { getIdentifiersApi, getVersionsApi } from 'api';
+import {
+  getActivityIdentifiersApi,
+  getFlowIdentifiersApi,
+  getActivityVersionsApi,
+  getFlowVersionsApi,
+  Identifier,
+  Version,
+} from 'api';
 import { RespondentsDataFormValues } from 'modules/Dashboard/features/RespondentData/RespondentData.types';
 
 import { GetIdentifiersVersions } from '../../RespondentDataSummary.types';
@@ -12,26 +19,53 @@ export const useDatavizSummaryRequests = () => {
   const getDecryptedIdentifiers = useDecryptedIdentifiers();
   const { setValue } = useFormContext<RespondentsDataFormValues>();
 
-  const getIdentifiersVersions = async ({ activity }: GetIdentifiersVersions) => {
-    try {
-      if (!appletId || !respondentId || !activity?.hasAnswer || activity?.isPerformanceTask) return;
+  const setIdentifiers = async (identifiers: Identifier[]) => {
+    if (!getDecryptedIdentifiers) return;
 
-      const identifiers = await getIdentifiersApi({
+    const decryptedIdentifiers = await getDecryptedIdentifiers(identifiers);
+    setValue('identifiers', decryptedIdentifiers);
+  };
+
+  const setVersions = (versions: Version[]) => {
+    const versionsFilter = versions.map(({ version }) => ({
+      id: version,
+      label: version,
+    }));
+    setValue('versions', versionsFilter);
+    setValue('apiVersions', versions);
+  };
+
+  const getIdentifiersVersions = async ({ entity }: GetIdentifiersVersions) => {
+    try {
+      if (!appletId || !respondentId || !entity?.hasAnswer) return;
+
+      if (entity.isFlow) {
+        const flowId = entity.id;
+        const identifiers = await getFlowIdentifiersApi({
+          appletId,
+          flowId,
+          respondentId,
+        });
+        await setIdentifiers(identifiers.data.result);
+
+        const versions = await getFlowVersionsApi({ appletId, flowId });
+        setVersions(versions.data.result);
+
+        return;
+      }
+
+      if (entity?.isPerformanceTask) return;
+
+      const activityId = entity.id;
+      const identifiers = await getActivityIdentifiersApi({
         appletId,
-        activityId: activity.id,
+        activityId,
         respondentId,
       });
-      if (!getDecryptedIdentifiers) return;
-      const decryptedIdentifiers = await getDecryptedIdentifiers(identifiers.data.result);
-      setValue('identifiers', decryptedIdentifiers);
+      await setIdentifiers(identifiers.data.result);
 
-      const versions = await getVersionsApi({ appletId, activityId: activity.id });
-      const versionsFilter = versions.data.result?.map(({ version }) => ({
-        id: version,
-        label: version,
-      }));
-      setValue('versions', versionsFilter);
-      setValue('apiVersions', versions.data.result);
+      const versions = await getActivityVersionsApi({ appletId, activityId });
+      setVersions(versions.data.result);
     } catch (error) {
       console.warn(error);
     }
