c49d15e431c710f53e052c967e8851d7cb947472
IhorNesterenko99 <inesterenko@scnsoft.com>
2024-05-17 10:13:12 +0300

    feat: added support of Flow to RespondentDataSummary (M2-6537)

--- src/modules/Dashboard/features/RespondentData/RespondentDataSummary/RespondentDataSummary.tsx
+++ src/modules/Dashboard/features/RespondentData/RespondentDataSummary/RespondentDataSummary.tsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useFormContext, useWatch } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
 
 import { StyledContainer } from 'shared/styles';
-import { DatavizActivity, getSummaryActivitiesApi } from 'api';
+import { DatavizEntity, getSummaryActivitiesApi, getSummaryFlowsApi } from 'api';
 import { useAsync } from 'shared/hooks';
 
 import { useDatavizSummaryRequests } from './hooks/useDatavizSummaryRequests';
 import { useRespondentAnswers } from './hooks/useRespondentAnswers';
 import { setDateRangeFormValues } from './utils/setDateRangeValues';
-import { RespondentsDataFormValues } from '../RespondentData.types';
+import { ActivityOrFlow, RespondentsDataFormValues } from '../RespondentData.types';
 import { getActivityWithLatestAnswer } from '../RespondentData.utils';
 import { ReportMenu } from './ReportMenu';
 import { StyledReportContainer } from './RespondentDataSummary.styles';
@@ -17,55 +17,80 @@ import { ReportContent } from './ReportContent';
 
 export const RespondentDataSummary = () => {
   const { appletId, respondentId } = useParams();
-  const [selectedActivity, summaryActivities]: [DatavizActivity | null, DatavizActivity[]] =
-    useWatch({
-      name: ['selectedActivity', 'summaryActivities'],
-    });
+  const requestBody = useMemo(() => {
+    if (!appletId || !respondentId) return null;
+
+    return {
+      appletId,
+      respondentId,
+    };
+  }, [appletId, respondentId]);
+  const [selectedEntity, summaryActivities, summaryFlows]: [
+    ActivityOrFlow | null,
+    DatavizEntity[],
+    DatavizEntity[],
+  ] = useWatch({
+    name: ['selectedEntity', 'summaryActivities', 'summaryFlows'],
+  });
   const [isLoading, setIsLoading] = useState(false);
   const { setValue } = useFormContext<RespondentsDataFormValues>();
   const { getIdentifiersVersions } = useDatavizSummaryRequests();
   const { fetchAnswers } = useRespondentAnswers();
 
+  const summaryActivitiesLength = summaryActivities.length;
+  const summaryFlowsLength = summaryFlows.length;
+
   const { execute: getSummaryActivities } = useAsync(getSummaryActivitiesApi, async (result) => {
     const summaryActivities = result?.data?.result || [];
     setValue('summaryActivities', summaryActivities);
-    if (selectedActivity) return;
+    if (selectedEntity) return;
 
-    const selectedActivityByDefault =
-      getActivityWithLatestAnswer(summaryActivities) || summaryActivities?.[0];
+    const selectedActivityByDefault = {
+      ...(getActivityWithLatestAnswer(summaryActivities) || summaryActivities?.[0]),
+      isFlow: false,
+    };
 
     if (!selectedActivityByDefault) return;
 
-    setValue('selectedActivity', selectedActivityByDefault);
+    setValue('selectedEntity', selectedActivityByDefault);
     setDateRangeFormValues(setValue, selectedActivityByDefault.lastAnswerDate);
 
     setIsLoading(true);
-    await getIdentifiersVersions({ activity: selectedActivityByDefault });
-    await fetchAnswers({ activity: selectedActivityByDefault });
+    await getIdentifiersVersions({ entity: selectedActivityByDefault });
+    await fetchAnswers({ entity: selectedActivityByDefault });
     setIsLoading(false);
   });
 
-  useEffect(() => {
-    if (!appletId || !respondentId || !!summaryActivities?.length) return;
+  const { execute: getSummaryFlows } = useAsync(getSummaryFlowsApi, async (result) => {
+    const summaryFlows = result?.data?.result || [];
+    setValue('summaryFlows', summaryFlows);
+  });
 
-    getSummaryActivities({
-      appletId,
-      respondentId,
-    });
-  }, [appletId, respondentId, summaryActivities, getSummaryActivities]);
+  useEffect(() => {
+    if (!requestBody || summaryFlowsLength) return;
+
+    getSummaryFlows(requestBody);
+  }, [requestBody, summaryFlowsLength, getSummaryFlows]);
+
+  useEffect(() => {
+    if (!requestBody || summaryActivitiesLength) return;
+
+    getSummaryActivities(requestBody);
+  }, [requestBody, summaryActivitiesLength, getSummaryActivities]);
 
   return (
     <StyledContainer>
-      {!!summaryActivities?.length && (
+      {(!!summaryActivitiesLength || !!summaryFlowsLength) && (
         <>
           <ReportMenu
             activities={summaryActivities}
+            flows={summaryFlows}
             getIdentifiersVersions={getIdentifiersVersions}
             fetchAnswers={fetchAnswers}
             setIsLoading={setIsLoading}
           />
           <StyledReportContainer>
-            <ReportContent selectedActivity={selectedActivity} isLoading={isLoading} />
+            <ReportContent selectedEntity={selectedEntity} isLoading={isLoading} />
           </StyledReportContainer>
         </>
       )}
