ac95cc7f35adc5cec5cb683a0d5597bc63c3c46c
Carlos Chacon <carlos.chacon@metalab.com>
2024-05-28 11:55:15 -0600

    Hide ActionsMenu button if there are no items

--- src/shared/components/ActionsMenu/ActionsMenu.tsx
+++ src/shared/components/ActionsMenu/ActionsMenu.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useState } from 'react';
+import { MouseEvent, useMemo, useState } from 'react';
 
 import { Svg } from 'shared/components/Svg';
 import { Menu, MenuUiType } from 'shared/components/Menu';
@@ -23,8 +23,18 @@ export const ActionsMenu = <T = unknown,>({
     setAnchorEl(null);
   };
   const openMenu = Boolean(anchorEl);
+  const hasMenuItems = useMemo(() => {
+    // Hides the ActionMenu if all the actions include .isDisplayed === false
+    const displayedMenuItems = menuItems.filter((item) => 'isDisplayed' in item).length;
+    if (displayedMenuItems) {
+      return menuItems.filter((item) => item.isDisplayed === true).length > 0;
+    }
 
-  return (
+    // And keeps compatibility with items that don't have this property
+    return true;
+  }, [menuItems]);
+
+  return hasMenuItems ? (
     <>
       <StyledButton
         onClick={handleMenuOpen}
@@ -47,5 +57,5 @@ export const ActionsMenu = <T = unknown,>({
         />
       )}
     </>
-  );
+  ) : null;
 };
