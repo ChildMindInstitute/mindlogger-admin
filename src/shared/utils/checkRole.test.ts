fb31989475c2de7d55c0d4b55d853419bc8401f9
Carlos Chacon <carlos.chacon@metalab.com>
2024-05-29 08:48:50 -0600

    fix checkIfCanAccessData test

--- src/shared/utils/checkRole.test.ts
+++ src/shared/utils/checkRole.test.ts
@@ -63,7 +63,7 @@ describe('checkIfCanAccessData', () => {
     ${[Roles.Coordinator]} | ${false} | ${'should be false for coordinator'}
     ${[Roles.Owner]}       | ${true}  | ${'should be true for owner'}
     ${[Roles.Respondent]}  | ${false} | ${'should be false for respondent'}
-    ${[Roles.Reviewer]}    | ${false} | ${'should be false for reviewer'}
+    ${[Roles.Reviewer]}    | ${true}  | ${'should be true for reviewer'}
     ${[Roles.SuperAdmin]}  | ${true}  | ${'should be true for superadmin'}
     ${undefined}           | ${false} | ${'should be false for undefined'}
   `('$description', ({ roles, expected }) => {
