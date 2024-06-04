e14b6ba41ff8051e373142a339e78a21ca05a89d
Justin <njustin@gmail.com>
2024-06-03 18:53:11 -0700

    Add links to overview table.

--- src/modules/Dashboard/features/Applet/Overview/Overview.tsx
+++ src/modules/Dashboard/features/Applet/Overview/Overview.tsx
@@ -1,5 +1,5 @@
-import { useMemo, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useCallback, useMemo, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 import { GetAppletSubmissionsResponse, getAppletSubmissionsApi } from 'api';
@@ -9,27 +9,28 @@ import { QuickStats } from 'modules/Dashboard/features/Applet/Overview/QuickStat
 import { Spinner } from 'shared/components';
 import { StyledFlexColumn, StyledTitleLarge } from 'shared/styles';
 import { workspaces } from 'redux/modules';
-import { useAsync, usePermissions } from 'shared/hooks';
+import { useAsync, useEncryptionStorage, usePermissions } from 'shared/hooks';
 import { DEFAULT_ROWS_PER_PAGE } from 'shared/consts';
 import { checkIfCanAccessData } from 'shared/utils';
 
 import { mapResponseToQuickStatProps, mapResponseToSubmissionsTableProps } from './Overview.utils';
 import { StyledRoot } from './Overview.styles';
+import { UnlockAppletPopup } from '../../Respondents/Popups/UnlockAppletPopup';
 
 const limit = DEFAULT_ROWS_PER_PAGE;
 
 export const Overview = () => {
   const [addPopupOpen, setAddPopupOpen] = useState(false);
   const [page, setPage] = useState(1);
+  const [unlockAppletPopupOpen, setUnlockAppletPopupOpen] = useState(false);
+  const [unlockedPath, setUnlockedPath] = useState<string>();
   const { appletId } = useParams();
   const { data: workspaceRolesData } = workspaces.useRolesData();
   const { execute, isLoading, previousValue, value } = useAsync(getAppletSubmissionsApi);
+  const navigate = useNavigate();
   const { t } = useTranslation('app');
+  const { getAppletPrivateKey } = useEncryptionStorage();
   const { data } = value ?? previousValue ?? {};
-  const dashboardTableProps = useMemo(
-    () => mapResponseToSubmissionsTableProps(data ?? ({} as GetAppletSubmissionsResponse)),
-    [data],
-  );
   const roles = appletId ? workspaceRolesData?.[appletId] : undefined;
   const canAccessData = checkIfCanAccessData(roles);
   const { isForbidden, noPermissionsComponent } = usePermissions(() => {
@@ -39,13 +40,35 @@ export const Overview = () => {
   }, [page, limit, appletId]);
   const showContent = !isLoading || (isLoading && data);
 
-  const handlePopupClose = (shouldRefetch = false) => {
+  const handleViewSubmissionPopupOpen = useCallback(
+    ({ path }: { path: string }) => {
+      const hasEncryptionCheck = !!getAppletPrivateKey(appletId ?? '');
+
+      if (!hasEncryptionCheck) {
+        setUnlockAppletPopupOpen(true);
+        setUnlockedPath(path);
+      } else {
+        navigate(path);
+      }
+    },
+    [appletId, getAppletPrivateKey, navigate],
+  );
+
+  const handleAddPopupClose = (shouldRefetch = false) => {
     setAddPopupOpen(false);
 
     if (!!appletId && shouldRefetch) {
       execute({ appletId, limit });
     }
   };
+
+  const dashboardTableProps = useMemo(
+    () =>
+      mapResponseToSubmissionsTableProps(data ?? ({} as GetAppletSubmissionsResponse), {
+        onViewSubmission: handleViewSubmissionPopupOpen,
+      }),
+    [data, handleViewSubmissionPopupOpen],
+  );
 
   if (isForbidden || !canAccessData) {
     return noPermissionsComponent;
@@ -84,9 +107,26 @@ export const Overview = () => {
             <AddParticipantPopup
               appletId={appletId}
               popupVisible={addPopupOpen}
-              onClose={handlePopupClose}
+              onClose={handleAddPopupClose}
             />
           )}
+
+          <UnlockAppletPopup
+            appletId={appletId ?? ''}
+            popupVisible={unlockAppletPopupOpen}
+            setPopupVisible={(value) => {
+              setUnlockAppletPopupOpen(value);
+
+              if (!value) {
+                setUnlockedPath(undefined);
+              }
+            }}
+            onSubmitHandler={() => {
+              if (unlockedPath) {
+                navigate(unlockedPath);
+              }
+            }}
+          />
         </>
       )}
     </StyledRoot>
