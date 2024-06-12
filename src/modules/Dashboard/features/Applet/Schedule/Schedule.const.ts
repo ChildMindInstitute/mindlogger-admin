cadf9c803cad3cc11221fe064aa3d349a3670317
IhorNesterenko99 <116339462+IhorNesterenko99@users.noreply.github.com>
2024-06-12 16:09:31 +0300

    fix: event start date is shifted to one day earlier in UTC- timezone (M2-6997) (#1795)

--- src/modules/Dashboard/features/Applet/Schedule/Schedule.const.ts
+++ src/modules/Dashboard/features/Applet/Schedule/Schedule.const.ts
@@ -1,1 +1,4 @@
 export const LEFT_SCHEDULE_PANEL_WIDTH = '32rem';
+export const iso8601DateTimeRegex =
+  /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|[+-]\d{2}:\d{2})?$/;
+export const iso8601DateOnlyRegex = /^\d{4}-\d{2}-\d{2}$/;
--- src/modules/Dashboard/features/Applet/Schedule/Schedule.utils.tsx
+++ src/modules/Dashboard/features/Applet/Schedule/Schedule.utils.tsx
@@ -1,17 +1,38 @@
-import { format } from 'date-fns';
+import { format, isValid, parseISO } from 'date-fns';
 
 import { variables } from 'shared/styles';
 import { Periodicity } from 'modules/Dashboard/api';
 import { DateFormats, Roles } from 'shared/consts';
-import { without } from 'shared/utils';
+import { getNormalizedTimezoneDate, without } from 'shared/utils';
 
 import { ActivitiesFlowsWithColors } from './Schedule.types';
+import { iso8601DateOnlyRegex, iso8601DateTimeRegex } from './Schedule.const';
 
 export const getCount = (ids: ActivitiesFlowsWithColors, id: string) =>
   ids.filter((item) => item.id === id).length;
 
 export const convertDateToYearMonthDay = (date: Date | string) =>
   typeof date === 'string' ? date : format(date, DateFormats.YearMonthDay);
+
+// get date string in format 'dd MMM yyyy'
+export const getEventStartDMYString = (isAlwaysAvailable: boolean, dateString?: string | null) => {
+  // Return the current date in the specified format if dateString is not provided or invalid
+  if (
+    !dateString ||
+    (!iso8601DateTimeRegex.test(dateString) && !iso8601DateOnlyRegex.test(dateString)) ||
+    !isValid(parseISO(dateString))
+  ) {
+    return format(new Date(), DateFormats.DayMonthYear);
+  }
+
+  if (isAlwaysAvailable) {
+    // Get date string from standardized date string (e.g., '2023-12-21T15:12:34.842050')
+    return format(new Date(dateString), DateFormats.DayMonthYear);
+  }
+
+  // Get date string from year-month-day string (e.g., '2024-06-12')
+  return format(getNormalizedTimezoneDate(dateString), DateFormats.DayMonthYear);
+};
 
 const {
   blue,
