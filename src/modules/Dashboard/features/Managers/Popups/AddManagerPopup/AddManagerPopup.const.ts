1b6f759d9dac75240b5b9ddbea356c91b161912d
Carlos Chacon <carlos.chacon@metalab.com>
2024-06-17 09:25:02 -0600

    fix: show Add Team Member button for Coordinators (M2-6909) (#1804)
    * show add team member button for Coordinators
    
    * use noPermissionComponent and remove search button

--- src/modules/Dashboard/features/Managers/Popups/AddManagerPopup/AddManagerPopup.const.ts
+++ src/modules/Dashboard/features/Managers/Popups/AddManagerPopup/AddManagerPopup.const.ts
@@ -1,10 +1,10 @@
 import { Roles } from 'shared/consts';
 
-export const defaultValues = {
-  role: Roles.Manager,
+export const defaultValues = (roles?: Roles[]) => ({
+  role: roles?.includes(Roles.Coordinator) ? Roles.Reviewer : Roles.Manager,
   email: '',
   firstName: '',
   lastName: '',
-};
+});
 
 export const USER_ALREADY_INVITED = 'User already invited. Edit their access in the Managers tab.';
