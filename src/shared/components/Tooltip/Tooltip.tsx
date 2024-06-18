d4d845a40e7534de64dd1d57068e2beba74c97e1
Justin <njustin@gmail.com>
2024-06-18 11:14:12 -0700

    Add calculated `maxHeight` to Tooltip.
    - Use `popper-max-size-modifier` to calculate correct max-height of popper.
    - This is meant to keep Tooltip content from extending out of the viewport. It will appear truncated instead.

--- src/shared/components/Tooltip/Tooltip.tsx
+++ src/shared/components/Tooltip/Tooltip.tsx
@@ -1,9 +1,30 @@
+import { State } from '@popperjs/core';
+import maxSize from 'popper-max-size-modifier';
+
 import { TooltipProps } from './Tooltip.types';
 import { StyledTooltip } from './Tooltip.styles';
+
+const applyMaxSize = {
+  name: 'applyMaxSize' as const,
+  enabled: true,
+  phase: 'beforeWrite' as const,
+  requires: ['maxSize'],
+  fn: ({ state }: { state: State }) => {
+    const { height } = state.modifiersData.maxSize;
+
+    state.styles.popper = {
+      ...state.styles.popper,
+      display: 'flex',
+      placeItems: state.placement.includes('bottom') ? 'flex-start' : 'flex-end',
+      height: `${height - 24}px`,
+    };
+  },
+};
 
 export const Tooltip = ({ tooltipTitle = '', children, maxWidth, ...props }: TooltipProps) => (
   <StyledTooltip
     {...props}
+    PopperProps={{ ...props.PopperProps, modifiers: [maxSize, applyMaxSize] }}
     title={tooltipTitle}
     sx={{
       '.MuiTooltip-tooltip': {
