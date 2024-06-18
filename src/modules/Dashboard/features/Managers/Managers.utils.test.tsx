8714115ed1116a0c51c4c7221d0e1aacf4eee29d
sultanofcardio <kenroy.gobourne@gmail.com>
2024-06-18 17:19:21 -0500

    Update manager utils tests

--- src/modules/Dashboard/features/Managers/Managers.utils.test.tsx
+++ src/modules/Dashboard/features/Managers/Managers.utils.test.tsx
@@ -1,12 +1,22 @@
-import { Svg } from 'shared/components';
-import { mockedManager, mockedAppletId } from 'shared/mock';
+import { MenuItemType, Svg } from 'shared/components';
+import {
+  mockedManager,
+  mockedAppletId,
+  mockedManagerId,
+  mockedEmail,
+  mockedEncryption,
+} from 'shared/mock';
 import { variables } from 'shared/styles';
 
 import { getManagerActions, getHeadCells } from './Managers.utils';
+import { Manager } from '../../types';
+import { Roles } from '../../../../shared/consts';
 
 const headCellProperties = ['avatar', 'firstName', 'lastName', 'title', 'email', 'actions'];
 const removeAccessAction = jest.fn();
 const editAccessAction = jest.fn();
+const copyEmailAddressAction = jest.fn();
+const copyInvitationLinkAction = jest.fn();
 
 describe('Managers utils tests', () => {
   describe('getHeadCells function', () => {
@@ -26,8 +36,11 @@ describe('Managers utils tests', () => {
   });
 
   describe('getManagerActions function', () => {
-    test('should return the correct actions for a manager', () => {
-      const actions = getManagerActions({ removeAccessAction, editAccessAction }, mockedManager);
+    test('should return the correct actions for an approved manager', () => {
+      const actions = getManagerActions(
+        { removeAccessAction, editAccessAction, copyEmailAddressAction, copyInvitationLinkAction },
+        mockedManager,
+      );
 
       expect(actions).toEqual([
         {
@@ -42,6 +55,78 @@ describe('Managers utils tests', () => {
           action: removeAccessAction,
           title: 'Remove Access',
           context: mockedManager,
+          customItemColor: variables.palette.dark_error_container,
+          'data-testid': 'dashboard-managers-remove-access',
+        },
+      ]);
+    });
+
+    test('should return the correct actions for a pending manager', () => {
+      const pendingManager: Manager = {
+        id: mockedManagerId,
+        firstName: 'TestFirstName',
+        lastName: 'TestLastName',
+        title: 'PhD',
+        email: mockedEmail,
+        roles: [Roles.Manager],
+        lastSeen: '2023-08-15T13:39:24.058402',
+        isPinned: false,
+        applets: [
+          {
+            id: mockedAppletId,
+            displayName: 'displayName',
+            image: '',
+            roles: [
+              {
+                accessId: '17ba7d95-f766-42ae-9ce6-2f8fcc3l24a',
+                role: Roles.Manager,
+              },
+            ],
+            encryption: mockedEncryption,
+          },
+        ],
+        createdAt: '2021-10-01T00:00:00.000Z',
+        titles: [],
+        status: 'pending',
+        invitationKey: 'df3c8ab9-78fc-4dd1-8eba-63bef39e5805',
+      };
+      const actions = getManagerActions(
+        { removeAccessAction, editAccessAction, copyEmailAddressAction, copyInvitationLinkAction },
+        pendingManager,
+      );
+
+      expect(actions).toEqual([
+        {
+          type: MenuItemType.Info,
+          title: 'Invitation Date: Oct 01, 2021 at 00:00',
+        },
+        { type: MenuItemType.Divider },
+        {
+          icon: <Svg id="duplicate" width={24} height={24} />,
+          action: expect.any(Function),
+          title: 'Copy Email Address',
+          context: pendingManager,
+        },
+        { type: MenuItemType.Divider },
+        {
+          icon: <Svg id="format-link" width={24} height={24} />,
+          action: expect.any(Function),
+          title: 'Copy Invitation Link',
+          context: pendingManager,
+        },
+        { type: MenuItemType.Divider },
+        {
+          icon: <Svg id="edit-user" />,
+          action: editAccessAction,
+          title: 'Edit Access',
+          context: pendingManager,
+          'data-testid': 'dashboard-managers-edit-user',
+        },
+        {
+          icon: <Svg id="remove-access" />,
+          action: removeAccessAction,
+          title: 'Remove Access',
+          context: pendingManager,
           customItemColor: variables.palette.dark_error_container,
           'data-testid': 'dashboard-managers-remove-access',
         },
