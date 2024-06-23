77f7fce429122c38d6aba75da9c3e036a0515de0
Marty <mbanting@yahoo.com>
2024-06-23 11:37:02 -0700

    chore: update multiinformant-metapod branch with latest from develop (M2-7019) (#1823)
    * tests: Unit tests for Applet settings / Report configuaration (M2-6608) (#1775)
    
    * fix: Applet editing - save changes after logout click (M2-5815) (#1812)
    
    * fix: performance tasks name in breadcrumbs (M2-6094, M2-6309) (#1817)
    
    ---------
    
    Co-authored-by: nmialik <115553085+nmialik@users.noreply.github.com>
    Co-authored-by: IhorNesterenko99 <116339462+IhorNesterenko99@users.noreply.github.com>

---
+++ src/modules/Builder/features/ReportConfigSetting/ServerNotConfigured/ServerNotConfigured.test.tsx
@@ -0,0 +1,41 @@
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+
+import { renderWithProviders } from 'shared/utils/renderWithProviders';
+
+import { ServerNotConfigured } from './ServerNotConfigured';
+
+const mockUseNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockUseNavigate,
+}));
+
+describe('ServerNotConfigured', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('renders correctly', () => {
+    renderWithProviders(<ServerNotConfigured appletId="testAppletId" data-testid="test" />);
+
+    screen.debug();
+
+    expect(
+      screen.getByText('Server Status: Not Configured. Report can not be generated.'),
+    ).toBeInTheDocument();
+    expect(screen.getByTestId('test-configure-report')).toBeInTheDocument();
+  });
+
+  test('navigates to the correct path on button click', async () => {
+    renderWithProviders(<ServerNotConfigured appletId="testAppletId" data-testid="test" />);
+
+    const button = screen.getByTestId('test-configure-report');
+    await userEvent.click(button);
+
+    expect(mockUseNavigate).toHaveBeenCalledWith(
+      '/builder/testAppletId/settings/report-configuration',
+    );
+  });
+});
