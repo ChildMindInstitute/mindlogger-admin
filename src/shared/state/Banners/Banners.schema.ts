b0de242e2f9897183500cbd92ccd4a7308b9faf9
farmerpaul <paul.hh@metalab.com>
2024-07-03 16:35:16 -0300

    feat: add soft-lock warning banner

--- src/shared/state/Banners/Banners.schema.ts
+++ src/shared/state/Banners/Banners.schema.ts
@@ -10,6 +10,7 @@ export enum BannerType {
   PasswordResetSuccessfulBanner,
   ShellAccountSuccessBanner,
   AddParticipantSuccessBanner,
+  SoftLockWarningBanner,
 }
 
 export type BannerPayload = {
