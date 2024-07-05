e214b3b5195b656ad42fe5489872788f91ac000d
IhorNesterenko99 <116339462+IhorNesterenko99@users.noreply.github.com>
2024-07-05 13:35:02 +0300

    chore: add exception for Text Item in case receiving object as an answer (M2-7244) (#1855)

--- src/modules/Dashboard/features/RespondentData/RespondentDataReview/ActivityResponses/ActivityResponses.utils.tsx
+++ src/modules/Dashboard/features/RespondentData/RespondentDataReview/ActivityResponses/ActivityResponses.utils.tsx
@@ -5,6 +5,7 @@ import { DateFormats, ItemResponseType } from 'shared/consts';
 import { ActivityItemAnswer, DecryptedTimeAnswer } from 'shared/types';
 import { StyledBodyLarge, variables } from 'shared/styles';
 import i18n from 'i18n';
+import { isObject } from 'shared/utils/isObject';
 
 import {
   MultiSelectItemAnswer,
@@ -44,7 +45,13 @@ export const getTimeResponseItem = (answer?: DecryptedTimeAnswer) => {
 };
 
 export const getResponseItem = (activityItemAnswer: ActivityItemAnswer) => {
-  if (!activityItemAnswer.answer) {
+  if (
+    !activityItemAnswer.answer ||
+    // exception for MiResource answers for Text Item: in case of receiving an object instead of
+    // string or null
+    (activityItemAnswer.activityItem.responseType === ItemResponseType.Text &&
+      isObject(activityItemAnswer.answer))
+  ) {
     return (
       <StyledBodyLarge color={variables.palette.outline} data-testid="no-response-data">
         {t('noResponseData')}
