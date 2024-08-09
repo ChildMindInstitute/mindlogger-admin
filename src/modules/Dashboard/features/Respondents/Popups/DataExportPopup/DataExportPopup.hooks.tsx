e0f96e8627628767c686519054e7bebe36705452
IhorNesterenko99 <inesterenko@scnsoft.com>
2024-08-09 17:36:52 +0300

    feat: created DataExportWorkersManager (M2-7496)

--- src/modules/Dashboard/features/Respondents/Popups/DataExportPopup/DataExportPopup.hooks.tsx
+++ src/modules/Dashboard/features/Respondents/Popups/DataExportPopup/DataExportPopup.hooks.tsx
@@ -1,19 +1,13 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-import { exportDecryptedDataSucceed } from 'shared/utils/exportData';
-import { Mixpanel } from 'shared/utils/mixpanel/mixpanel';
 import { getParsedEncryptionFromServer } from 'shared/utils/encryption';
 import { ExportDataResult } from 'shared/types/answer';
 import { useEncryptionStorage } from 'shared/hooks/useEncryptionStorage';
 import { isProduction } from 'shared/utils/env';
 import { SessionStorageKeys } from 'shared/utils/storage';
-import { ItemResponseType } from 'shared/consts';
 
-import { IdleWorker, MultipleDecryptWorkersProps } from './DataExportPopup.types';
-import { WorkerOnMessageEvent } from './DataExportWorker/DataExportWorker.types';
-import DecryptionWorker from './DataExportWorker/DataExportWorker.worker';
-import { getExportDataSuffix } from './DataExportPopup.utils';
-import { NUM_WORKERS } from './DataExportWorker/DataExportWorker.const';
+import { MultipleDecryptWorkersProps } from './DataExportPopup.types';
+import { DataExportWorkersManager as DataExportWorkersManagerClass } from './DataExportWorkersManager';
 
 export const useMultipleDecryptWorkers = ({
   handleExportPopupClose,
@@ -26,68 +20,31 @@ export const useMultipleDecryptWorkers = ({
   const [exportDataQueue, setExportDataQueue] = useState<ExportDataResult[] | null>(null);
 
   const dataExportingApiRef = useRef(false);
-  const workersRef = useRef<IdleWorker[]>([]);
-  const processedPagesRef = useRef<Set<number>>(new Set());
-  const finishedPagesRef = useRef<Set<number>>(new Set());
   const limitRef = useRef(1);
-  const taskQueueRef = useRef<{ pageData: ExportDataResult; page: number }[]>([]);
-  const shouldLogDataInDebugMode =
-    !isProduction && sessionStorage.getItem(SessionStorageKeys.DebugMode) === 'true';
+  const finishedPagesRef = useRef<Set<number>>(new Set());
 
   const { getAppletPrivateKey } = useEncryptionStorage();
-
   const encryptionInfoFromServer = getParsedEncryptionFromServer(encryption);
   const privateKey = getAppletPrivateKey(appletId) as number[];
 
-  const distributeTasks = useCallback(() => {
-    workersRef.current.forEach((worker) => {
-      if (worker.isIdle && taskQueueRef.current.length > 0) {
-        const currentTask = taskQueueRef.current.shift();
-        if (!currentTask) return;
+  const shouldLogDataInDebugMode =
+    !isProduction && sessionStorage.getItem(SessionStorageKeys.DebugMode) === 'true';
 
-        const { pageData, page } = currentTask;
-        worker.isIdle = false;
-        worker.postMessage({
-          encryptedData: pageData,
-          encryptionInfoFromServer,
-          page,
-          privateKey,
-          hasSuffix: limitRef.current > 1,
-          filters,
-          shouldLogDataInDebugMode,
-          ItemResponseType,
-        });
-
-        setCurrentPage((prevState) => prevState + 1);
-      }
-    });
-  }, [encryptionInfoFromServer, privateKey, filters, shouldLogDataInDebugMode]);
-
-  const processData = useCallback(
-    (pageData: ExportDataResult, page: number) => {
-      if (workersRef.current.length === 0 || processedPagesRef.current.has(page)) return;
-
-      taskQueueRef.current.push({ pageData, page });
-      distributeTasks();
-    },
-    [distributeTasks],
-  );
-
-  const handleAllTasksCompleted = () => {
-    // check if all pages are processed and all workers are idle
-    const allPagesProcessed =
-      processedPagesRef.current.size === limitRef.current &&
-      finishedPagesRef.current.size === limitRef.current;
-    const allWorkersIdle = workersRef.current.every((worker) => worker.isIdle);
-
-    if (!dataExportingApiRef.current && allPagesProcessed && allWorkersIdle) {
-      setDataIsExporting(false);
-      handleExportPopupClose();
-      Mixpanel.track('Export Data Successful', {
-        'Applet ID': appletId,
-      });
-    }
-  };
+  const DataExportWorkerManager = useRef(
+    new DataExportWorkersManagerClass(
+      encryptionInfoFromServer,
+      privateKey,
+      filters,
+      shouldLogDataInDebugMode,
+      dataExportingApiRef,
+      setDataIsExporting,
+      handleExportPopupClose,
+      appletId,
+      setCurrentPage,
+      limitRef,
+      finishedPagesRef,
+    ),
+  ).current;
 
   useEffect(() => {
     if (!exportDataQueue) return;
@@ -95,45 +52,18 @@ export const useMultipleDecryptWorkers = ({
     const processingBatch = exportDataQueue[currentPage - 1];
 
     if (processingBatch) {
-      processData(processingBatch, currentPage);
+      DataExportWorkerManager.processData(processingBatch, currentPage);
     }
-  }, [exportDataQueue, currentPage, processData]);
+  }, [exportDataQueue, currentPage, DataExportWorkerManager]);
 
-  useEffect(() => {
-    const workers: IdleWorker[] = Array.from({ length: NUM_WORKERS }, () => {
-      const worker = new DecryptionWorker() as IdleWorker;
-      worker.isIdle = true;
-      worker.onmessage = async (event: WorkerOnMessageEvent) => {
-        const { success, decryptedData, error, page, hasSuffix, filters } = event.data;
-        worker.isIdle = true;
-        processedPagesRef.current.add(page);
+  useEffect(
+    () => () => {
+      if (!DataExportWorkerManager) return;
 
-        if (success) {
-          await exportDecryptedDataSucceed({
-            suffix: hasSuffix ? getExportDataSuffix(page) : '',
-            filters,
-          })(decryptedData);
-
-          finishedPagesRef.current.add(page);
-          handleAllTasksCompleted();
-        } else {
-          console.error('Decryption failed:', error);
-        }
-
-        distributeTasks();
-      };
-
-      return worker;
-    });
-
-    workersRef.current = workers;
-
-    return () => {
-      workers.forEach((worker) => worker.terminate());
-    };
-    // create workers only once when the component mounts
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+      DataExportWorkerManager.terminateWorkers();
+    },
+    [DataExportWorkerManager],
+  );
 
   return {
     dataIsExporting,
---
+++ src/modules/Dashboard/features/Respondents/Popups/DataExportPopup/DataExportWorkersManager/DataExportWorkersManager.ts
@@ -0,0 +1,113 @@
+import { Dispatch, MutableRefObject, SetStateAction } from 'react';
+
+import { ExportDataResult } from 'shared/types/answer';
+import { EncryptionParsed } from 'shared/utils/encryption';
+import { ExportDataFilters, exportDecryptedDataSucceed } from 'shared/utils/exportData';
+import { ItemResponseType } from 'shared/consts';
+import { Mixpanel } from 'shared/utils/mixpanel/mixpanel';
+
+import DecryptionWorker from '../DataExportWorker/DataExportWorker.worker';
+import { IdleWorker } from '../DataExportPopup.types';
+import { NUM_WORKERS } from '../DataExportWorker/DataExportWorker.const';
+import { WorkerOnMessageEvent } from '../DataExportWorker/DataExportWorker.types';
+import { getExportDataSuffix } from '../DataExportPopup.utils';
+
+export class DataExportWorkersManager {
+  private workers: IdleWorker[] = [];
+  private processedPages = new Set<number>();
+  private taskQueue: Array<{ pageData: ExportDataResult; page: number }> = [];
+
+  constructor(
+    private encryptionInfoFromServer: EncryptionParsed | null,
+    private privateKey: number[],
+    private filters: ExportDataFilters | undefined,
+    private shouldLogDataInDebugMode: boolean,
+    private dataExportingApiRef: MutableRefObject<boolean>,
+    private setDataIsExporting: Dispatch<SetStateAction<boolean>>,
+    private handleExportPopupClose: () => void,
+    private appletId: string,
+    private setCurrentPage: Dispatch<SetStateAction<number>>,
+    private limitRef: MutableRefObject<number>,
+    private finishedPagesRef: MutableRefObject<Set<number>>,
+  ) {
+    this.initializeWorkers();
+  }
+
+  private initializeWorkers = () => {
+    this.workers = Array.from({ length: NUM_WORKERS }, () => {
+      const worker = new DecryptionWorker() as IdleWorker;
+      worker.isIdle = true;
+      worker.onmessage = (event: WorkerOnMessageEvent) => this.handleWorkerMessage(worker, event);
+
+      return worker;
+    });
+  };
+
+  private handleAllTasksCompleted = () => {
+    const allPagesProcessed =
+      this.processedPages.size === this.limitRef.current &&
+      this.finishedPagesRef.current.size === this.limitRef.current;
+    const allWorkersIdle = this.workers.every((worker) => worker.isIdle);
+
+    if (!this.dataExportingApiRef.current && allPagesProcessed && allWorkersIdle) {
+      this.setDataIsExporting(false);
+      this.handleExportPopupClose();
+      Mixpanel.track('Export Data Successful', { 'Applet ID': this.appletId });
+    }
+  };
+
+  private handleWorkerMessage = (worker: IdleWorker, event: WorkerOnMessageEvent) => {
+    const { success, decryptedData, error, page, hasSuffix, filters } = event.data;
+    worker.isIdle = true;
+    this.processedPages.add(page);
+
+    if (success) {
+      exportDecryptedDataSucceed({
+        suffix: hasSuffix ? getExportDataSuffix(page) : '',
+        filters,
+      })(decryptedData).then(() => {
+        this.finishedPagesRef.current.add(page);
+        this.handleAllTasksCompleted();
+      });
+    } else {
+      console.error('Decryption failed:', error);
+    }
+
+    this.distributeTasks();
+  };
+
+  private distributeTasks = () => {
+    this.workers.forEach((worker) => {
+      if (worker.isIdle && this.taskQueue.length > 0) {
+        const currentTask = this.taskQueue.shift();
+        if (!currentTask) return;
+
+        const { pageData, page } = currentTask;
+        worker.isIdle = false;
+        worker.postMessage({
+          encryptedData: pageData,
+          encryptionInfoFromServer: this.encryptionInfoFromServer,
+          page,
+          privateKey: this.privateKey,
+          hasSuffix: this.limitRef.current > 1,
+          filters: this.filters,
+          shouldLogDataInDebugMode: this.shouldLogDataInDebugMode,
+          ItemResponseType,
+        });
+
+        this.setCurrentPage((prevState) => prevState + 1);
+      }
+    });
+  };
+
+  public processData = (pageData: ExportDataResult, page: number) => {
+    if (this.workers.length === 0 || this.processedPages.has(page)) return;
+
+    this.taskQueue.push({ pageData, page });
+    this.distributeTasks();
+  };
+
+  public terminateWorkers = () => {
+    this.workers.forEach((worker) => worker.terminate());
+  };
+}
