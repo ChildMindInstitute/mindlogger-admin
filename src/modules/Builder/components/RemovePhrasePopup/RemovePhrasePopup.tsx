9d5b3df9eefecb9b6c1829288488f0b54ec0de72
Justin <njustin@gmail.com>
2024-10-11 08:27:00 -0700

    Address PR comments.

--- src/modules/Builder/components/RemovePhrasePopup/RemovePhrasePopup.tsx
+++ src/modules/Builder/components/RemovePhrasePopup/RemovePhrasePopup.tsx
@@ -1,8 +1,7 @@
-import { Button } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
 import { Modal, ModalProps } from 'shared/components';
-import { StyledFlexAllCenter, StyledFlexTopStart } from 'shared/styles';
+import { StyledModalWrapper } from 'shared/styles';
 
 export const RemovePhrasePopup = ({
   onRemove,
@@ -12,20 +11,13 @@ export const RemovePhrasePopup = ({
 
   return (
     <Modal
-      footer={
-        <StyledFlexAllCenter sx={{ width: '100%' }}>
-          <Button color="error" onClick={onRemove} variant="contained">
-            {t('phrasalTemplateRemovePopup.confirm')}
-          </Button>
-        </StyledFlexAllCenter>
-      }
-      hasActions={false}
+      buttonText={t('phrasalTemplateRemovePopup.confirm')}
+      onSubmit={onRemove}
+      submitBtnColor="error"
       title={t('phrasalTemplateRemovePopup.title')}
       {...otherProps}
     >
-      <StyledFlexTopStart as="p" sx={{ px: 3.2, m: 0, pt: 2.4 }}>
-        {t('phrasalTemplateRemovePopup.areYouSure')}
-      </StyledFlexTopStart>
+      <StyledModalWrapper>{t('phrasalTemplateRemovePopup.areYouSure')}</StyledModalWrapper>
     </Modal>
   );
 };
