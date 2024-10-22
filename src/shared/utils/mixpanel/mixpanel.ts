ad38c35ce7b9678d09ce9bd25087e9334574e93c
sultanofcardio <kenroy.gobourne@gmail.com>
2024-10-22 15:15:29 -0500

    Make Mixpanel methods non-async
    These methods don't return anything and are never awaited, so they don't need to be async

--- src/shared/utils/mixpanel/mixpanel.ts
+++ src/shared/utils/mixpanel/mixpanel.ts
@@ -10,38 +10,43 @@ const shouldEnableMixpanel =
   (isProduction || isStaging || isUat || process.env.REACT_APP_MIXPANEL_FORCE_ENABLE === 'true');
 
 export const Mixpanel = {
-  async init() {
+  init() {
     if (shouldEnableMixpanel) {
-      const { default: mixpanel } = await import('mixpanel-browser');
-      mixpanel.init(PROJECT_TOKEN, { ignore_dnt: isDev });
+      import('mixpanel-browser').then(({ default: mixpanel }) => {
+        mixpanel.init(PROJECT_TOKEN, { ignore_dnt: isDev });
+      });
     }
   },
-  async trackPageView(pageName: string) {
+  trackPageView(pageName: string) {
     if (shouldEnableMixpanel) {
-      const { default: mixpanel } = await import('mixpanel-browser');
-      mixpanel.track_pageview({ page: `[Admin] ${pageName}` });
+      import('mixpanel-browser').then(({ default: mixpanel }) => {
+        mixpanel.track_pageview({ page: `[Admin] ${pageName}` });
+      });
     }
   },
-  async track(action: string, payload?: MixpanelPayload) {
+  track(action: string, payload?: MixpanelPayload) {
     if (shouldEnableMixpanel) {
-      const { default: mixpanel } = await import('mixpanel-browser');
-      mixpanel.track(`[Admin] ${action}`, payload);
+      import('mixpanel-browser').then(({ default: mixpanel }) => {
+        mixpanel.track(`[Admin] ${action}`, payload);
+      });
     }
   },
-  async login(userId: string) {
+  login(userId: string) {
     if (shouldEnableMixpanel) {
-      const { default: mixpanel } = await import('mixpanel-browser');
-      mixpanel.identify(userId);
+      import('mixpanel-browser').then(({ default: mixpanel }) => {
+        mixpanel.identify(userId);
+      });
       mixpanel.people.set({
         'User ID': userId,
         'App Build Number': process.env.REACT_APP_DEVELOP_BUILD_VERSION,
       });
     }
   },
-  async logout() {
+  logout() {
     if (shouldEnableMixpanel) {
-      const { default: mixpanel } = await import('mixpanel-browser');
-      mixpanel.reset();
+      import('mixpanel-browser').then(({ default: mixpanel }) => {
+        mixpanel.reset();
+      });
     }
   },
 };
