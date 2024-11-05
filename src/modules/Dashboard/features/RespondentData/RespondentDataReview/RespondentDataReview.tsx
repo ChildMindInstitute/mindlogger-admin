11a43335f1a4059336cc6167140d1fc133185123
LashaunnaS <shaunna191@icloud.com>
2024-11-05 16:42:11 -0500

    Adds additional commentary to the sourceSubject value

--- src/modules/Dashboard/features/RespondentData/RespondentDataReview/RespondentDataReview.tsx
+++ src/modules/Dashboard/features/RespondentData/RespondentDataReview/RespondentDataReview.tsx
@@ -251,6 +251,11 @@ export const RespondentDataReview = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [lastActivityCompleted]);
 
+  /**
+   * Determines the source subject based on the presence of activity responses.
+   * If activity responses are available, it retrieves the source subject from the first activity answer.
+   * Otherwise, it retrieves the source subject from the first flow answer.
+   */
   const sourceSubject = hasActivityResponses
     ? activityAnswers?.[0]?.sourceSubject
     : flowAnswers?.[0]?.answers[0].sourceSubject;
