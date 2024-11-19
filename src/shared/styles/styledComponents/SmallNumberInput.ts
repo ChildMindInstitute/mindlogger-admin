169a5bb6ac945ca4a3b90232fa6dd70d766e4d1f
André Vital <andre@andrevital.com>
2024-11-19 15:03:58 -0600

    fix: Fix input visibility (M2-7837) (#1971)
    * fix: Change input wrapper styling from fixed width to min-width
    
    * fix: Add extra styling (width) for small number input component
    
    * feature: Create generic component for percentage inputs
    
    * feature: Propagate generic percentage input component changes
    
    * feature: Add extra styling for SmallNumberInput component
    
    * feature: Adjust percentage small input width

--- src/shared/styles/styledComponents/SmallNumberInput.ts
+++ src/shared/styles/styledComponents/SmallNumberInput.ts
@@ -4,11 +4,15 @@ import { theme } from 'shared/styles/theme';
 import { variables } from 'shared/styles/variables';
 
 export const StyledSmallNumberInput = styled(Box)`
-  width: 8.8rem;
+  min-width: 8.8rem;
   margin: ${theme.spacing(0.8, 0)};
 
   .MuiBox-root {
     margin: 0;
+
+    .MuiTextField-root {
+      width: auto;
+    }
   }
 
   .MuiButton-root {
@@ -30,10 +34,27 @@ export const StyledSmallNumberInput = styled(Box)`
         border-color: ${variables.palette.primary};
       }
     }
-  }
 
-  .MuiInputBase-input {
-    padding: ${theme.spacing(0.7, 0)};
-    text-align: center;
+    .MuiInputBase-input {
+      padding: ${theme.spacing(0.7, 0)};
+      text-align: center;
+      max-width: 7rem;
+      overflow: scroll;
+    }
+  }
+`;
+
+export const StyledSmallPercentageInput = styled(StyledSmallNumberInput)`
+  .MuiInputBase-root {
+    .MuiInputBase-input {
+      margin-right: ${theme.spacing(0.75)};
+      padding-right: 0;
+      text-align: end;
+      max-width: 3rem;
+    }
+
+    > .MuiBox-root > .MuiTypography-root {
+      margin-right: ${theme.spacing(0.75)};
+    }
   }
 `;
