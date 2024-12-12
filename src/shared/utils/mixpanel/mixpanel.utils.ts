d2de6a9dd2c288147c67d43c74948067f5b8ce54
Farmer Paul <paul.hh@metalab.com>
2024-12-12 08:51:36 -0500

    feat: Add properties to SSI Phrase Builder analytics events (M2-8295) (#1989)
    * feat: add additional SSI properties to analytics
    
    The data team has requested tracking new SSI properties in Mixpanel,
    as well as requested that hidden items be ignored from all counts.
    
    * test: update/add unit tests

--- src/shared/utils/mixpanel/mixpanel.utils.ts
+++ src/shared/utils/mixpanel/mixpanel.utils.ts
@@ -27,40 +27,93 @@ export const trackAppletSave = ({
 }) => {
   if (!applet) return;
 
-  const itemTypes: ItemResponseType[] = [];
+  let itemCount = 0;
+  let phraseBuilderItemCount = 0;
+  let countOfUniqueItemsInPhraseBuilders = 0;
+  const phraseBuilderFieldCounts = {
+    itemResponse: 0,
+    sentence: 0,
+    lineBreak: 0,
+  };
   let autoAssignedActivityCount = 0;
   let manualAssignedActivityCount = 0;
-  for (const activity of applet.activities ?? []) {
-    itemTypes.push(...(activity.items ?? []).map((item) => item.responseType));
+  const itemTypes = new Set<ItemResponseType>();
+
+  for (const activity of applet.activities) {
     if (activity.autoAssign) {
       autoAssignedActivityCount++;
     } else {
       manualAssignedActivityCount++;
     }
-  }
-  const uniqueItemTypes = new Set(itemTypes);
 
-  const autoAssignedActivityFlowCount = (applet.activityFlows ?? []).filter(
-    (flow) => flow.autoAssign,
-  ).length;
-  const manualAssignedActivityFlowCount =
-    autoAssignedActivityFlowCount === 0
-      ? 0
-      : applet.activityFlows.length - autoAssignedActivityFlowCount;
+    const visibleItemNames = new Set<string>();
+    const items = activity.items;
+
+    for (const item of items) {
+      // Hidden items are not included in any counts
+      if (item.isHidden) continue;
+
+      itemCount++;
+      visibleItemNames.add(item.name);
+      itemTypes.add(item.responseType);
+
+      if (item.responseType === ItemResponseType.PhrasalTemplate) {
+        phraseBuilderItemCount++;
+        const referencedItemNames = new Set<string>();
+
+        for (const phrase of item.responseValues.phrases) {
+          for (const field of phrase.fields) {
+            if (field.type === 'item_response') {
+              if (visibleItemNames.has(field.itemName)) {
+                referencedItemNames.add(field.itemName);
+                phraseBuilderFieldCounts.itemResponse++;
+              }
+            } else if (field.type === 'sentence') {
+              phraseBuilderFieldCounts.sentence++;
+            } else if (field.type === 'line_break') {
+              phraseBuilderFieldCounts.lineBreak++;
+            }
+          }
+        }
+        countOfUniqueItemsInPhraseBuilders += referencedItemNames.size;
+      }
+    }
+  }
+
+  const autoAssignedFlowCount = (applet.activityFlows ?? []).filter((f) => f.autoAssign).length;
+  const manualAssignedFlowCount = (applet.activityFlows ?? []).length - autoAssignedFlowCount;
+
+  const getPhraseBuilderAverage = (value: number) =>
+    phraseBuilderItemCount ? Math.round((value / phraseBuilderItemCount) * 100) / 100 : null;
 
   const event: AppletSaveEvent = {
     action,
     [MixpanelProps.AppletId]: appletId,
-    [MixpanelProps.ItemTypes]: [...uniqueItemTypes],
     ...(action === MixpanelEventType.AppletSaveClick && {
       [MixpanelProps.AutoAssignedActivityCount]: autoAssignedActivityCount,
-      [MixpanelProps.AutoAssignedFlowCount]: autoAssignedActivityFlowCount,
+      [MixpanelProps.AutoAssignedFlowCount]: autoAssignedFlowCount,
       [MixpanelProps.ManuallyAssignedActivityCount]: manualAssignedActivityCount,
-      [MixpanelProps.ManuallyAssignedFlowCount]: manualAssignedActivityFlowCount,
+      [MixpanelProps.ManuallyAssignedFlowCount]: manualAssignedFlowCount,
     }),
+    [MixpanelProps.ItemTypes]: [...itemTypes],
+    [MixpanelProps.ItemCount]: itemCount,
+    [MixpanelProps.PhraseBuilderItemCount]: phraseBuilderItemCount,
+    [MixpanelProps.CountOfUniqueItemsInPhraseBuilders]: countOfUniqueItemsInPhraseBuilders,
+    [MixpanelProps.AverageUniqueItemsPerPhraseBuilder]: getPhraseBuilderAverage(
+      countOfUniqueItemsInPhraseBuilders,
+    ),
+    [MixpanelProps.AverageItemsPerPhraseBuilder]: getPhraseBuilderAverage(
+      phraseBuilderFieldCounts.itemResponse,
+    ),
+    [MixpanelProps.AverageTextBoxesPerPhraseBuilder]: getPhraseBuilderAverage(
+      phraseBuilderFieldCounts.sentence,
+    ),
+    [MixpanelProps.AverageLineBreaksPerPhraseBuilder]: getPhraseBuilderAverage(
+      phraseBuilderFieldCounts.lineBreak,
+    ),
   };
 
-  if (uniqueItemTypes.has(ItemResponseType.PhrasalTemplate)) {
+  if (phraseBuilderItemCount) {
     addFeatureToEvent(event, MixpanelFeature.SSI);
   }
 
