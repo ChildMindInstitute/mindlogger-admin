48b87027f7d515ea2e0997efd439d50f173dfdef
Farmer Paul <paul.hh@metalab.com>
2025-01-08 16:05:35 -0400

    fix: Update src/shared/components/Svg/Svg.tsx
    Co-authored-by: Kenroy Gobourne <14842108+sultanofcardio@users.noreply.github.com>

--- src/shared/components/Svg/Svg.tsx
+++ src/shared/components/Svg/Svg.tsx
@@ -1,7 +1,7 @@
 import { SvgProps } from './Svg.types';
 
 export const Svg = ({ id, width = 24, height = 24, className, ...rest }: SvgProps) => (
-  <svg className={`svg-${id} ${className}`} width={width} height={height} {...rest}>
+  <svg className={`svg-${id} ${className || ''}`} width={width} height={height} {...rest}>
     <use xlinkHref={`#svg-${id}`} />
   </svg>
 );
