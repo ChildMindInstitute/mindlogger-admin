20c2ebf095d7aeb6f5dec4e6f7a0a76e765c18f7
farmerpaul <paul.hh@metalab.com>
2025-01-22 11:28:06 -0400

    refactor: add auth token refresh to RTK Query

--- src/shared/api/apiSlice.ts
+++ src/shared/api/apiSlice.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-import { BASE_API_URL } from 'api';
+import { BASE_API_URL, RefreshResponse, ResponseWithObject } from 'api';
 import { regionalLangFormats, UiLanguages } from 'shared/ui';
 import { authStorage } from 'shared/utils';
 import { LocalStorageKeys, storage } from 'shared/utils/storage';
@@ -21,10 +21,51 @@ export const baseQuery = fetchBaseQuery({
   },
 });
 
+export const baseQueryWithRefresh: typeof baseQuery = async (args, store, extraOptions) => {
+  let result = await baseQuery(args, store, extraOptions);
+
+  if (result.error?.status === 401) {
+    const oldAccessToken = authStorage.getAccessToken();
+    const oldRefreshToken = authStorage.getRefreshToken();
+
+    if (!oldAccessToken || !oldRefreshToken) return result;
+
+    // Update baseQuery to use refresh token instead of access token
+    authStorage.setAccessToken(oldRefreshToken);
+
+    // Try to refresh the token
+    const { data } = await baseQuery(
+      {
+        method: 'POST',
+        url: '/auth/token/refresh',
+        body: { refreshToken: oldRefreshToken },
+      },
+      store,
+      extraOptions,
+    );
+
+    if (data) {
+      const {
+        result: { accessToken, refreshToken },
+      } = data as ResponseWithObject<RefreshResponse>;
+
+      // Store the new tokens
+      authStorage.setAccessToken(accessToken);
+      authStorage.setRefreshToken(refreshToken);
+
+      // Retry the original request
+      result = await baseQuery(args, store, extraOptions);
+    } else {
+      throw new Error('Access token refresh failed.');
+    }
+  }
+
+  return result;
+};
+
 export const apiSlice = createApi({
   reducerPath: 'api',
-  // TODO: replace baseQuery with expired access token logic
-  baseQuery,
+  baseQuery: baseQueryWithRefresh,
   tagTypes: ['Applet', 'Respondent', 'Manager'],
   endpoints: (builder) => ({}),
 });
