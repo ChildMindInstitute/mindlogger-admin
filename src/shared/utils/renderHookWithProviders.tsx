129ded850541168c2bf31a073591e453bc603e7b
farmerpaul <paul.hh@metalab.com>
2025-03-27 08:41:14 -0300

    test: add ThemeProvider to renderHookWithProviders
    For good measure.

--- src/shared/utils/renderHookWithProviders.tsx
+++ src/shared/utils/renderHookWithProviders.tsx
@@ -2,10 +2,12 @@ import { ReactNode } from 'react';
 import { Route, Routes, MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { renderHook } from '@testing-library/react';
+import { ThemeProvider } from '@mui/material';
 
 import { ExtendedRenderOptions, setupStore } from 'redux/store';
 import { page } from 'resources';
 import Login from 'modules/Auth/pages/Login';
+import theme from 'shared/styles/theme';
 
 export const renderHookWithProviders = <Result, Props>(
   hook: (props: Props) => Result,
@@ -19,12 +21,14 @@ export const renderHookWithProviders = <Result, Props>(
 ) => {
   const Providers = ({ children }: { children: ReactNode }) => (
     <Provider store={store}>
-      <MemoryRouter initialEntries={[route]}>
-        <Routes>
-          <Route path={routePath} element={children} />
-          <Route path={page.login} element={<Login />} />
-        </Routes>
-      </MemoryRouter>
+      <ThemeProvider theme={theme}>
+        <MemoryRouter initialEntries={[route]}>
+          <Routes>
+            <Route path={routePath} element={children} />
+            <Route path={page.login} element={<Login />} />
+          </Routes>
+        </MemoryRouter>
+      </ThemeProvider>
     </Provider>
   );
 
