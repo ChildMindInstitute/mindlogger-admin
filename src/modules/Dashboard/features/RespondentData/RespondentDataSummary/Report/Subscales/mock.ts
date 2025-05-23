21a5e60ad055ab12b15d28b76526c5e3ff447be5
André Vital <andre@andrevital.com>
2025-05-22 22:28:33 -0600

    feat: Update hardcoded mock references

--- src/modules/Dashboard/features/RespondentData/RespondentDataSummary/Report/Subscales/mock.ts
+++ src/modules/Dashboard/features/RespondentData/RespondentDataSummary/Report/Subscales/mock.ts
@@ -6,7 +6,7 @@ export const subscales = [
     name: 'Subscale One',
     additionalInformation: {
       description: `**Patient’s features and volnurabilities**
-      The MindLogger iOS app and Android app come bundled with a Welcome applet, pictured below, that gives a brief tour of some of MindLogger's many features. You can add existing applets to the MindLogger app or create your own for personal use or for your students, patients, study participants, etc. MindLogger can deliver scheduled reminders, trigger alerts, compute scores, display data, generate reports, and more!`,
+      The Curious iOS app and Android app come bundled with a Welcome applet, pictured below, that gives a brief tour of some of Curious's many features. You can add existing applets to the Curious app or create your own for personal use or for your students, patients, study participants, etc. Curious can deliver scheduled reminders, trigger alerts, compute scores, display data, generate reports, and more!`,
     },
     items: [
       {
@@ -21,9 +21,9 @@ export const subscales = [
             id: '111',
             additionalInformation: {
               tooltip:
-                'You can add existing applets to the MindLogger app or create your own for personal use or for your students, patients, study participants, etc.',
+                'You can add existing applets to the Curious app or create your own for personal use or for your students, patients, study participants, etc.',
               description:
-                '**MindLogger can deliver scheduled reminders, trigger alerts, compute scores, display data, generate reports, and more! For more background information, please see our JMIR publication.**',
+                '**Curious can deliver scheduled reminders, trigger alerts, compute scores, display data, generate reports, and more! For more background information, please see our JMIR publication.**',
             },
             type: SubscalesTypes.Table,
           },
