e00cc50e2e6cb08df1e36e2ca584d8218ff9dd0b
farmerpaul <paul.hh@metalab.com>
2025-06-05 14:55:18 -0300

    test: Add tests for `ExportDataSetting`

--- src/shared/features/AppletSettings/ExportDataSetting/ExportDataSetting.test.tsx
+++ src/shared/features/AppletSettings/ExportDataSetting/ExportDataSetting.test.tsx
@@ -1,18 +1,29 @@
 import { fireEvent, screen, waitFor, within } from '@testing-library/react';
 import { addDays, roundToNearestMinutes } from 'date-fns';
+import { AxiosResponse } from 'axios';
 
-import { ExportData } from 'api';
 import { initialStateData } from 'redux/modules';
 import { mockedApplet, mockedPassword } from 'shared/mock';
 import { renderWithProviders } from 'shared/utils/renderWithProviders';
 import * as encryptionFunctions from 'shared/utils/encryption';
+import { useFeatureFlags } from 'shared/hooks/useFeatureFlags';
+import { useHasEhrHealthData } from 'shared/hooks/useHasEhrHealthData';
+import * as apiFunctions from 'modules/Dashboard/api';
+import { mockSuccessfulHttpResponse } from 'shared/utils/axios-mocks';
+import { ResponseWithObject } from 'api';
+import { ExportDataResult } from 'shared/types';
+import * as ScheduleHistoryExporterClasses from 'shared/utils/exportData/exporters/ScheduleHistoryExporter';
+import * as FlowActivityHistoryExporterClasses from 'shared/utils/exportData/exporters/FlowActivityHistoryExporter';
+import * as EHRDataExporterClasses from 'shared/utils/exportData/exporters/EHRDataExporter';
+import { getPreloadedState } from 'shared/tests/getPreloadedState';
 
+import { ExportDataExported, ExportDateType } from './ExportDataSetting.types';
 import { ExportDataSetting } from './ExportDataSetting';
-import { ExportDateType } from './ExportDataSetting.types';
 
 const createdDate = '2023-11-14T14:43:33.369902';
 
 const preloadedState = {
+  ...getPreloadedState(),
   applet: {
     applet: {
       ...initialStateData,
@@ -23,15 +34,92 @@ const preloadedState = {
 
 const getPublicKeyMock = () => Buffer.from(JSON.parse(mockedApplet?.encryption?.publicKey || ''));
 
-const mockedExportDataApi = jest.fn();
-
 jest.mock('modules/Dashboard/api', () => ({
-  getExportDataApi: (body: ExportData) => mockedExportDataApi(body),
+  ...jest.requireActual('modules/Dashboard/api'),
+  getExportDataApi: jest.fn(),
+}));
+
+jest.mock('shared/hooks/useFeatureFlags', () => ({
+  ...jest.requireActual('shared/hooks/useFeatureFlags'),
+  useFeatureFlags: jest.fn(),
+}));
+
+jest.mock('shared/hooks/useHasEhrHealthData', () => ({
+  useHasEhrHealthData: jest.fn(),
+}));
+
+jest.mock('shared/utils/exportData/exporters/ScheduleHistoryExporter', () => ({
+  ScheduleHistoryExporter: jest.fn(),
+}));
+
+jest.mock('shared/utils/exportData/exporters/FlowActivityHistoryExporter', () => ({
+  FlowActivityHistoryExporter: jest.fn(),
+}));
+
+jest.mock('shared/utils/exportData/exporters/EHRDataExporter', () => ({
+  EHRDataExporter: jest.fn(),
+}));
+
+jest.mock('shared/utils/exportTemplate', () => ({
+  exportTemplate: jest.fn(),
 }));
 
 const dataTestId = 'export-data';
 
 describe('ExportDataSetting', () => {
+  const mockedExportDataApi = jest.spyOn(apiFunctions, 'getExportDataApi');
+  const mockedScheduleHistoryExporter = jest
+    .spyOn(ScheduleHistoryExporterClasses, 'ScheduleHistoryExporter')
+    .mockImplementation(
+      () =>
+        ({
+          exportData: jest.fn().mockResolvedValue(true),
+        }) as unknown as ScheduleHistoryExporterClasses.ScheduleHistoryExporter,
+    );
+  const mockedFlowHistoryExporter = jest
+    .spyOn(FlowActivityHistoryExporterClasses, 'FlowActivityHistoryExporter')
+    .mockImplementation(
+      () =>
+        ({
+          exportData: jest.fn().mockResolvedValue(true),
+        }) as unknown as FlowActivityHistoryExporterClasses.FlowActivityHistoryExporter,
+    );
+  const mockedEhrDataExporter = jest
+    .spyOn(EHRDataExporterClasses, 'EHRDataExporter')
+    .mockImplementation(
+      () =>
+        ({
+          exportData: jest.fn().mockResolvedValue(true),
+        }) as unknown as EHRDataExporterClasses.EHRDataExporter,
+    );
+
+  beforeEach(() => {
+    jest.mocked(useFeatureFlags).mockReturnValue({
+      featureFlags: {
+        enableEmaExtraFiles: true,
+        enableEhrHealthData: 'active',
+        enableDataExportSpeedUp: false,
+      },
+      resetLDContext: jest.fn(),
+    });
+
+    jest.spyOn(encryptionFunctions, 'getAppletEncryptionInfo').mockImplementation(() =>
+      Promise.resolve({
+        getPublicKey: getPublicKeyMock,
+      }),
+    );
+
+    mockedExportDataApi.mockResolvedValue(
+      mockSuccessfulHttpResponse({
+        result: {
+          answers: [],
+          activities: [],
+        },
+        count: 0,
+      }) as AxiosResponse<ResponseWithObject<ExportDataResult>>,
+    );
+  });
+
   it('should render nothing if isExportSettingsOpen is false', async () => {
     const mockOnClose = jest.fn();
 
@@ -85,12 +173,6 @@ describe('ExportDataSetting', () => {
     `('$description', async ({ exportType, expectedFromTime }) => {
       const mockOnClose = jest.fn();
 
-      jest.spyOn(encryptionFunctions, 'getAppletEncryptionInfo').mockImplementation(() =>
-        Promise.resolve({
-          getPublicKey: getPublicKeyMock,
-        }),
-      );
-
       renderWithProviders(
         <ExportDataSetting
           isExportSettingsOpen
@@ -133,7 +215,7 @@ describe('ExportDataSetting', () => {
         // When checking relative dates, round the date to the nearest
         // 10 minute to account for the test run time.
         expect(
-          roundToNearestMinutes(new Date(requestBody.fromDate), {
+          roundToNearestMinutes(new Date(requestBody.fromDate as string), {
             nearestTo: 10,
           }),
         ).toEqual(
@@ -141,6 +223,176 @@ describe('ExportDataSetting', () => {
             nearestTo: 10,
           }),
         );
+      });
+    });
+
+    it('should pass schedule history setting to the export popup', async () => {
+      jest.mocked(useFeatureFlags).mockReturnValue({
+        featureFlags: {
+          enableEmaExtraFiles: true,
+          enableEhrHealthData: 'unavailable',
+          enableDataExportSpeedUp: false,
+        },
+        resetLDContext: jest.fn(),
+      });
+
+      const mockOnClose = jest.fn();
+      jest.mocked(useHasEhrHealthData).mockReturnValue(false);
+
+      renderWithProviders(
+        <ExportDataSetting
+          isExportSettingsOpen
+          onExportSettingsClose={mockOnClose}
+          data-testid={dataTestId}
+        />,
+        {
+          preloadedState,
+        },
+      );
+
+      await waitFor(() =>
+        expect(screen.queryByTestId(`${dataTestId}-settings`)).toBeInTheDocument(),
+      );
+
+      const scheduleHistoryCheckbox = screen
+        .getByText('Include schedule history')
+        .closest('label')
+        ?.querySelector('input');
+
+      expect(scheduleHistoryCheckbox).toBeInTheDocument();
+
+      if (scheduleHistoryCheckbox) {
+        fireEvent.click(scheduleHistoryCheckbox);
+      }
+
+      fireEvent.click(screen.getByText('Download'));
+
+      expect(screen.getByTestId(`${dataTestId}-modal-password`)).toBeVisible();
+
+      fireEvent.change(await screen.findByLabelText('Password'), {
+        target: { value: mockedPassword },
+      });
+
+      fireEvent.click(
+        within(screen.getByTestId(`${dataTestId}-modal-password`)).getByText('Submit'),
+      );
+
+      await waitFor(() => {
+        expect(mockedScheduleHistoryExporter).toHaveBeenCalled();
+      });
+    });
+
+    it('should pass flow history setting to the export popup', async () => {
+      jest.mocked(useFeatureFlags).mockReturnValue({
+        featureFlags: {
+          enableEmaExtraFiles: true,
+          enableEhrHealthData: 'unavailable',
+          enableDataExportSpeedUp: false,
+        },
+        resetLDContext: jest.fn(),
+      });
+
+      const mockOnClose = jest.fn();
+      jest.mocked(useHasEhrHealthData).mockReturnValue(false);
+
+      renderWithProviders(
+        <ExportDataSetting
+          isExportSettingsOpen
+          onExportSettingsClose={mockOnClose}
+          data-testid={dataTestId}
+        />,
+        {
+          preloadedState,
+        },
+      );
+
+      await waitFor(() =>
+        expect(screen.queryByTestId(`${dataTestId}-settings`)).toBeInTheDocument(),
+      );
+
+      const flowHistoryCheckbox = screen
+        .getByText('Include activity flow history')
+        .closest('label')
+        ?.querySelector('input');
+
+      expect(flowHistoryCheckbox).toBeInTheDocument();
+
+      if (flowHistoryCheckbox) {
+        fireEvent.click(flowHistoryCheckbox);
+      }
+
+      fireEvent.click(screen.getByText('Download'));
+
+      expect(screen.getByTestId(`${dataTestId}-modal-password`)).toBeVisible();
+
+      fireEvent.change(await screen.findByLabelText('Password'), {
+        target: { value: mockedPassword },
+      });
+
+      fireEvent.click(
+        within(screen.getByTestId(`${dataTestId}-modal-password`)).getByText('Submit'),
+      );
+
+      await waitFor(() => {
+        expect(mockedFlowHistoryExporter).toHaveBeenCalled();
+      });
+    });
+
+    it('should pass EHR health data setting to the export popup', async () => {
+      jest.mocked(useFeatureFlags).mockReturnValue({
+        featureFlags: {
+          enableEmaExtraFiles: false,
+          enableEhrHealthData: 'active',
+          enableDataExportSpeedUp: false,
+        },
+        resetLDContext: jest.fn(),
+      });
+
+      const mockOnClose = jest.fn();
+      jest.mocked(useHasEhrHealthData).mockReturnValue(true);
+
+      renderWithProviders(
+        <ExportDataSetting
+          isExportSettingsOpen
+          onExportSettingsClose={mockOnClose}
+          data-testid={dataTestId}
+        />,
+        {
+          preloadedState,
+        },
+      );
+
+      await waitFor(() =>
+        expect(screen.queryByTestId(`${dataTestId}-settings`)).toBeInTheDocument(),
+      );
+
+      // Reset the mock before the test
+      jest.clearAllMocks();
+
+      // Select the ResponsesAndEhrData option
+      const exportDataExportedSetting = screen.getByTestId(
+        `${`${dataTestId}-settings`}-data-exported`,
+      );
+      expect(exportDataExportedSetting).toBeVisible();
+      const input = exportDataExportedSetting.querySelector('input');
+      input &&
+        fireEvent.change(input, { target: { value: ExportDataExported.ResponsesAndEhrData } });
+
+      fireEvent.click(screen.getByText('Download'));
+
+      expect(screen.getByTestId(`${dataTestId}-modal-password`)).toBeVisible();
+
+      fireEvent.change(await screen.findByLabelText('Password'), {
+        target: { value: mockedPassword },
+      });
+
+      fireEvent.click(
+        within(screen.getByTestId(`${dataTestId}-modal-password`)).getByText('Submit'),
+      );
+
+      await waitFor(() => {
+        // Verify that the EHR data exporter was instantiated
+        expect(mockedEhrDataExporter).toHaveBeenCalled();
       });
     });
   });
