5e8955273195584108daa4a031f64d7fd3851ce5
André Vital <andre@andrevital.com>
2025-06-11 14:51:52 -0600

    feat: Rollback top bar avatar badge bg color update

--- src/shared/styles/styledComponents/Badge.ts
+++ src/shared/styles/styledComponents/Badge.ts
@@ -10,7 +10,7 @@ export const StyledBadge = styled(Badge, shouldForwardProp)`
     min-width: ${theme.spacing(1.8)};
     top: ${theme.spacing(0.2)};
     right: ${theme.spacing(0.2)};
-    background-color: ${variables.palette.tertiary};
+    background-color: ${variables.palette.error};
     color: ${variables.palette.white};
     font-weight: ${variables.font.weight.bold};
     font-size: ${variables.font.size.label1};
