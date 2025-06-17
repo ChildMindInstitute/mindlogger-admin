340008a5c0e3642a200f08130bcd45c01b02008c
André Vital <andre@andrevital.com>
2025-06-17 10:46:35 -0600

    feat: Update elevation (boxShadow) stylings

--- src/shared/styles/variables/index.ts
+++ src/shared/styles/variables/index.ts
@@ -1,5 +1,5 @@
-import { palette, modalBackground } from './palette';
 import { font } from './font';
+import { modalBackground, palette } from './palette';
 
 export const variables = {
   palette,
@@ -33,19 +33,24 @@ export const variables = {
   boxShadow: {
     buttonElevation1: '0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 2px rgba(0, 0, 0, 0.24)',
     buttonElevation2: '0px 3px 6px rgba(0, 0, 0, 0.16), 0px 3px 6px rgba(0, 0, 0, 0.23)',
-    light0: '0 1px 2px rgba(0, 0, 0, 0.05), 0 1px 3px 1px rgba(0, 0, 0, 0.05)',
-    light1: '0 1px 2px rgba(0, 0, 0, 0.3), 0 1px 3px 1px rgba(0, 0, 0, 0.15)',
-    light2: '0 1px 2px rgba(0, 0, 0, 0.3), 0 2px 6px 2px rgba(0, 0, 0, 0.15)',
-    light3: '0 6px 6px 0px rgba(0, 0, 0, 0.23), 0 10px 20px 0px rgba(0, 0, 0, 0.19)',
-    light4: '0 6px 10px 4px rgba(0, 0, 0, 0.15), 0 2px 3px rgba(0, 0, 0, 0.3)',
-    light5: '0 15px 12px 0px rgba(0, 0, 0, 0.22), 0 19px 38px 0px rgba(0, 0, 0, 0.30)',
-    dark1: '0 1px 3px 1px rgba(0, 0, 0, 0.15), 0 1px 2px rgba(0, 0, 0, 0.3)',
-    dark2: '0 2px 6px 2px rgba(0, 0, 0, 0.15), 0 1px 2px rgba(0, 0, 0, 0.3)',
-    dark3: '0 4px 8px 3px rgba(0, 0, 0, 0.15), 0 1px 3px rgba(0, 0, 0, 0.3)',
-    dark4: '0 6px 10px 4px rgba(0, 0, 0, 0.15), 0 2px 3px rgba(0, 0, 0, 0.3)',
-    dark5: '0 8px 12px 6px rgba(0, 0, 0, 0.15), 0 4px 4px rgba(0, 0, 0, 0.3)',
-    soft: '0 0 8px rgba(0, 0, 0, 0.08)',
-    soft2: '0 0 16px rgba(0, 0, 0, 0.08)',
+    light0: '0px 1px 2px 0px rgba(0, 0, 0, 0.05), 0 1px 3px 1px rgba(0, 0, 0, 0.05)',
+    light1: '0px 1px 3px 0px rgba(0, 0, 0, 0.12), 0px 1px 2px 0px rgba(0, 0, 0, 0.24)',
+    light2: '0px 3px 6px 0px rgba(0, 0, 0, 0.16), 0px 3px 6px 0px rgba(0, 0, 0, 0.23)',
+    light3: '0px 10px 20px 0px rgba(0, 0, 0, 0.19), 0px 6px 6px 0px rgba(0, 0, 0, 0.23)',
+    light4: '0px 14px 28px 0px rgba(0, 0, 0, 0.25), 0px 10px 10px 0px rgba(0, 0, 0, 0.22)',
+    light5: '0px 19px 38px 0px rgba(0, 0, 0, 0.30), 0px 15px 12px 0px rgba(0, 0, 0, 0.22)',
+    dark1:
+      '0px 1px 3px 0px rgba(0, 0, 0, 0.12), 0px 1px 2px 0px rgba(0, 0, 0, 0.24), 0px 8px 48px 0px rgba(255, 255, 255, 0.03)',
+    dark2:
+      '0px 3px 6px 0px rgba(0, 0, 0, 0.16), 0px 3px 6px 0px rgba(0, 0, 0, 0.23), 0px 10px 60px 0px rgba(255, 255, 255, 0.03)',
+    dark3:
+      '0px 10px 20px 0px rgba(0, 0, 0, 0.19), 0px 6px 6px 0px rgba(0, 0, 0, 0.23), 0px 12px 72px 0px rgba(255, 255, 255, 0.03)',
+    dark4:
+      '0px 14px 28px 0px rgba(0, 0, 0, 0.25), 0px 10px 10px 0px rgba(0, 0, 0, 0.22), 0px 14px 84px 0px rgba(255, 255, 255, 0.03)',
+    dark5:
+      '0px 19px 38px 0px rgba(0, 0, 0, 0.30), 0px 15px 12px  0px rgba(0, 0, 0, 0.22), 0px 16px 96px 0px rgba(255, 255, 255, 0.03)',
+    soft: '0px 0px 8px rgba(0, 0, 0, 0.08)',
+    soft2: '0px 0px 16px rgba(0, 0, 0, 0.08)',
   },
   transitions: {
     all: 'all 0.3s',
