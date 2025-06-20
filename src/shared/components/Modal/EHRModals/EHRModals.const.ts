7688cf6d8b3a5cac0026bc527ca493ce326a9377
André Vital <andre@andrevital.com>
2025-06-20 16:46:51 -0600

    feat: Update EHR Banner links (M2-8965) (#2095)
    * feat: Update EHR Banner links
    
    * feat: Update EHR Modal link

--- src/shared/components/Modal/EHRModals/EHRModals.const.ts
+++ src/shared/components/Modal/EHRModals/EHRModals.const.ts
@@ -1,3 +1,2 @@
-// TODO: Replace with actual link once microsite is ready
-// https://mindlogger.atlassian.net/browse/M2-8965
-export const EHR_GO_TO_WEBSITE_URL = 'https://mindlogger.org/';
+export const EHR_GO_TO_WEBSITE_URL =
+  'https://mindlogger.atlassian.net/servicedesk/customer/portal/3/article/1238368259';
