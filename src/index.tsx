b57016317b7a0e60195bb72b913c9fa3dbd1ebd9
Andrew Weiland <aweiland@users.noreply.github.com>
2025-11-14 13:24:24 -0500

    Fix?

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { asyncWithLDProvider } from 'launchdarkly-react-client-sdk';
 import { datadogRum, RumEvent } from '@datadog/browser-rum';
 import { datadogLogs } from '@datadog/browser-logs';
-import type { RumEventDomainContext } from '@datadog/browser-rum-core/src/domainContext.types';
 
 import { Mixpanel } from 'shared/utils/mixpanel';
 
@@ -49,7 +48,7 @@ if (
       (url) => url.indexOf('api-uat.cmiml.net') > -1,
       (url) => url.indexOf('api-v2.gettingcurious.com') > -1,
     ],
-    beforeSend: (event: RumEvent, context: RumEventDomainContext) => {
+    beforeSend: (event: RumEvent) => {
       // Do not instrument S3 calls, especially for exports.
       if (event.type === 'resource' && event.resource.url.indexOf('s3.amazonaws.com') > -1) {
         return false;
