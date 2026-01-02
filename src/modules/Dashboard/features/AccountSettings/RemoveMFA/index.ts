e178aa4da0a08f5944f1b4da42b632ae81d050a3
sricharan varanasi <59170910+sricharan-varanasi@users.noreply.github.com>
2026-01-02 18:30:25 -0500

    fix: Export ErrorResponse from types instead of utils

--- src/modules/Dashboard/features/AccountSettings/RemoveMFA/index.ts
+++ src/modules/Dashboard/features/AccountSettings/RemoveMFA/index.ts
@@ -2,7 +2,6 @@ export { RemoveMFA } from './RemoveMFA';
 export { RemoveMFAConfirmation } from './RemoveMFAConfirmation';
 export { useRemoveMFA } from './useRemoveMFA';
 export type { RemoveMFAConfirmationProps } from './RemoveMFAConfirmation.types';
-export type { RemoveMFAProps } from './RemoveMFA.types';
+export type { RemoveMFAProps, ErrorResponse } from './RemoveMFA.types';
 export { MFA_DISABLE_ERROR_MESSAGES } from './RemoveMFA.constants';
 export { getErrorMessage } from './RemoveMFA.utils';
-export type { ErrorResponse } from './RemoveMFA.utils';
