5483006422e63c8e666184f6b71c1503741e4fb8
sricharan varanasi <59170910+sricharan-varanasi@users.noreply.github.com>
2026-06-12 11:43:21 -0400

    feat(M2-10875): update footer links from mindlogger.org to gettingcurious.com

--- src/shared/components/Footer/Footer.const.ts
+++ src/shared/components/Footer/Footer.const.ts
@@ -1,6 +1,6 @@
 export const CMI_LINK = 'https://childmind.org';
-export const PRIVACY_LINK = 'https://mindlogger.org/privacy-policy';
-export const TERMS_LINK = 'https://mindlogger.org/terms';
-export const ABOUT_LINK = 'https://mindlogger.org';
+export const PRIVACY_LINK = 'https://gettingcurious.com/privacy-policy';
+export const TERMS_LINK = 'https://gettingcurious.com/terms';
+export const ABOUT_LINK = 'https://gettingcurious.com';
 export const CREDITS_LINK = 'https://www.gettingcurious.com/open-source-credits';
-export const SUPPORT_LINK = 'https://help.mindlogger.org';
+export const SUPPORT_LINK = 'https://help.gettingcurious.com';
